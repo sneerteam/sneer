c83323c990a7f4368234f703d4e043be5686c092
Rodrigo B. de Oliveira <rbo@acm.org>
2014-04-14 17:43:49 -0300

    service connection porcelain

---
+++ src/sneerteam/snapi/CloudServiceConnection.java
@@ -0,0 +1,53 @@
+package sneerteam.snapi;
+
+import rx.Observable;
+import rx.subjects.AsyncSubject;
+import rx.subjects.Subject;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.IBinder;
+
+public class CloudServiceConnection {
+	
+	public static CloudServiceConnection prepare() {
+		return new CloudServiceConnection();
+	}
+	
+	final Subject<CloudConnection, CloudConnection> connection = AsyncSubject.create();
+	
+	final ServiceConnection snapi = new ServiceConnection() {		@Override
+		public void onServiceConnected(ComponentName name, IBinder binder) {
+			connection.onNext(new CloudConnection(binder));
+		}
+
+		@Override
+		public void onServiceDisconnected(ComponentName arg0) {
+		}
+	};
+
+	private boolean isBound;
+	
+	private CloudServiceConnection() {
+	}
+	
+	public Observable<CloudConnection> cloud() {
+		return connection;
+	}
+
+	public void bind(Context context) {
+		if (isBound) throw new IllegalStateException("connection already bound!");
+		isBound = true;
+		context.bindService(
+				bindCloudServiceIntent(),
+				snapi,
+				Context.BIND_AUTO_CREATE + Context.BIND_ADJUST_WITH_ACTIVITY);
+	}
+
+	private Intent bindCloudServiceIntent() {
+		Intent bindIntent = new Intent("sneerteam.intent.action.BIND_CLOUD_SERVICE");
+		bindIntent.setClassName("sneerteam.android.main", "sneerteam.android.main.CloudService");
+		return bindIntent;
+	}
+}
