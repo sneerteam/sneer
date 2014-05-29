a73798bb10b28a4e08fd33509ee3c579b60907c3
Fabio Roger Manera <fabioroger@gmail.com>
2014-05-29 19:58:36 +0100

    static import Pair.create

--- src/sneerteam/snapi/ContactUtils.java
+++ src/sneerteam/snapi/ContactUtils.java
@@ -1,5 +1,6 @@
 package sneerteam.snapi;
 
+import static android.util.Pair.*;
 import rx.*;
 import rx.functions.*;
 import android.util.*;
@@ -9,13 +10,13 @@ public class ContactUtils {
     public static Observable<String> nickname(Cloud cloud, String publicKey) {
         return Observable.merge(
                 cloud.path(":me", "contacts", publicKey, "nickname").value().map(new Func1<Object, Pair<Integer, Object>>() {@Override public Pair<Integer, Object> call(Object nickname) {
-                    return Pair.create(3, nickname);
+                    return create(3, nickname);
                 }}),
                 cloud.path(publicKey, "profile", "nickname").value().map(new Func1<Object, Pair<Integer, Object>>() {@Override public Pair<Integer, Object> call(Object nickname) {
-                    return Pair.create(2, nickname);
+                    return create(2, nickname);
                 }}),
                 cloud.path(publicKey, "profile", "name").value().map(new Func1<Object, Pair<Integer, Object>>() {@Override public Pair<Integer, Object> call(Object nickname) {
-                    return Pair.create(1, nickname);
+                    return create(1, nickname);
                 }})).scan(new Func2<Pair<Integer, Object>, Pair<Integer, Object>, Pair<Integer, Object>>() {@Override public Pair<Integer, Object> call(Pair<Integer, Object> a, Pair<Integer, Object> b) {
                     return a.first > b.first ? a : b;
                 }})
