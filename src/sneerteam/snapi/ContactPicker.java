7a4b10d21c194e472ac6e69578294c383732e33c
Fabio Roger Manera <fabioroger@gmail.com>
2014-06-04 12:25:50 +0100

    new contact picker using ResultReceiver

--- src/sneerteam/snapi/ContactPicker.java
+++ src/sneerteam/snapi/ContactPicker.java
@@ -1,18 +1,26 @@
 package sneerteam.snapi;
 
-import android.app.Activity;
-import android.content.Intent;
+import rx.*;
+import rx.Observable.OnSubscribe;
+import rx.android.schedulers.*;
+import android.content.*;
+import android.os.*;
 
 public class ContactPicker {
 
-	public static void startActivityForResult(Activity caller, int requestCode) {
-		Intent intent = new Intent("sneerteam.intent.action.PICK_CONTACT");
-		caller.startActivityForResult(intent, requestCode);
-	}
-	
-	public static Contact contactFrom(Intent intent) {
-		return new Contact(
-			intent.getExtras().get("public_key").toString(),
-			intent.getExtras().get("nickname").toString());
+	public static Observable<Contact> pickContact(final Context context) {
+	    return Observable.create(new OnSubscribe<Contact>() {@Override public void call(final Subscriber<? super Contact> subscriber) {
+	        Intent intent = new Intent("sneerteam.intent.action.PICK_CONTACT");
+	        intent.putExtra("result", new ResultReceiver(null) {
+	            @Override
+	            protected void onReceiveResult(int resultCode, Bundle resultData) {
+	                subscriber.onNext(new Contact(
+	                        resultData.get("public_key").toString(),
+	                        resultData.get("nickname").toString()));
+	                subscriber.onCompleted();
+	            }
+	        });
+	        context.startActivity(intent);
+        }}).subscribeOn(AndroidSchedulers.mainThread()).observeOn(AndroidSchedulers.mainThread());
 	}
 }
