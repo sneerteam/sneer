dbec6641b74cc4e0f14ce37a264cb67704686c47
Klaus Wuestefeld <klauswuestefeld@gmail.com>
2014-07-17 14:43:49 -0300

    Usable PrivateKeys done.

--- java-api/src/sneer/impl/Keys.java
+++ java-api/src/sneer/impl/Keys.java
@@ -4,9 +4,30 @@ import sneer.*;
 
 public class Keys {
 
+	private static int nextNumber = 1;
+
 	public static PrivateKey newPrivateKey() {
-		// TODO Auto-generated method stub
-		return null;
+		
+		final long number = nextNumber++;
+		
+		return new PrivateKey() {
+			
+			@Override
+			public String toString() {
+				return "PRIK:" + number;
+			}
+
+			@Override
+			public PublicKey publicKey() {
+				return new PublicKey() {
+					@Override
+					public String toString() {
+						return "PUK:" + number;
+					}
+				};
+			}
+			
+		};
 	}
 
 }
