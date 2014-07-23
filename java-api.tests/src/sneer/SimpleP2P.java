97da995a83065ee5ad2b77ef2b593d4dca6713b2
Rodrigo B. de Oliveira <rbo@acm.org>
2014-07-23 12:20:38 -0700

    move commented out code to the end

--- java-api.tests/src/sneer/SimpleP2P.java
+++ java-api.tests/src/sneer/SimpleP2P.java
@@ -13,6 +13,88 @@ import sneer.tuples.*;
 public class SimpleP2P extends TestsBase {
 	
 	
+	@Test
+	public void messagePassing() throws IOException {
+
+		TuplePublisher publisher = tuplesA.newTuplePublisher()
+			.audience(userB.publicKey())
+			.type("rock-paper-scissor/move")
+			.pub("paper");
+			
+		publisher.pub("rock");
+		
+		publisher.type("rock-paper-scissor/message")
+			.pub("hehehe");
+		
+		
+		TupleSubscriber subscriber = tuplesB.newTupleSubscriber();
+
+		expectValues(subscriber.tuples(), "paper", "rock", "hehehe");
+		expectValues(subscriber.type("rock-paper-scissor/move").tuples(), "paper", "rock");
+		expectValues(subscriber.type("rock-paper-scissor/message").tuples(), "hehehe");
+		
+	}
+
+	@Test
+	public void tupleWithType() throws IOException {
+
+		tuplesA.newTuplePublisher()
+			.audience(userB.publicKey())
+			.type("rock-paper-scissor/move")
+			.pub("paper")
+			.type("rock-paper-scissor/message")
+			.pub("hehehe");
+		
+		assertEqualsUntilNow(tuplesB.newTupleSubscriber().tuples().map(TO_TYPE), "rock-paper-scissor/move", "rock-paper-scissor/message");
+		
+	}
+	
+	@Test
+	public void targetUser() {
+		
+		tuplesA.newTuplePublisher()
+			.audience(userC.publicKey())
+			.type("rock-paper-scissor/move")
+			.pub("paper");
+		
+		assertCount(0, tuplesB.newTupleSubscriber().tuples());
+		assertCount(1, tuplesC.newTupleSubscriber().tuples());
+	}
+	
+	@Test
+	public void publicTuples() {
+		
+		tuplesA.newTuplePublisher()
+			.type("profile/name")
+			.pub("UserA McCloud");
+		
+		assertCount(1, tuplesA.newTupleSubscriber().tuples()); // should I receive my own public tuples?
+		assertCount(1, tuplesB.newTupleSubscriber().tuples());
+		assertCount(1, tuplesC.newTupleSubscriber().tuples());
+		
+	}
+	
+	@Test
+	public void byAuthor() {
+		tuplesA.newTuplePublisher()
+			.type("profile/name")
+			.pub("UserA McCloud");
+		
+		assertCount(1, tuplesB.newTupleSubscriber().author(userA.publicKey()).tuples());
+		assertCount(0, tuplesB.newTupleSubscriber().author(userC.publicKey()).tuples());
+	}
+	
+	@Test
+	public void audienceIgnoresPublic() {
+		
+		tuplesA.newTuplePublisher()
+			.type("chat/message")
+			.pub("hey people!");
+		
+		PrivateKey group = Keys.createPrivateKey();
+		assertCount(0, tuplesB.newTupleSubscriber().audience(group).tuples());
+	}
+	
 //	public static void main(String[] args) {
 //		
 //		Sneer sneer = SneerFactory.newSneer();
@@ -148,87 +230,5 @@ public class SimpleP2P extends TestsBase {
 //	}
 	
 	
-	
-	@Test
-	public void messagePassing() throws IOException {
-
-		TuplePublisher publisher = tuplesA.newTuplePublisher()
-			.audience(userB.publicKey())
-			.type("rock-paper-scissor/move")
-			.pub("paper");
-			
-		publisher.pub("rock");
-		
-		publisher.type("rock-paper-scissor/message")
-			.pub("hehehe");
-		
-		
-		TupleSubscriber subscriber = tuplesB.newTupleSubscriber();
-
-		expectValues(subscriber.tuples(), "paper", "rock", "hehehe");
-		expectValues(subscriber.type("rock-paper-scissor/move").tuples(), "paper", "rock");
-		expectValues(subscriber.type("rock-paper-scissor/message").tuples(), "hehehe");
-		
-	}
-
-	@Test
-	public void tupleWithType() throws IOException {
-
-		tuplesA.newTuplePublisher()
-			.audience(userB.publicKey())
-			.type("rock-paper-scissor/move")
-			.pub("paper")
-			.type("rock-paper-scissor/message")
-			.pub("hehehe");
-		
-		assertEqualsUntilNow(tuplesB.newTupleSubscriber().tuples().map(TO_TYPE), "rock-paper-scissor/move", "rock-paper-scissor/message");
-		
-	}
-	
-	@Test
-	public void targetUser() {
-		
-		tuplesA.newTuplePublisher()
-			.audience(userC.publicKey())
-			.type("rock-paper-scissor/move")
-			.pub("paper");
-		
-		assertCount(0, tuplesB.newTupleSubscriber().tuples());
-		assertCount(1, tuplesC.newTupleSubscriber().tuples());
-	}
-	
-	@Test
-	public void publicTuples() {
-		
-		tuplesA.newTuplePublisher()
-			.type("profile/name")
-			.pub("UserA McCloud");
-		
-		assertCount(1, tuplesA.newTupleSubscriber().tuples()); // should I receive my own public tuples?
-		assertCount(1, tuplesB.newTupleSubscriber().tuples());
-		assertCount(1, tuplesC.newTupleSubscriber().tuples());
-		
-	}
-	
-	@Test
-	public void byAuthor() {
-		tuplesA.newTuplePublisher()
-			.type("profile/name")
-			.pub("UserA McCloud");
-		
-		assertCount(1, tuplesB.newTupleSubscriber().author(userA.publicKey()).tuples());
-		assertCount(0, tuplesB.newTupleSubscriber().author(userC.publicKey()).tuples());
-	}
-	
-	@Test
-	public void audienceIgnoresPublic() {
-		
-		tuplesA.newTuplePublisher()
-			.type("chat/message")
-			.pub("hey people!");
-		
-		PrivateKey group = Keys.createPrivateKey();
-		assertCount(0, tuplesB.newTupleSubscriber().audience(group).tuples());
-	}
 	
 }
