208c756cbde988eecfb01caa702828a6baf32997
Fabio Roger Manera <fabioroger@gmail.com>
2014-07-24 21:48:49 +0100

    refactor show install sneer dialog

--- android-api/src/sneer/snapi/SneerUtils.java
+++ android-api/src/sneer/snapi/SneerUtils.java
@@ -13,21 +13,25 @@ public class SneerUtils {
         try {
            pm.getPackageInfo("sneer.android.main", PackageManager.GET_ACTIVITIES);
         } catch (PackageManager.NameNotFoundException e) {
-            new AlertDialog.Builder(activity)
-            .setTitle("You Need the Sneer App")
-            .setMessage("Do you want to install it now?")
-            .setPositiveButton("Yes", new OnClickListener() {@Override public void onClick(DialogInterface arg0, int option) {
-                Intent goToMarket = new Intent(Intent.ACTION_VIEW).setData(Uri.parse("market://details?id=sneer.android.main"));
-                goToMarket.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                activity.startActivity(goToMarket);
-                activity.finish();
-            }})
-            .setNegativeButton("No", new OnClickListener() {@Override public void onClick(DialogInterface arg0, int option) {
-                activity.finish();
-            }})
-            .setIcon(android.R.drawable.ic_dialog_alert)
-            .show();
+            showInstallSneerDialog(activity);
         }
     }
+
+	public static void showInstallSneerDialog(final Activity activity) {
+		new AlertDialog.Builder(activity)
+		.setTitle("You Need the Sneer App")
+		.setMessage("Do you want to install it now?")
+		.setPositiveButton("Yes", new OnClickListener() {@Override public void onClick(DialogInterface arg0, int option) {
+		    Intent goToMarket = new Intent(Intent.ACTION_VIEW).setData(Uri.parse("market://details?id=sneer.android.main"));
+		    goToMarket.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+		    activity.startActivity(goToMarket);
+		    activity.finish();
+		}})
+		.setNegativeButton("No", new OnClickListener() {@Override public void onClick(DialogInterface arg0, int option) {
+		    activity.finish();
+		}})
+		.setIcon(android.R.drawable.ic_dialog_alert)
+		.show();
+	}
 
 }
