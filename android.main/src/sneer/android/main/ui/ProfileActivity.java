99443c9e5a6c02e7e8f08ebf109688b7254fd161
Felipe Bueno <felipe@bueno-eos.(none)>
2014-07-26 00:31:47 -0300

    ProfileActivity progress

--- android.main/src/sneer/android/main/ui/ProfileActivity.java
+++ android.main/src/sneer/android/main/ui/ProfileActivity.java
@@ -3,7 +3,6 @@ package sneer.android.main.ui;
 import static sneer.android.main.SneerSingleton.*;
 
 import java.io.*;
-import java.util.*;
 
 import sneer.*;
 import sneer.android.main.*;
@@ -13,7 +12,6 @@ import android.content.*;
 import android.content.pm.*;
 import android.graphics.*;
 import android.graphics.drawable.*;
-import android.net.*;
 import android.os.*;
 import android.provider.*;
 import android.view.*;
@@ -35,6 +33,7 @@ public class ProfileActivity extends Activity {
 	
 	Bitmap bitMap;
 	
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -50,28 +49,22 @@ public class ProfileActivity extends Activity {
 		cityEdit = (EditText) profileView.findViewById(R.id.city);
 		privacyCheckBox = (CheckBox) profileView.findViewById(R.id.privacycheckBox);
 		
-		
-		
-		// Does your device have a camera?
-        if(hasCamera()){
-        	firstNameEdit.setBackgroundColor(0xFF00CC00);
-        	firstNameEdit.setText("You have Camera");
-        }
- 
-        // Do you have Camera Apps?
-        if(hasDefualtCameraApp(MediaStore.ACTION_IMAGE_CAPTURE)){
-        	lastNameEdit.setBackgroundColor(0xFF00CC00);
-        	lastNameEdit.setText("You have Camera Apps");
-        }
-		
+		loadProfile();
 	}
 	
+	
+	private void loadProfile() {
+//	     TODO Load user profile here
+//		firstNameEdit.setText(...);
+	}
+
+
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		getMenuInflater().inflate(R.menu.profile, menu);
 		return true;
 	}
-
+	
 	
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
@@ -84,15 +77,23 @@ public class ProfileActivity extends Activity {
 			}
 			break;
 		}
-
 		return true;
 	}
 	
+
 	@Override
 	public void onContentChanged() {
 		super.onContentChanged();
 	}
 	
+    @Override
+    protected void onResume() {
+    	super.onResume();
+    	LayoutInflater inflater1 = (LayoutInflater)getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+//    	 TODO Redraw UI here to update EditTexts text.
+    }
+	
+
 	public void saveProfile() throws FriendlyException {
 		toast("saving profile...");
 
@@ -115,41 +116,35 @@ public class ProfileActivity extends Activity {
 		profile.setSelfie(imageInByte);
 	}
 	
+	
 	public void selfieOnClick(View v) {
-		 toast("Selfie was clicked. :-)");
-		 Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-		 
-		 File file = new File(Environment.getExternalStorageDirectory(), "selfie.jpg");
-		 Uri photoPath = Uri.fromFile(file);
-		 intent.putExtra(MediaStore.EXTRA_OUTPUT, photoPath);
-		
-		 startActivityForResult(intent, TAKE_PICTURE);
+		if (hasCamera()) {
+			toast("Selfie was clicked. :-)");
+			Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+			startActivityForResult(intent, TAKE_PICTURE);
+		} else {
+			toast("Your phone doesn't have a camera. :-(");
+		}
 	}
+	
+	
 	@Override
     protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
-        if (requestCode == TAKE_PICTURE && resultCode== RESULT_OK && intent != null){
-            Bundle extras = intent.getExtras();
-            bitMap = (Bitmap) extras.get("data");
-            selfieImage.setImageBitmap(bitMap);
-        }
+		if (requestCode == TAKE_PICTURE && resultCode== RESULT_OK && intent != null){
+			Bundle extras = intent.getExtras();
+			bitMap = (Bitmap) extras.get("data");
+
+			selfieImage.setImageBitmap(bitMap); // TODO setImageBitmap not working
+		}
     }
 	
-	 // method to check if you have a Camera
-    private boolean hasCamera(){
+
+	private boolean hasCamera(){
         return getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA);
     }
  
-    // method to check you have Camera Apps
-    private boolean hasDefualtCameraApp(String action){
-        final PackageManager packageManager = getPackageManager();
-        final Intent intent = new Intent(action);
-        List<ResolveInfo> list = packageManager.queryIntentActivities(intent, PackageManager.MATCH_DEFAULT_ONLY);
- 
-        return list.size() > 0;
- 
-    }
 
-	private void toast(String message) {
+    private void toast(String message) {
 		Toast toast = Toast.makeText(this, message, Toast.LENGTH_SHORT);
 		toast.show();
 	}
