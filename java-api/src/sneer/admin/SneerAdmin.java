cb7d10fe07be358c61757fdbac2facb1d34797fb
Klaus Wuestefeld <klauswuestefeld@gmail.com>
2014-08-04 09:52:36 -0300

    Minor

--- java-api/src/sneer/admin/SneerAdmin.java
+++ java-api/src/sneer/admin/SneerAdmin.java
@@ -6,7 +6,7 @@ public interface SneerAdmin {
 
 	void initialize(PrivateKey prik);
 	
-	/** @return The private key used to initialize this Sneer node. */
+	/** @return The private key used to initialize this Sneer node. Null if this has not yet been initialized. */
 	PrivateKey privateKey();
 	
 	/** This must have a privateKey(). */
