9d4fa6bbf88bf81bd1957cb276073d99c871bafa
Fabio Roger Manera <fabioroger@gmail.com>
2014-08-06 17:10:55 +0100

    reformat

--- core/test/sneer/InteractionsAPITest.java
+++ core/test/sneer/InteractionsAPITest.java
@@ -13,85 +13,83 @@ public class InteractionsAPITest extends InteractionsAPITestsBase {
 
 	@Test
 	public void changesPrik() {
-		
+
 		assertEquals(prikA, adminA.privateKey());
-		
+
 		PrivateKey anotherPrikInTheFireWall = Keys.createPrivateKey();
 		adminA.initialize(anotherPrikInTheFireWall);
-		
+
 		assertEquals(anotherPrikInTheFireWall, adminA.privateKey());
 	}
-	
+
 	@Test
 	public void myOwnPublicKey() {
-		
-		assertEquals(prikA.publicKey(),  sneerA.self().publicKey().mostRecent());
-		
+
+		assertEquals(prikA.publicKey(), sneerA.self().publicKey().mostRecent());
+
 		assertEqualsUntilNow(sneerA.self().publicKey().observable(), prikA.publicKey());
-		
+
 	}
 
 	@Test
 	public void pukOfParty() {
-		
+
 		Party someone = sneerA.produceParty(prikB.publicKey());
-		
+
 		assertEquals(prikB.publicKey(), someone.publicKey().mostRecent());
-		
+
 	}
 
 	@Test
 	public void alwaysReturnsSamePartyInstance() {
-		
+
 		Party someoneElse = sneerA.produceParty(prikB.publicKey());
-		
+
 		assertSame(someoneElse, sneerA.produceParty(prikB.publicKey()));
-		
+
 	}
 
 	@Test
 	public void addContact() throws FriendlyException {
-		
+
 		Party partyB = sneerA.produceParty(prikB.publicKey());
-		
+
 		assertNull(sneerA.findContact(partyB));
-		
+
 		sneerA.setContact("Party Boy", partyB);
-		
+
 		Contact contactB = sneerA.findContact(partyB);
 		assertNotNull(contactB);
 		assertSame(partyB, contactB.party());
 	}
 
-	@Test(expected=FriendlyException.class)
+	@Test(expected = FriendlyException.class)
 	public void exceptionOnDuplicatedNickname() throws FriendlyException {
-		
+
 		sneerA.setContact("Party Boy", sneerA.produceParty(prikB.publicKey()));
 		sneerA.setContact("Party Boy", sneerA.produceParty(prikC.publicKey()));
 	}
-	
+
 	@Test
 	public void changeContactNickname() throws FriendlyException {
-		
+
 		Party partyB = sneerA.produceParty(prikB.publicKey());
-		
+
 		sneerA.setContact("Party Boy", partyB);
-		
+
 		Contact contactB = sneerA.findContact(partyB);
-		
+
 		assertEquals("Party Boy", contactB.nickname().mostRecent());
-		
+
 		ReplaySubject<String> nicknames = ReplaySubject.create();
-		
+
 		contactB.nickname().observable().subscribe(nicknames);
-		
+
 		sneerA.setContact("Party Man", partyB);
-		
+
 		assertEqualsUntilNow(nicknames, "Party Boy", "Party Man");
-		
+
 		assertEqualsUntilNow(contactB.nickname().observable(), "Party Man");
 	}
-	
-	
 
 }
