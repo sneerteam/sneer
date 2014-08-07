f62e08cbb619e73aea5f4ddb3a0defbc1d9ddcc2
Rodrigo B. de Oliveira <rbo@acm.org>
2014-08-07 10:09:02 -0700

    set groupId as me.sneer for all projects

--- server/project.clj
+++ server/project.clj
@@ -1,6 +1,7 @@
 (defproject sneer.server "0.1.0-SNAPSHOT"
   :description "The Sneer Temporary Server"
-  :dependencies [[sneer/java-api "0.0.1"]
+  :dependencies [[me.sneer/java-api "0.0.1"]
+                 [me.sneer/core "0.0.1"]
                  [com.taoensso/timbre "3.2.1"] ;Logging
                  [org.clojure/core.async "0.1.303.0-886421-alpha"]
                  [org.clojure/clojure "1.6.0"]]
