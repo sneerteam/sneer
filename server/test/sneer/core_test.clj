bee819fb4666e5870bee9899017c5696527c8e9b
Rodrigo B. de Oliveira <rbo@acm.org>
2014-08-07 10:15:46 -0700

    [server protocol] ping/pong

--- server/test/sneer/core_test.clj
+++ server/test/sneer/core_test.clj
@@ -1,6 +1,6 @@
 (ns sneer.core-test
   (:require
-   [clojure.core.async :as async :refer [chan >! <! >!! <!! alts!! timeout]]
+   [clojure.core.async :as async :refer [>! <! >!! <!! alts!! timeout]]
    [clojure.test :refer :all]
    [midje.sweet :refer :all]
    [sneer.server.core :as core]
@@ -13,35 +13,58 @@
 (defn create-puk [seed]
   seed)
 
-(defn create-server []
-  "Creates a packet relay server.
+(defn create-server [packets-in packets-out]
+  "Store-and-Forward Server Protocol
 
-The server can either accept a packet from a client or not in which
-case the client should retry later.
+The purpose of the temporary central server is to store
+payloads (byte[]s) being sent from one peer to another in a
+limited-size (100 elements?) FIFO queue and forward them as soon as
+possible.  Clients must gracefully handle the server losing all its
+state at any moment and restarting fresh.
 
-After accepting a packet the server guarantees eventual delivery of the packet.
+Packets From Client to Server
+ping()
+Let's the server know the client is
+online, when the client has no other useful packet to send. Keeps the
+UDP connection open. Server replies with pong().
 
-The following operations are supported:
+sendTo(Puk receiver, boolean reset, long sequence, byte[] payload)
+If reset is true, the server will discard the entire queue of packets to
+be sent to receiver, if any, and use sequence as the next expected
+sequence number. The server does not expect any sequence number when it
+starts, so the first packet sent from a peer to another must always have
+reset true. If sequence is the next expected sequence number, adds
+payload to the queue of payloads to be sent from the sender to receiver,
+otherwise ignores the payload. Server replies with a statusOfQueues
+packet for receiver (see below), even if sequence was wrong.
 
-# source to server protocol
+ack(Puk sender, long sequence)
+See receiveFrom(...) below.
 
-peer->server: {:intent :packet :address <destination puk> :sequence <client assigned sequence number> :payload <.> }
-server->peer: {:intent :ack :last-sent <sequence> :last-to-send <sequence>}
+queryStatusOfQueues(Puk[] peers)
+Server replies with a statusOfQueue packet for peers. See below.
 
-If both :last-sent and :last-to-send are nil this is the first time the server hears about client
-or the server lost its state, the client should send all packets.
+Packets From Server to Client
+pong()
+Reply to ping() to indicate the server is alive.
 
-peer->server: {:intent :status}
-server->peer: same as :ack above
+receiveFrom(Puk sender, long sequence, byte[] payload)
+Client replies with ack(sender, sequence);
 
-# destination to server protocol
-
-peer->server: {:intent :query-next-packet :puk <puk>}
-server->peer: {:intent :packet :packet <packet> :id <server assigned packet id>}
-peer->server: {:intent :ack :id <id>}
-
+statusOfQueues(StatusOfQueue[] statuses)
+Indicates de status of the queues of payloads from this client to some
+peers. StatusOfQueue is {Puk receiver, long highest-sequence-delivered,
+long highest-sequence-to-send, boolean isFull}. The client can consider
+all payloads up to highest-sequence-delivered as received by the
+receiver. The client can consider all payloads up to
+highest-sequence-to-send as received by the server and temporarily (the
+server might crash and restart fresh) queued to be sent to
+receiver. This packet is sent as a reply to queryStatusOfQueues(...), as
+a reply to sendTo(...) and when the receiver
 "
-  (atom {}))
+  (async/go-loop []
+    (let [packet (<! packets-in)]
+      (>!! packets-out {:intent :pong :to (:from packet)}))))
 
 (defn route-packet [server-map packet])
 
@@ -50,19 +73,12 @@ peer->server: {:intent :ack :id <id>}
 (defn next-packet-for [puk server])
 
 (facts
- "Facts"
- (let [a (create-puk "a")
-       b (create-puk "b")
-       server (create-server)
-       packet-a {:address a}
-       packet-b {:address b}]
+ "Facts about client/server interactions"
 
-   (route! server packet-a)
-   (route! server packet-b)
-
-   (fact "server stores packets"
-         (next-packet-for a server) => packet-a
-         (next-packet-for b server) => packet-b)
-
-   (fact "server forgets sent packets"
-         (next-packet-for a server)) => nil))
+ (fact "for each ping its pong"
+       (let [client (create-puk "c")
+             packets-in (async/chan 1)
+             packets-out (async/chan)
+             server (create-server packets-in packets-out)]
+         (>!! packets-in {:intent :ping :from client})
+         (<?!! packets-out) => {:intent :pong :to client})))
