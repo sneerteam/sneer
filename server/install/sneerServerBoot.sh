d8f49c8f0b49c77b372926970344f6edf7d44620
Rodrigo B. de Oliveira <rbo@acm.org>
2014-08-09 07:33:41 -0700

    update simploy server scripts

--- server/install/sneerServerBoot.sh
+++ server/install/sneerServerBoot.sh
@@ -3,15 +3,5 @@ git pull
 rm *.class
 javac Simploy.java
 
-cd /root/server
+cd /root/sneer
 java -cp /root/simploy/ Simploy 44321 senhamalucaqq&
-
-cd /root/snapi
-java -cp /root/simploy/ Simploy 44322 senhamalucaqq&
-
-cd /root/android.main
-java -cp /root/simploy/ Simploy 44323 senhamalucaqq&
-
-cd /root/android.chat
-java -cp /root/simploy/ Simploy 44324 senhamalucaqq&
-
--- simploy.sh
+++ simploy.sh
@@ -1,26 +1,4 @@
-# Allow running lein as root
-export LEIN_ROOT=yes
-
 # Install necessary project dependencies to local maven cache
-./gradlew install
+./gradlew install || exit -1
 
-# Build new server version
-cd server
-lein do clean, midje, uberjar || exit -1
-
-echo REPLACE UBERJAR
-mv target/uberjar/*standalone.jar . || exit -1
-
-echo MKDIR LOGS
-mkdir -p logs || exit -1
-
-echo PKILL OLD SERVER PROCESS
-pkill -f sneer.server-0.1.0-SNAPSHOT-standalone.jar
-
-# Append date to previous log filename
-mv logs/log "logs/log-`date +%Y-%m-%d_%H-%M`"
-
-echo START NEW SERVER PROCESS
-SERVER_PORT=5555
-java -jar ./sneer.server-0.1.0-SNAPSHOT-standalone.jar $SERVER_PORT  > logs/log&
-echo DONE
+cd server && ./simploy.sh && cd - || exit -1
