76e5d76200f1e8a2032d9d265f4c3024cc17053e
Klaus Wuestefeld <klauswuestefeld@gmail.com>
2014-08-09 09:37:45 -0300

    [Server] Append date to previous log filename.

--- server/simploy.sh
+++ server/simploy.sh
@@ -1,22 +1,24 @@
+# Allow running lein as root
 export LEIN_ROOT=yes
 
-# update networker
+# Update networker
 cd ../networker && git pull && ./gradlew uploadArchives && cd -
 
-# build new server version
+# Build new server version
 lein do clean, test, uberjar || exit -1
 
 echo REPLACE UBERJAR
-mv ./target/*standalone.jar . || exit -1
+mv target/*standalone.jar . || exit -1
 
 echo MKDIR LOGS
-mkdir -p ./logs || exit -1
-#rename ./logs/log file to ./logs/[date]
+mkdir -p logs || exit -1
 
-# restart
 echo PKILL OLD SERVER PROCESS
 pkill -f sneer.server-0.1.0-SNAPSHOT-standalone.jar
-echo START NEW SERVER PROCESS
-java -jar ./sneer.server-0.1.0-SNAPSHOT-standalone.jar > ./logs/log&
-echo DONE
 
+# Append date to previous log filename
+mv logs/log "logs/log-`date +%Y-%m-%d_%H-%M`"
+
+echo START NEW SERVER PROCESS
+java -jar ./sneer.server-0.1.0-SNAPSHOT-standalone.jar > logs/log&
+echo DONE
