96f2be7bb744f5c406cad1ca3da0675c569508d6
Klaus Wuestefeld <klauswuestefeld@gmail.com>
2014-08-11 12:44:40 -0300

    Contact.problemWithNewNickname(String newNick)

--- java-api/src/sneer/Contact.java
+++ java-api/src/sneer/Contact.java
@@ -9,7 +9,9 @@ public interface Contact {
 	Party party();
 	
 	Observed<String> nickname();
-	
+
+	/** @return null if the new nickname is ok or a reason why the new nickname is not ok. */
+	String problemWithNewNickname(String newNick);
 	
 	Comparator<Contact> BY_NICKNAME = new Comparator<Contact>() { @Override public int compare(Contact c1, Contact c2) {
 		return c1.nickname().mostRecent().compareToIgnoreCase(c2.nickname().mostRecent());
