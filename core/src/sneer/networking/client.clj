fd292e09baa6c83fa6a108598336f58210aa9720
Rodrigo B. de Oliveira <rbo@acm.org>
2014-08-11 14:11:03 -0700

    ensure no network activity takes place on caller thread to workaround android limitation

--- core/src/sneer/networking/client.clj
+++ core/src/sneer/networking/client.clj
@@ -6,23 +6,25 @@
 
 (defn start [& [server-host server-port]]
 
-  (let [server-addr (InetSocketAddress. (or server-host "dynamic.sneer.me") (or server-port 5555))
-        packets-in (async/chan)
-        packets-out (async/chan)
-        udp-server (udp/serve-udp packets-in packets-out)
-        ping [server-addr {:intent :ping}]]
+  (let [packets-in (async/chan)
+        packets-out (async/chan)]
 
-    ; server ping loop
-    (async/go-loop []
-      (when  (>! packets-out ping)
-        (<! (async/timeout 20000))
-        (recur)))
+    (async/thread
+      ; ensure no network activity takes place on caller thread to workaround android limitation
+      (let [server-addr (InetSocketAddress. (or server-host "dynamic.sneer.me") (or server-port 5555))
+            udp-server (udp/serve-udp packets-in packets-out)
+            ping [server-addr {:intent :ping}]]
+                                        ; server ping loop
+        (async/go-loop []
+          (when  (>! packets-out ping)
+            (<! (async/timeout 20000))
+            (recur)))
 
-    ; just report received packets for now
-    (async/go-loop []
-      (when-let [packet (<! packets-in)]
-        (SystemReport/updateReport "packet" packet)
-        (recur)))
+        ; just report received packets for now
+        (async/go-loop []
+          (when-let [packet (<! packets-in)]
+            (SystemReport/updateReport "packet" packet)
+            (recur)))))
 
     {:packets-out packets-out}))
 
