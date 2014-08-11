5fd43ba5961935aa094916905c7e0abf4b47c8ea
Klaus Wuestefeld <klauswuestefeld@gmail.com>
2014-08-11 12:35:34 -0300

    Minor

--- java-api/src/sneer/commons/exceptions/FriendlyException.java
+++ java-api/src/sneer/commons/exceptions/FriendlyException.java
@@ -3,6 +3,10 @@ package sneer.commons.exceptions;
 /** An Exception with a very user-friendly message. */
 public class FriendlyException extends Exception {
 
+	public FriendlyException(String veryUserFriendlyMessage) {
+		super(veryUserFriendlyMessage);
+	}
+
 	private static final long serialVersionUID = 1L;
 
 }
