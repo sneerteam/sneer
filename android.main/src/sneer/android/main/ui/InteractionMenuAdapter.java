bc36f9d4a1f23f025263a72acb51294db98d081e
Felipe Bueno <bueno.felipe@gmail.com>
2014-08-11 15:26:58 -0300

    add InteractionMenuAdapter

---
+++ android.main/src/sneer/android/main/ui/InteractionMenuAdapter.java
@@ -0,0 +1,15 @@
+package sneer.android.main.ui;
+
+import sneer.*;
+import android.content.*;
+import android.widget.*;
+
+public class InteractionMenuAdapter extends ArrayAdapter<InteractionMenuItem> {
+
+	public InteractionMenuAdapter(Context context, int resource) {
+		super(context, resource);
+		// TODO Auto-generated constructor stub
+	}
+
+	
+}
