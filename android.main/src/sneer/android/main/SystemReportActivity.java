580eb279d658bf192c0bc9a8507180d252d8796c
Felipe Bueno <bueno.felipe@gmail.com>
2014-08-12 10:59:25 -0300

    Start reporting stuff.

--- android.main/src/sneer/android/main/SystemReportActivity.java
+++ android.main/src/sneer/android/main/SystemReportActivity.java
@@ -1,23 +1,32 @@
 package sneer.android.main;
 
-import android.app.Activity;
-import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import sneer.commons.*;
+import android.app.*;
+import android.os.*;
+import android.view.*;
+import android.widget.*;
 
 public class SystemReportActivity extends Activity {
 
+	TextView reportView;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_system_report);
+		
+		reportView = (TextView)findViewById(R.id.reportView);
+		
+		report();
 	}
+
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		getMenuInflater().inflate(R.menu.system_report, menu);
 		return true;
 	}
+
 
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
@@ -26,5 +35,13 @@ public class SystemReportActivity extends Activity {
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
+	}
+
+	
+	private void report() {
+		SystemReport.updateReport("Shields On", true);
+		SystemReport.updateReport("Phasers", "Stun");
+		String report = SystemReport.report().toBlockingObservable().first();
+		reportView.setText(report);
 	}
 }
