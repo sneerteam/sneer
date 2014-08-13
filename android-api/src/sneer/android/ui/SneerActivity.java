8f72d23778fd6439e754ac2d241965d0f9ce0f81
Klaus Wuestefeld <klauswuestefeld@gmail.com>
2014-08-12 21:25:41 -0300

    Minor

--- android-api/src/sneer/android/ui/SneerActivity.java
+++ android-api/src/sneer/android/ui/SneerActivity.java
@@ -42,16 +42,14 @@ public class SneerActivity extends Activity {
 		return Observable.combineLatest(Observable.timer(0, 1, TimeUnit.MINUTES), dates, new Func2<Long, Long, Long>() { @Override public Long call(Long tick, Long date) {
 			return date;
 		}})
-		.observeOn(AndroidSchedulers.mainThread()).subscribe(new Action1<Long>() { @Override public void call(Long obj) {
-			textView.setText(userFriendlyDateTime(obj.toString()));
+		.observeOn(AndroidSchedulers.mainThread()).subscribe(new Action1<Long>() { @Override public void call(Long timestamp) {
+			textView.setText(userFriendly(timestamp));
 		}});
 	}
 	
 	
-	public static String userFriendlyDateTime(CharSequence charSequence) {
-		PrettyTime pt = new PrettyTime();
-		long timestamp = Long.parseLong(charSequence.toString());
-		return pt.format(new Date(timestamp));
+	public static String userFriendly(Long timestamp) {
+		return new PrettyTime().format(new Date(timestamp));
 	}
 	
 	
