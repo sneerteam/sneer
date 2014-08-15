06fb8e7f5cb28f53cb75bed1d64328dc982801c0
Klaus Wuestefeld <klauswuestefeld@gmail.com>
2014-08-15 14:59:00 -0300

    SneerFactoryImpl created.

--- java-api/src/sneer/admin/impl/SneerFactoryImpl.java
+++ java-api/src/sneer/admin/impl/SneerFactoryImpl.java
@@ -1,5 +1,15 @@
 package sneer.admin.impl;
 
-public class SneerFactoryImpl {
+import java.io.*;
+
+import sneer.admin.*;
+import sneer.commons.exceptions.*;
+
+public class SneerFactoryImpl implements SneerFactory {
+
+	@Override
+	public SneerAdmin open(File secureFolder) throws FriendlyException {
+		throw new NotImplementedYet();
+	}
 
 }
