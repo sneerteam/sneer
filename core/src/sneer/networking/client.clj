738e7e1d4903b79e95e99391cfc606407dd4fb56
Rodrigo B. de Oliveira <rbo@acm.org>
2014-08-19 11:59:38 -0300

    code gardening

--- core/src/sneer/networking/client.clj
+++ core/src/sneer/networking/client.clj
@@ -10,40 +10,31 @@
 (defn start
 
   ([puk]
-    (start puk (dropping-chan 1) (dropping-chan 1)  "dynamic.sneer.me" 5555))
+     (start puk (dropping-chan 1) (dropping-chan 1) "dynamic.sneer.me" 5555))
 
   ([puk from-server to-server server-host server-port]
-    (let [packets-in (async/chan)
-          packets-out (async/chan)]
+     (let [packets-in (async/chan)
+           packets-out (async/chan)]
 
-      (async/thread
+       (async/thread
 
-        ; ensure no network activity takes place on caller thread to workaround android limitation
-        (let [server-addr (InetSocketAddress. server-host server-port)
-              udp-server (udp/serve-udp packets-in packets-out)
-              ping [server-addr {:intent :ping :from puk}]]
+         ; ensure no network activity takes place on caller thread to workaround android limitation
+         (let [server-addr (InetSocketAddress. server-host server-port)
+               udp-server (udp/serve-udp packets-in packets-out)
+               ping [server-addr {:intent :ping :from puk}]]
 
-          ; server ping loop
-          (async/go-loop []
-            (when  (>! packets-out ping)
-              (<! (async/timeout 20000))
-              (recur)))
+           ; server ping loop
+           (async/go-loop []
+             (when (>! packets-out ping)
+               (<! (async/timeout 20000))
+               (recur)))
 
-          ; just report received packets for now
-          (async/go-loop []
-            (when-let [packet (<! packets-in)]
-              (SystemReport/updateReport "packet" packet)
-              (when-let [payload (-> packet second :payload)]
-                (>! from-server payload))
-              (recur)))
-
-          (async/pipe
-           (async/map
-            (fn [payload] [server-addr {:intent :send :from puk :to (:address payload) :payload payload}])
-            [to-server])
-           packets-out)))
-
-      {:packets-in packets-in :packets-out packets-out :from-server from-server :to-server to-server})))
+           (async/go-loop []
+             (when-let [packet (<! packets-in)]
+               (SystemReport/updateReport "packet" packet)
+               (when-let [payload (-> packet second :payload)]
+                 (>! from-server payload))
+               (recur))))
 
 (defn stop [client]
   (async/close! (:packets-out client)))
