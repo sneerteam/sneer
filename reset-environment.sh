7a68393cb35be10def0582264844d54594a2ca65
Fabio Roger Manera <fabioroger@gmail.com>
2014-08-20 21:47:03 +0100

    revert project dependency to use project references instead of maven

--- reset-environment.sh
+++ reset-environment.sh
@@ -6,11 +6,10 @@ git reset --hard
 
 git pull --rebase
 
-# rm -rf ~/.m2/repository/me/sneer
-
-./gradlew cleanEclipse eclipse
+./gradlew eclipse
 
 if [ -d "../rockpaperscissors" ]; then
   cp -f android-api/bin/*.jar ../rockpaperscissors/libs/
 fi
+
 ./gradlew install
