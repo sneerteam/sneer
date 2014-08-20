3515fcf24a5d1471b0de087b1a6c9cb50619a841
Rodrigo B. de Oliveira <rbo@acm.org>
2014-08-20 18:21:10 -0300

    don't pretend to be type safe

--- java-api/src/sneer/commons/Arrays.java
+++ java-api/src/sneer/commons/Arrays.java
@@ -4,12 +4,11 @@ import rx.functions.*;
 
 public class Arrays {
 
-	@SuppressWarnings("unchecked")
-	public static <T, R> R[] map(T[] source, Func1<T, R> converter) {		
+	public static <T, R> Object[] map(T[] source, Func1<T, R> converter) {
 		Object[] r = new Object[source.length];
 		for (int i = 0; i < r.length; i++)
 			r[i] = converter.call(source[i]);
-		return (R[]) r;
+		return r;
 	}
 
 }
