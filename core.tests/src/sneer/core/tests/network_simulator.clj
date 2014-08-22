fae53df7097ad22724069fd2e58000bc26c36b13
Rodrigo B. de Oliveira <rbo@acm.org>
2014-08-22 17:45:37 -0300

    extend Network protocol to rx.subjects.Subject

--- core.tests/src/sneer/core/tests/network_simulator.clj
+++ core.tests/src/sneer/core/tests/network_simulator.clj
@@ -2,14 +2,14 @@
   (:require [rx.lang.clojure.core :as rx]
             [sneer.core :as core]
             [sneer.rx :refer [subject*]])
-  (:import [rx.subjects PublishSubject]))
+  (:import [rx.subjects Subject PublishSubject]))
 
 (defn addressed-to [puk]
   (fn [envelope]
     (let [destination-address (:address envelope)]
       (= destination-address puk))))
 
-(extend-type PublishSubject
+(extend-type Subject
   core/Network
   (connect [network own-puk]
     (let [envelopes-for-me (rx/filter (addressed-to own-puk) network)]
