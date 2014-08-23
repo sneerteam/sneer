54cd123928c5d87d0b596e306259c9f01423139a
Rodrigo B. de Oliveira <rbo@acm.org>
2014-08-23 15:24:09 -0300

    don't reset env when there are local changes

--- reset-environment.sh
+++ reset-environment.sh
@@ -2,15 +2,12 @@ echo "-------------------> Resetting workspace and pulling changes"
 
 hash_before_pull=`git hash-object $0`
 
-#Delete all untracked files and directories (-d), even ignored ones (-x).
-git clean -x -d --force --quiet
-
-#Revert all modified files.
-git reset --hard
-
-git pull --rebase
+git pull --rebase || exit -1
 
 hash_after_pull=`git hash-object $0`
+
+#Delete all untracked files and directories (-d), even ignored ones (-x).
+git clean -x -d --force --quiet
 
 if [ "$hash_before_pull" != "$hash_after_pull" ]; then
 	echo "-------------------> This script changed, rerunning it"
