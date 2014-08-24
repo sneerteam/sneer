3274c7285916f261ba91460c53d53e1f8aec8df3
Klaus Wuestefeld <klauswuestefeld@gmail.com>
2014-08-24 18:51:54 -0300

    Lists.join(list, separator)

--- java-api/src/sneer/commons/Lists.java
+++ java-api/src/sneer/commons/Lists.java
@@ -4,6 +4,15 @@ import java.util.*;
 
 public class Lists {
 
+	public static String join(List<?> list, String separator) {
+		StringBuilder ret = new StringBuilder();
+		for (Object element : list) {
+			if (ret.length() > 0) ret.append(separator);
+			ret.append(element);
+		}
+		return ret.toString();
+	}
+	
 	public static <T> T lastIn(List<T> list) {
 		return list.isEmpty() ? null : list.get(list.size() - 1);
 	}
