4e26c1547013b4d805dc0b254c7acb4bc15be0d8
Klaus Wuestefeld <klauswuestefeld@gmail.com>
2014-08-24 19:46:13 -0300

    PubSubOverSqliteAndroid test

---
+++ android/android.main.tests/src/sneer/core/tests/PubSubOverSqliteAndroid.java
@@ -0,0 +1,21 @@
+package sneer.core.tests;
+
+import java.io.*;
+
+import junit.framework.*;
+import rx.functions.*;
+import sneer.android.main.core.*;
+
+public class PubSubOverSqliteAndroid extends TestCase { //PubSubOverPersistentTupleSpace {
+
+	public void testMessagePassing() throws IOException {
+//		new PubSubOverPersistentTupleSpace(new Func0<Object>() {  @Override public Object call() {
+//			try {
+//				return SneerSqliteDatabase.tmpTupleBase();
+//			} catch (IOException e) {
+//				throw new IllegalStateException(e);
+//			}
+//		}}).messagePassing();
+	}
+	
+}
