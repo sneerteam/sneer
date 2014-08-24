efeb35e20c12e7338f885a9bc5300f1eb2cd4825
Klaus Wuestefeld <klauswuestefeld@gmail.com>
2014-08-24 19:45:14 -0300

    PubSubOverPersistentTupleSpace receives a TupleBase factory.

--- core.tests/src/sneer/core/tests/PubSubOverPersistentTupleSpace.java
+++ core.tests/src/sneer/core/tests/PubSubOverPersistentTupleSpace.java
@@ -1,10 +1,24 @@
 package sneer.core.tests;
 
+import rx.functions.*;
+
 public class PubSubOverPersistentTupleSpace extends PubSubTest {
+	
+	private final Func0<Object> tupleBaseFactory;
+
+	PubSubOverPersistentTupleSpace(Func0<Object> tupleBaseFactory) {
+		this.tupleBaseFactory = tupleBaseFactory;
+	}
+	
+	public PubSubOverPersistentTupleSpace() {
+		this(new Func0<Object>() {  @Override public Object call() {
+			return Glue.var("sneer.core.tests.jdbc-tuple-base", "create").invoke();
+		}});
+	}
 	
 	@Override
 	protected Object newTupleBase() {
-		return Glue.var("sneer.core.tests.jdbc-tuple-base", "create").invoke();
+		return tupleBaseFactory.call();
 	}
 
 }
