1262ff02922bde5ad8ad5d8146d6ef28eeb5b1c0
Klaus Wuestefeld <klauswuestefeld@gmail.com>
2014-08-25 02:57:28 -0300

    This is how we use Keys. PublicKey.asBitcoinAddress() -> PublicKey.bytesAsString().

---
+++ java-api.tests/src/sneer/KeysTests.java
@@ -0,0 +1,47 @@
+package sneer;
+
+import static org.junit.Assert.*;
+
+import org.junit.*;
+
+import sneer.impl.keys.*;
+
+public class KeysTests {
+	
+	
+	@Test
+	public void recreate() {
+		PrivateKey prik = Keys.createPrivateKey();
+		assertEquals(prik, Keys.createPrivateKey(prik.bytes()));
+		assertEquals(prik, Keys.createPrivateKey(prik.bytesAsString()));
+		
+		PublicKey puk = prik.publicKey();
+		assertEquals(puk, Keys.createPublicKey(puk.bytes()));
+		assertEquals(puk, Keys.createPublicKey(puk.bytesAsString()));
+	}
+	
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
