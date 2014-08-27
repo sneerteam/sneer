f3d2f23eeeb6f0b4c4a05fd667b77ea89c8b0dc7
Fabio Roger Manera <fabioroger@gmail.com>
2014-08-27 15:08:37 +0100

    sqlite jdbc version extends Disposable protocol

--- core.tests/src/sneer/core/tests/jdbc_tuple_base.clj
+++ core.tests/src/sneer/core/tests/jdbc_tuple_base.clj
@@ -1,12 +1,14 @@
 (ns sneer.core.tests.jdbc-tuple-base
   (:require [clojure.java.jdbc :as sql]
-            [sneer.persistent-tuple-base :as tuple-base])
+            [sneer.persistent-tuple-base :as tuple-base]
+            [sneer.core :as core])
   (:import [java.sql DriverManager]))
 
 (defn create-sqlite-db [databaseFile]
   (let [connection (DriverManager/getConnection (if databaseFile (str "jdbc:sqlite:" (.getAbsolutePath databaseFile)) "jdbc:sqlite::memory:"))
         db {:connection connection}]
-  (reify tuple-base/Database
+  (reify 
+    tuple-base/Database
     (db-create-table [this table columns]
       (let [tuple-ddl (apply sql/create-table-ddl table columns)]
          (sql/execute! db [tuple-ddl])))
@@ -15,7 +17,11 @@
       (sql/insert! db table row))
     
     (db-query [this sql-and-params]
-      (sql/query db sql-and-params :result-set-fn doall :as-arrays? true)))))
+      (sql/query db sql-and-params :result-set-fn doall :as-arrays? true))
+
+    core/Disposable
+    (dispose [this]
+      (.close connection)))))
 
 (defn create [& [databaseFile]]
   (let [db (create-sqlite-db databaseFile)]
