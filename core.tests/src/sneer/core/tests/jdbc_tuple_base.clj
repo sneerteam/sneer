d0a93d34b06bbd55573bae1991fe6530c858eb7c
Fabio Roger Manera <fabioroger@gmail.com>
2014-08-27 14:51:59 +0100

    add file support for jdbc sqlite impl

--- core.tests/src/sneer/core/tests/jdbc_tuple_base.clj
+++ core.tests/src/sneer/core/tests/jdbc_tuple_base.clj
@@ -3,20 +3,22 @@
             [sneer.persistent-tuple-base :as tuple-base])
   (:import [java.sql DriverManager]))
 
-(defn create-sqlite-db [db]
+(defn create-sqlite-db [databaseFile]
+  (let [connection (DriverManager/getConnection (if databaseFile (str "jdbc:sqlite:" (.getAbsolutePath databaseFile)) "jdbc:sqlite::memory:"))
+        db {:connection connection}]
   (reify tuple-base/Database
     (db-create-table [this table columns]
       (let [tuple-ddl (apply sql/create-table-ddl table columns)]
-        (sql/execute! db [tuple-ddl])))
+         (sql/execute! db [tuple-ddl])))
     
     (db-insert [this table row]
       (sql/insert! db table row))
     
     (db-query [this sql-and-params]
-      (sql/query db sql-and-params :result-set-fn doall :as-arrays? true))))
+      (sql/query db sql-and-params :result-set-fn doall :as-arrays? true)))))
 
-(defn create []
-  (let [connection (DriverManager/getConnection "jdbc:sqlite::memory:")
-        db (create-sqlite-db {:connection connection})]
+(defn create [& [databaseFile]]
+  (let [db (create-sqlite-db databaseFile)]
     (tuple-base/create-tuple-table db)
     (tuple-base/create db)))
+
