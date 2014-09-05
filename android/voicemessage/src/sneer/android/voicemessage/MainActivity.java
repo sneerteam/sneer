f3ed0aeac5862170efbcabd8d70172f735191f95
Felipe Bueno <bueno.felipe@gmail.com>
2014-09-05 11:14:46 -0300

    Voice Message ui

--- android/voicemessage/src/sneer/android/voicemessage/MainActivity.java
+++ android/voicemessage/src/sneer/android/voicemessage/MainActivity.java
@@ -3,40 +3,46 @@ package sneer.android.voicemessage;
 import java.io.*;
 
 import android.app.*;
-import android.content.*;
 import android.media.*;
 import android.os.*;
 import android.util.*;
 import android.view.*;
+import android.view.View.OnClickListener;
 import android.widget.*;
 
 
 public class MainActivity extends Activity {
 
-	private static final String LOG_TAG = "AudioRecordTest";
-    private static String mFileName = null;
+	static final String LOG_TAG = "----> Sneer VoiceMessage";
+    static String mFileName = null;
 
-    private RecordButton mRecordButton = null;
-    private MediaRecorder mRecorder = null;
+    ImageButton mRecordButton;
+    Button mPlayButton;
+    TextView messageView;
+    TextView lengthView;
 
-    private PlayButton   mPlayButton = null;
-    private MediaPlayer   mPlayer = null;
+    MediaRecorder mRecorder = null;
+    MediaPlayer mPlayer = null;
+
+    boolean mStartRecording = true;
+    boolean mStartPlaying = true;
 
     private void onRecord(boolean start) {
         if (start) {
-            startRecording();
+        	startRecording();
         } else {
-            stopRecording();
+        	stopRecording();
         }
     }
 
-    private void onPlay(boolean start) {
-        if (start) {
+    
+	private void onPlay(boolean start) {
+        if (start)
             startPlaying();
-        } else {
+        else
             stopPlaying();
-        }
     }
+    
 
     private void startPlaying() {
         mPlayer = new MediaPlayer();
@@ -48,6 +54,7 @@ public class MainActivity extends Activity {
             Log.e(LOG_TAG, "prepare() failed");
         }
     }
+    
 
     private void stopPlaying() {
         mPlayer.release();
@@ -69,6 +76,7 @@ public class MainActivity extends Activity {
 
         mRecorder.start();
     }
+    
 
     private void stopRecording() {
         mRecorder.stop();
@@ -76,49 +84,6 @@ public class MainActivity extends Activity {
         mRecorder = null;
     }
 
-    class RecordButton extends Button {
-        boolean mStartRecording = true;
-
-        OnClickListener clicker = new OnClickListener() {
-            public void onClick(View v) {
-                onRecord(mStartRecording);
-                if (mStartRecording) {
-                    setText("Stop recording");
-                } else {
-                    setText("Start recording");
-                }
-                mStartRecording = !mStartRecording;
-            }
-        };
-
-        public RecordButton(Context ctx) {
-            super(ctx);
-            setText("Start recording");
-            setOnClickListener(clicker);
-        }
-    }
-
-    class PlayButton extends Button {
-        boolean mStartPlaying = true;
-
-        OnClickListener clicker = new OnClickListener() {
-            public void onClick(View v) {
-                onPlay(mStartPlaying);
-                if (mStartPlaying) {
-                    setText("Stop playing");
-                } else {
-                    setText("Start playing");
-                }
-                mStartPlaying = !mStartPlaying;
-            }
-        };
-
-        public PlayButton(Context ctx) {
-            super(ctx);
-            setText("Start playing");
-            setOnClickListener(clicker);
-        }
-    }
 
     public MainActivity() {
         mFileName = Environment.getExternalStorageDirectory().getAbsolutePath();
@@ -128,21 +93,32 @@ public class MainActivity extends Activity {
     @Override
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
+        setContentView(R.layout.activity_main);
+        
+        messageView = (TextView)findViewById(R.id.lblMessage);
+        lengthView = (TextView)findViewById(R.id.lblLength);
 
-        LinearLayout ll = new LinearLayout(this);
-        mRecordButton = new RecordButton(this);
-        ll.addView(mRecordButton,
-            new LinearLayout.LayoutParams(
-                ViewGroup.LayoutParams.WRAP_CONTENT,
-                ViewGroup.LayoutParams.WRAP_CONTENT,
-                0));
-        mPlayButton = new PlayButton(this);
-        ll.addView(mPlayButton,
-            new LinearLayout.LayoutParams(
-                ViewGroup.LayoutParams.WRAP_CONTENT,
-                ViewGroup.LayoutParams.WRAP_CONTENT,
-                0));
-        setContentView(ll);
+        mRecordButton = (ImageButton)findViewById(R.id.mRecordButton);
+        mRecordButton.setOnClickListener(new OnClickListener() { @Override public void onClick(View v) {
+			onRecord(mStartRecording);
+			if (mStartRecording) {
+				messageView.setText("Recording...");
+			} else {
+				messageView.setText("Hold to record, release to send");
+			}
+			mStartRecording = !mStartRecording;
+		}});
+        
+        mPlayButton = (Button)findViewById(R.id.mPlayButton);
+        mPlayButton.setOnClickListener(new OnClickListener() { @Override public void onClick(View v) {
+        	onPlay(mStartPlaying);
+            if (mStartPlaying)
+                ((Button)v).setText("Stop playing");
+            else
+            	((Button)v).setText("Start playing");
+
+            mStartPlaying = !mStartPlaying;
+		}});
     }
 
     @Override
