2e509cd64ee61d3dfc3ce8bb63b4ff0122fa166a
Klaus Wuestefeld <klauswuestefeld@gmail.com>
2014-09-09 20:36:28 -0300

    Minor

--- android/apps/voicemessage/src/sneer/android/voicemessage/VoiceMessageOpenActivity.java
+++ android/apps/voicemessage/src/sneer/android/voicemessage/VoiceMessageOpenActivity.java
@@ -2,8 +2,6 @@ package sneer.android.voicemessage;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 
 public class VoiceMessageOpenActivity extends Activity {
 
