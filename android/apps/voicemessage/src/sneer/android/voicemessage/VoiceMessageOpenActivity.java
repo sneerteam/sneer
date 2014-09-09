39607e96276950f8dce123e14c18b664785f3bce
Felipe Bueno <bueno.felipe@gmail.com>
2014-09-09 10:07:20 -0300

    Add VoiceMessageOpenActivity and update VoiceMessageActivity title

---
+++ android/apps/voicemessage/src/sneer/android/voicemessage/VoiceMessageOpenActivity.java
@@ -0,0 +1,15 @@
+package sneer.android.voicemessage;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class VoiceMessageOpenActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_voice_message_open);
+	}
+}
