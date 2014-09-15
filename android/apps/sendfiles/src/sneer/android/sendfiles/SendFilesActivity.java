e462976e2bf3ef228ad382f7ca6aa8924e279b16
Felipe Bueno <bueno.felipe@gmail.com>
2014-09-15 11:28:26 -0300

    sendfiles: add pick files from filesystem (wip)

--- android/apps/sendfiles/src/sneer/android/sendfiles/SendFilesActivity.java
+++ android/apps/sendfiles/src/sneer/android/sendfiles/SendFilesActivity.java
@@ -1,13 +1,49 @@
 package sneer.android.sendfiles;
 
 import sneer.android.ui.MessageActivity;
+import android.content.Intent;
 import android.os.Bundle;
 
 public class SendFilesActivity extends MessageActivity {
 
+	private static final int PICKFILE_RESULT_CODE = 0;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_send_files);
+
+		if (message() != null) {
+			open(message());
+		} else {
+			composeMessage();
+		}
+	}
+
+	private void composeMessage() {
+		Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+		intent.setType("file/*");
+		startActivityForResult(intent, PICKFILE_RESULT_CODE);
+
+	}
+
+	private void open(Object message) {
+		navigateTo(ViewSendFilesActivity.class);
+	}
+
+	@Override
+	public void onPause() {
+		super.onPause();
+		finish();
+	}
+
+	@Override
+	protected void onStop() {
+		super.onStop();
+		finish();
+	}
+
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
 	}
 }
