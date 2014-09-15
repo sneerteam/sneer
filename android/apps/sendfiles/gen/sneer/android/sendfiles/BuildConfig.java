29a6e442baf32305227b67a8df62e57f5914264b
Felipe Bueno <bueno.felipe@gmail.com>
2014-09-15 10:22:42 -0300

    sendfiles: New SneerApp (sendfiles)

---
+++ android/apps/sendfiles/gen/sneer/android/sendfiles/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package sneer.android.sendfiles;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ android/apps/sendfiles/src/sneer/android/sendfiles/SendFilesActivity.java
@@ -0,0 +1,15 @@
+package sneer.android.sendfiles;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class SendFilesActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_send_files);
+	}
+}
