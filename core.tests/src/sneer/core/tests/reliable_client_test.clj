296280a8c8e8bcc7295d0f8d565c537f0af6a190
Klaus Wuestefeld <klauswuestefeld@gmail.com>
2014-09-16 15:02:39 -0300

    Progress

--- core.tests/src/sneer/core/tests/reliable_client_test.clj
+++ core.tests/src/sneer/core/tests/reliable_client_test.clj
@@ -2,19 +2,24 @@
   (:require [midje.sweet :refer :all]))
 
 (defn create []
-  clojure.lang.PersistentQueue/EMPTY)
+  {:sequence 0
+   :payloads clojure.lang.PersistentQueue/EMPTY})
 
 (defn enqueue-to-send [payload state]
-  (conj state {:payload payload :sequence (count state)}))
+  (update-in state [:payloads] conj payload))
 
 (defn handle-packet-from-server [packet state]
   state)
 
 (defn peek-packet [state]
-  (first state))
+  (when-some [payload (-> state :payloads first)]
+    {:sequence (state :sequence)
+     :payload payload}))
 
 (defn pop-packet [state]
-  (pop state))
+  (-> state
+    (update-in [:payloads] pop)
+    (update-in [:sequence] inc)))
 
 
 (facts
@@ -26,5 +31,9 @@
   (fact "Packet enqueing is FIFO."
     (let [queue (->> (create) (enqueue-to-send :foo) (enqueue-to-send :bar))]
       (->> queue peek-packet) => {:sequence 0 :payload :foo}
-      (->> queue pop-packet peek-packet) => {:sequence 1 :payload :bar})))
+      (->> queue pop-packet peek-packet) => {:sequence 1 :payload :bar}))
+  
+  (fact "Every new packet gets a new sequence number."
+  (let [queue (->> (create) (enqueue-to-send :foo))]
+    (->> queue pop-packet (enqueue-to-send :bar) peek-packet :sequence) => 1)))
 
