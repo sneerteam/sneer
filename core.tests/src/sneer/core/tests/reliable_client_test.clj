33b7e92fba3c91176f97fb30588490fbc1195627
Klaus Wuestefeld <klauswuestefeld@gmail.com>
2014-09-16 14:19:17 -0300

    Reliable client progress.

--- core.tests/src/sneer/core/tests/reliable_client_test.clj
+++ core.tests/src/sneer/core/tests/reliable_client_test.clj
@@ -2,20 +2,31 @@
   (:require [midje.sweet :refer :all]))
 
 (defn create []
-  {})
+  clojure.lang.PersistentQueue/EMPTY)
 
 (defn enqueue-to-send [packet state]
-  [state])
+  (conj state packet))
 
 (defn handle-packet [packet state]
-  [state])
+  state)
 
-(defn next-packet-to-send [state]
-  ;[packet])
-  )
+(defn peek-packet-to-send [state]
+  (first state))
+
+(defn pop-packet-to-send [state]
+  (pop state))
+
 
 (facts
  "New Queues"
 
   (fact "A new queue has no packet to send."
-       (-> create next-packet-to-send) => nil))
+    (-> (create) next-packet-to-send) => nil)
+
+  (fact "First packet enqueue is first to be sent."
+    (let [queue (create)]
+      (->> queue (enqueue-to-send :foo) (peek-packet-to-send)) => :foo))
+
+  (fact "First packet enqueue is first to be sent."
+    (let [queue (create)]
+      (->> queue (enqueue-to-send :foo) pop-packet-to-send peek-packet-to-send) => nil)))
