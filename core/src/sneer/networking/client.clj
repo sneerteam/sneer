993055711259fc2e71702cd1328726b546cf2563
Klaus Wuestefeld <klauswuestefeld@gmail.com>
2014-09-18 15:29:51 -0300

    Minor

--- core/src/sneer/networking/client.clj
+++ core/src/sneer/networking/client.clj
@@ -33,39 +33,39 @@
 
 (defn start
   ([puk from-server to-server server-host server-port]
-     (let [packets-in (async/chan)
-           packets-out (async/chan)]
+     (let [udp-in (async/chan)
+           udp-out (async/chan)]
 
        (async/thread
 
          ; ensure no network activity takes place on caller thread to workaround android limitation
          (let [server-addr (InetSocketAddress. server-host server-port)
-               udp-server (udp/serve-udp packets-in packets-out)
+               udp-server (udp/serve-udp udp-in udp-out)
                ping [server-addr {:intent :ping :from puk}]]
 
            ; server ping loop
            (async/go-loop []
-             (when (>! packets-out ping)
+             (when (>! udp-out ping)
                (<! (async/timeout 20000))
                (recur)))
 
            (async/go-loop []
-             (when-let [packet (<! packets-in)]
+             (when-let [packet (<! udp-in)]
                (SystemReport/updateReport "packet" packet)
                (when-let [payload (-> packet second :payload)]
                  (>! from-server payload))
                (recur)))
 
            (async/pipe
-            (async/map
-             (fn [payload] [server-addr {:intent :send :from puk :to (:address payload) :payload payload}])
-             [to-server])
-            packets-out)))
+             (async/map
+               (fn [payload] [server-addr {:intent :send :from puk :to (:address payload) :payload payload}])
+               [to-server])
+             udp-out)))
 
-       {:packets-in packets-in :packets-out packets-out :from-server from-server :to-server to-server})))
+       {:udp-out udp-out :from-server from-server :to-server to-server})))
 
 (defn stop [client]
-  (async/close! (:packets-out client)))
+  (async/close! (:udp-out client)))
 
 (defn create-connection [puk from-server to-server host port]
   (start puk from-server to-server host port)
