9a56cde1b2e64a473a8874150eac711345bc5ef2
Klaus Wuestefeld <klauswuestefeld@gmail.com>
2014-09-22 19:19:07 -0300

    Minor

--- java-api/src/sneer/Party.java
+++ java-api/src/sneer/Party.java
@@ -1,14 +1,13 @@
 package sneer;
 
-import rx.*;
-import sneer.rx.*;
+import rx.Observable;
+import sneer.rx.Observed;
 
 /** An individual or a group. */
 public interface Party {
 
 	Observed<PublicKey> publicKey();
 	
-	@Deprecated //Change return type to Observed.
 	Observable<String> name();
 
 }
