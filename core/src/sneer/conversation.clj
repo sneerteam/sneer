55126b889712d7c4c36929043697583ec055391c
Klaus Wuestefeld <klauswuestefeld@gmail.com>
2014-09-23 00:57:21 -0300

    Core: party.clj extracted.

--- core/src/sneer/conversation.clj
+++ core/src/sneer/conversation.clj
@@ -1,91 +1,13 @@
 (ns sneer.conversation
   (:require
    [rx.lang.clojure.core :as rx]
-   [sneer.rx :refer [subject* observe-for-computation atom->observable flatmapseq shared-latest]]
-   [sneer.commons :refer [produce]])
+   [sneer.rx :refer [atom->observable]]
+   [sneer.party :refer [name-subject produce-party party-puk]])
   (:import
-   [sneer Sneer PrivateKey PublicKey Party Contact Profile Conversation Message]
-   [sneer.commons.exceptions FriendlyException]
+   [sneer PublicKey Party Contact Conversation Message]
    [sneer.rx ObservedSubject]
-   [sneer.tuples Tuple TupleSpace TuplePublisher TupleFilter]
-   [java.text SimpleDateFormat]
-   [java.util Date]
-   [rx.subjects Subject BehaviorSubject]))
-
-(defprotocol PartyImpl
-  (name-subject [this]))
-
-(defn new-party [puk]
-  (let [name (ObservedSubject/create (str "? PublicKey: " (-> puk .bytesAsString (subs 0 7)) "..."))]
-    (reify
-      Party
-        (name [this] (.observable name))
-        (publicKey [this]
-          (.observed (ObservedSubject/create puk)))
-        (toString [this]
-          (str "#<Party " puk ">"))
-      PartyImpl
-        (name-subject [this] name))))
-
-(defn party-puk [^Party party]
-  (.. party publicKey current))
-
-(defn produce-party [parties puk]
-  (produce parties puk new-party))
-
-(defn reify-profile [party ^TupleSpace tuple-space]
-
-  (letfn [(payloads-of [type]
-            (rx/map
-             (fn [^Tuple tuple] (.payload tuple))
-             (.. tuple-space
-                 filter
-                 (type type)
-                 (author (party-puk party))
-                 tuples)))
-
-          (payload-subject [tuple-type]
-            (let [latest (shared-latest (payloads-of tuple-type))
-                  publish #(.. tuple-space
-                               publisher
-                               (type tuple-type)
-                               (pub %))]
-              (subject*
-               latest
-               (reify rx.Observer
-                 (onNext [this value]
-                   (publish value))))))]
-
-    (let [^Subject preferred-nickname (payload-subject "profile/preferred-nickname")
-          ^Subject own-name (payload-subject "profile/own-name")
-          ^Subject selfie (payload-subject "profile/selfie")
-          ^Subject city (payload-subject "profile/city")
-          ^Subject country (payload-subject "profile/country")]
-
-      (reify Profile
-        (ownName [this]
-          (.asObservable own-name))
-        (setOwnName [this value]
-          (rx/on-next own-name value))
-        (selfie [this]
-          (.asObservable selfie))
-        (setSelfie [this value]
-          (rx/on-next selfie value))
-        (preferredNickname [this]
-          (.asObservable preferred-nickname))
-        (setPreferredNickname [this value]
-          (rx/on-next preferred-nickname value))
-        (city [this]
-          (.asObservable city))
-        (setCity [this value]
-          (rx/on-next city value))
-        (country [this]
-          (.asObservable country))
-        (setCountry [this value]
-          (rx/on-next country value))))))
-
-(defn produce-profile [tuple-space profiles party]
-  (produce profiles party #(reify-profile % tuple-space)))
+   [sneer.tuples Tuple TupleSpace]
+   [java.text SimpleDateFormat]))
 
 (defn reify-contact [nickname party]
   (let [nick-subject (ObservedSubject/create nickname)]
@@ -119,11 +41,11 @@
   (.. contact nickname current))
 
 (defn now []
-  (.getTime (java.util.Date.)))
+  (System/currentTimeMillis))
 
 (def simple-date-format (SimpleDateFormat. "HH:mm"))
 
-(defn format-date [time] (.format simple-date-format (Date. time)))
+(defn format-date [time] (.format simple-date-format (now)))
 
 (defn tuple->message [own-puk ^Tuple tuple]
   (let [created (now)
