8ce2d3ce0fff2b32ec67f5c2309499e00370a869
Klaus Wuestefeld <klauswuestefeld@gmail.com>
2014-10-03 17:43:55 -0300

    Minor

--- java-api/src/sneer/Message.java
+++ java-api/src/sneer/Message.java
@@ -1,18 +1,8 @@
 package sneer;
 
-import java.util.Comparator;
-
-import sneer.commons.Comparators;
 import sneer.tuples.Tuple;
 
 public interface Message {
-
-	public static final Comparator<Message> BY_TIME_RECEIVED = new Comparator<Message>() {
-		@Override
-		public int compare(Message e1, Message e2) {
-			return Comparators.compare(e1.timestampReceived(), e2.timestampReceived());
-		}
-	};
 
 	boolean isOwn();
 
