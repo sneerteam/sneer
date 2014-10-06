3847dd48b52de8ed097cb49ea6e8f3571e41d873
Felipe Bueno <bueno.felipe@gmail.com>
2014-10-06 14:18:13 -0300

    sendvideo: add ViewVideoMessageActivity

---
+++ android/apps/sendvideo/src/sneer/android/sendvideo/ViewVideoMessageActivity.java
@@ -0,0 +1,15 @@
+package sneer.android.sendvideo;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class ViewVideoMessageActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_view_video_message);
+	}
+}
