46f28c2218e25a1cd31d17fa708caa7f80deef6a
Felipe Bueno <bueno.felipe@gmail.com>
2014-10-06 10:11:46 -0300

    reset-env: rockpaperscissors -> lizard-spock

--- reset-environment.sh
+++ reset-environment.sh
@@ -28,6 +28,6 @@ rm android/apps/.project
 
 if [ -d "../rockpaperscissors" ]; then
 	echo COPYING NODEPS JAR...
-	rm -f ../rockpaperscissors/libs/sneer-android-api-nodeps-*.jar
-	cp -f android/android-api/build/libs/sneer-android-api-nodeps-*.jar ../rockpaperscissors/libs/
+	rm -f ../lizard-spock/libs/sneer-android-api-nodeps-*.jar
+	cp -f android/android-api/build/libs/sneer-android-api-nodeps-*.jar ../lizard-spock/libs/
 fi
