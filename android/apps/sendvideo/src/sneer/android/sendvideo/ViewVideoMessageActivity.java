a728dc13d4e12cedf91f7375b7fec34c41b5881f
Felipe Bueno <bueno.felipe@gmail.com>
2014-10-06 16:29:42 -0300

    videomessage: WIP add ViewVideoMessageActivity

--- android/apps/sendvideo/src/sneer/android/sendvideo/ViewVideoMessageActivity.java
+++ android/apps/sendvideo/src/sneer/android/sendvideo/ViewVideoMessageActivity.java
@@ -1,13 +1,48 @@
 package sneer.android.sendvideo;
 
-import android.app.Activity;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import sneer.android.ui.MessageActivity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.Toast;
 
-public class ViewVideoMessageActivity extends Activity {
+public class ViewVideoMessageActivity extends MessageActivity {
 
+	static final File TEMP_VIDEO = new File(System.getProperty("java.io.tmpdir"), "videomessage.mp4");
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_view_video_message);
+
+		TEMP_VIDEO.deleteOnExit();
+		
+		playVideoMessage();
 	}
+	
+	
+	private void playVideoMessage() {
+		byte[] message = (byte[])message();
+
+		try {
+			FileOutputStream fos = new FileOutputStream(TEMP_VIDEO);
+			fos.write(message);
+			fos.close();
+			
+			Uri videoUri = Uri.fromFile(TEMP_VIDEO);
+			
+			Intent intent = new Intent(Intent.ACTION_VIEW, videoUri);
+			intent.setDataAndType(videoUri, "video/mp4");
+			
+	    	startActivity(intent);
+		} catch (IOException e) {
+			Toast.makeText(this, e.getMessage(), Toast.LENGTH_LONG).show();
+		}
+		finish();
+	}
+	
 }
