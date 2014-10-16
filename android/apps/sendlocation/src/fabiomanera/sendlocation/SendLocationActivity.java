d78daaacd2128aebb6d40320f92f40de77422ccb
Felipe Bueno <bueno.felipe@gmail.com>
2014-10-16 16:40:50 -0300

    sendlocation: wip - action send almost ok

--- android/apps/sendlocation/src/fabiomanera/sendlocation/SendLocationActivity.java
+++ android/apps/sendlocation/src/fabiomanera/sendlocation/SendLocationActivity.java
@@ -6,51 +6,39 @@ import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
-import android.util.Log;
 
 public class SendLocationActivity extends MessageActivity implements LocationListener {
 
 	protected LocationManager locationManager;
-	@SuppressWarnings("unused")
-	private String longitude;
-	@SuppressWarnings("unused")
-	private String latitude;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		
 		locationManager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
 		locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
 	}
 	
 	@Override
-	protected void onPostCreate(Bundle savedInstanceState) {
-		super.onPostCreate(savedInstanceState);
-//		send("Location: ", "Latitude - " + latitude + "Longitude - " + longitude);
-//		finish();
-	}
-
-	@Override
 	public void onLocationChanged(Location location) {
-//		txtLat.setText("Latitude:" + location.getLatitude() + ", Longitude:" + location.getLongitude());
-		latitude = "" + location.getLatitude();
-		longitude = "" + location.getLongitude();
+		if (location != null) {
+			send("Location: ", "Latitude - " + location.getLatitude() + "Longitude - " + location.getLongitude());
+			finish();
+		}
 	}
 
 	@Override
-	public void onProviderDisabled(String arg0) {
-		Log.d("Latitude","disable");
-	}
+	public void onProviderDisabled(String arg0) {}
 
 	@Override
-	public void onProviderEnabled(String arg0) {
-		Log.d("Latitude","enable");
-	}
+	public void onProviderEnabled(String arg0) {}
 
 	@Override
-	public void onStatusChanged(String arg0, int arg1, Bundle arg2) {
-		Log.d("Latitude","----------> status");		
-	}
+	public void onStatusChanged(String arg0, int arg1, Bundle arg2) {}
 
+	@Override
+	protected void onPause() {
+		super.onPause();
+		locationManager.removeUpdates(this);
+	}
+	
 }
