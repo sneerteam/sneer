f2446522a6123d5c2a149f0eb2a54b9a8bd7d5c9
Klaus Wuestefeld <klauswuestefeld@gmail.com>
2014-10-17 13:40:40 -0300

    Minor

--- install-sneer-apps.sh
+++ install-sneer-apps.sh
@@ -1,6 +1,11 @@
 cd android &&
+
+#Remove android.main.tests stuff that is not working :(
 cp settings.gradle settings.gradle.bak &&
 grep -Ev "test" settings.gradle > temp &&
 mv temp settings.gradle &&
+
 ./gradlew installDebug &&
+
+#Recover original with android.main.tests stuff.
 mv settings.gradle.bak settings.gradle
