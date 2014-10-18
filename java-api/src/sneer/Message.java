cb5af4d7cee3dd29a023b56e88393b3320a6f898
Klaus Wuestefeld <klauswuestefeld@gmail.com>
2014-10-17 22:29:15 -0300

    Message: "content" uncomplected to Payload, Text and Label.

--- java-api/src/sneer/Message.java
+++ java-api/src/sneer/Message.java
@@ -6,14 +6,24 @@ public interface Message {
 
 	boolean isOwn();
 
-	Object content();
-
+	/** Can be null if this is a regular chat message. */
+	String messageType();
+	/** Text, if present, or url, if present, or messageType, if present, or empty string. Never null. */
+	String label();
+	/** Can be null. */
+	String text();
+	/** Not guaranteed to be a valid url. Can be null. */
+	String url();
+	/** Can be null. */
+	byte[] image();
+	/** Any parcelable object such as arrays, collections, number types, etc. Can be null. */
+	Object payload();
+	
 	/** When this message was created. */
 	long timestampCreated();
-
 	/** When this message was received. */
 	long timestampReceived();
-
+	/** Pretty representation of the time created. */
 	String timeCreated();
 	
 	Tuple tuple();
