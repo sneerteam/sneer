5eb25b779f49e71b50dcaeb2a4d3760eef6dd732
Rodrigo B. de Oliveira <rbo@acm.org>
2014-10-20 18:51:08 -0200

    [core] add midje testing infrastructure (works with both `lein midje' and `gradlew :core:test')

--- core/project.clj
+++ core/project.clj
@@ -4,7 +4,14 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/java.jdbc "0.3.4"]
-                 [org.xerial/sqlite-jdbc "3.7.2"]
+                 [org.clojure/core.match "0.2.1"]
+                 [org.clojure/core.async "0.1.303.0-886421-alpha"]
                  [com.cognitect/transit-clj "0.8.229"]
-                 [com.netflix.rxjava/rxjava-clojure "0.19.6"]])
+                 [midje "1.6.3"]
+                 [com.netflix.rxjava/rxjava-clojure "0.20.3"]
+                 [me.sneer/java-api "0.0.1"]
+                 [me.sneer/keys "0.0.1"]]
+  :plugins [[lein-midje "3.0.0"]]
+  :source-paths ["src/main/clojure"]
+  :java-source-paths ["src/main/java"]
+  :test-paths ["src/test/clojure"])
---
+++ core/src/test/clojure/sneer/tests/tuple_transmission_test.clj
@@ -0,0 +1,6 @@
+(ns sneer.tests.tuple-transmission-test
+  (:require [midje.sweet :refer :all]))
+
+(facts
+  "about tuple transmission"
+  (inc 1) => 2)
