71527f563590b2e5d34f0710b577f845476ee658
Klaus Wuestefeld <klauswuestefeld@gmail.com>
2014-10-21 18:38:29 -0200

    Minor

--- android/android.main/src/sneer/android/main/ui/MainActivity.java
+++ android/android.main/src/sneer/android/main/ui/MainActivity.java
@@ -21,7 +21,6 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.Window;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ListView;
