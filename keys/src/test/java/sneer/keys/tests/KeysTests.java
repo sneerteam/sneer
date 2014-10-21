3d6d7a1d65a56024d50a91aaa4f348f213927c03
Rodrigo B. de Oliveira <rbo@acm.org>
2014-10-21 10:40:59 -0200

    extract key api impl to its own module so core can become a mostly clojure project (with no runtime depenencies to java classes in the same project)

--- keys/src/test/java/sneer/keys/tests/KeysTests.java
+++ keys/src/test/java/sneer/keys/tests/KeysTests.java
@@ -1,4 +1,4 @@
-package sneer.core.tests;
+package sneer.keys.tests;
 
 import static org.junit.Assert.assertEquals;
 
@@ -9,43 +9,20 @@ import sneer.PublicKey;
 import sneer.impl.keys.KeysImpl;
 
 public class KeysTests {
-	
-	
+
+
 	@Test
 	public void recreate() {
 		KeysImpl subject = new KeysImpl();
-		
+
 		PrivateKey prik = subject.createPrivateKey();
 		assertEquals(prik, subject.createPrivateKey(prik.bytes()));
 		assertEquals(prik, subject.createPrivateKey(prik.bytesAsString()));
-		
+
 		PublicKey puk = prik.publicKey();
 		assertEquals(puk, subject.createPublicKey(puk.bytes()));
 		assertEquals(puk, subject.createPublicKey(puk.bytesAsString()));
 	}
-	
-	
+
+
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
