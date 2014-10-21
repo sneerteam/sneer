e2c91ef528e42577289236e354f4d333a3ed256e
Klaus Wuestefeld <klauswuestefeld@gmail.com>
2014-10-21 16:54:02 -0200

    Core: optimization to avoid unnecessary swap function calls.

--- core/src/sneer/commons.clj
+++ core/src/sneer/commons.clj
@@ -1,9 +1,12 @@
 (ns sneer.commons)
 
 (defn produce! [fn-if-absent map-atom key]
-  (get
-    (swap! map-atom update-in [key]
-           (fn [existing] (if (some? existing) existing (fn-if-absent key))))
-    key))
+  (let [existing (get @map-atom key)]
+    (if (some? existing)
+      existing
+      (get
+        (swap! map-atom update-in [key]
+               (fn [existing2] (if (some? existing2) existing2 (fn-if-absent key))))
+        key))))
 
 (defn now [] (sneer.commons.Clock/now))
