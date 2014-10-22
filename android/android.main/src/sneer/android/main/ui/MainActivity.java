11a471e028802bec1d882ffb3e3cc52c110f1092
Felipe Bueno <bueno.felipe@gmail.com>
2014-10-22 15:33:28 -0200

    minor

--- android/android.main/src/sneer/android/main/ui/MainActivity.java
+++ android/android.main/src/sneer/android/main/ui/MainActivity.java
@@ -24,7 +24,6 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ListView;
-import android.widget.Toast;
 
 public class MainActivity extends SneerActivity {
 	
