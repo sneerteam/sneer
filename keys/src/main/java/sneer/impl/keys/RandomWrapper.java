ce62667ed9ee939477256e6d37856df0926ffefb
Klaus Wuestefeld <klauswuestefeld@gmail.com>
2014-10-23 17:53:19 -0200

    Core: New keys impl started.

---
+++ keys/src/main/java/sneer/impl/keys/RandomWrapper.java
@@ -0,0 +1,51 @@
+package sneer.impl.keys;
+
+import static sneer.commons.exceptions.Exceptions.check;
+
+import java.security.SecureRandom;
+
+/**
+ * Using SecureRandom instances provided by the java security mechanism is a
+ * bloody mess. There is no way of creating a new Secure random implementation
+ * with a given seed and specific algorithm.
+ */
+class RandomWrapper extends SecureRandom { private static final long serialVersionUID = 1L;
+
+	private final byte[] randomBytes;
+	private boolean wasUsed = false;
+
+	RandomWrapper(byte[] randomBytes) {
+		this.randomBytes = randomBytes;
+	}
+
+	@Override
+	public String getAlgorithm() {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public synchronized void setSeed(byte[] seed) {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public void setSeed(long seed) {
+		return; // ignore initial seed set by superclasses
+	}
+
+	@Override
+	public byte[] generateSeed(int numBytes) {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public synchronized void nextBytes(byte[] bytes) {
+		check(bytes.length == randomBytes.length);
+		check(!wasUsed);
+		wasUsed = true;
+
+		System.arraycopy(randomBytes, 0, bytes, 0, randomBytes.length);
+	}
+
+	
+}
