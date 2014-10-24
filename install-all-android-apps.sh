4e6f614738ee3cceccc4a0aeaa4af0b3eb38d0be
Felipe Bueno <bueno.felipe@gmail.com>
2014-10-24 11:43:45 -0200

    install-all-apps: add lizardspock

--- install-all-android-apps.sh
+++ install-all-android-apps.sh
@@ -1,11 +1,20 @@
 cd android &&
 
-#Remove android.main.tests stuff that is not working :(
+# Remove android.main.tests stuff that is not working :(
 cp settings.gradle settings.gradle.bak &&
 grep -Ev "test" settings.gradle > temp &&
 mv temp settings.gradle &&
 
+# Install everything \o/
 ./gradlew installDebug &&
 
-#Recover original with android.main.tests stuff.
+# Recover original with android.main.tests stuff.
 mv settings.gradle.bak settings.gradle
+
+cd -
+
+# Install lizardspock
+# It must be a directory in ../lizardspock
+cd ../lizardspock/ || exit -1
+echo "-------------------> Installing lizardspock"
+./gradlew installDebug
