d98c45607f6279dafa77bd5595d590b1780425d2
Klaus Wuestefeld <klauswuestefeld@gmail.com>
2014-10-29 18:40:29 -0200

    Base64 moved from crypto to core

---
+++ core/src/main/java/org/apache/commons/codec/binary/Base64.java
@@ -0,0 +1,23 @@
+package org.apache.commons.codec.binary;
+
+import java.nio.charset.Charset;
+
+import javax.xml.bind.DatatypeConverter;
+
+/** Replacement for apache.commons.codec.Base64 class used by transit-java. The original apache.commons.codec lib includes native libs and Android doesn't like that. :( */
+public class Base64 {
+	
+	private static final Charset US_ASCII = Charset.forName("US-ASCII");
+	
+
+	public static byte[] encodeBase64(byte[] bytes) {
+		String string = DatatypeConverter.printBase64Binary(bytes);
+		return string.getBytes(US_ASCII); //Optimize: avoid conversion to String.
+	}
+	
+	public static byte[] decodeBase64(byte[] bytes) {
+		String string = new String(bytes, US_ASCII);
+		return DatatypeConverter.parseBase64Binary(string); //Optimize: avoid conversion to String.
+	}
+
+}
