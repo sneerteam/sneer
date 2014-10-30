c582ae2072a62cb2c16e20323ade42add73e77f0
Felipe Bueno <bueno.felipe@gmail.com>
2014-10-30 11:56:46 -0200

    android-api.MessageActivity: add messageJpegImage()

--- android/android-api/src/sneer/android/ui/MessageActivity.java
+++ android/android-api/src/sneer/android/ui/MessageActivity.java
@@ -1,6 +1,7 @@
 package sneer.android.ui;
 
 import static sneer.SneerAndroidClient.PAYLOAD;
+import static sneer.SneerAndroidClient.JPEG_IMAGE;
 import static sneer.SneerAndroidClient.RESULT_RECEIVER;
 import static sneer.SneerAndroidClient.TEXT;
 import sneer.SneerAndroidClient;
@@ -13,18 +14,22 @@ public abstract class MessageActivity extends SneerActivity {
 	protected Object messagePayload() {
 		Intent intent = getIntent();
 		if (intent == null) return null;
+
 		Value envelope = (Value)intent.getParcelableExtra(PAYLOAD);
 		if (envelope == null) return null;
+
 		return envelope.get();
 	}
 
 
-	protected Object messageUrl() {
+	protected byte[] messageJpegImage() {
 		Intent intent = getIntent();
 		if (intent == null) return null;
-		Value envelope = (Value)intent.getParcelableExtra(PAYLOAD);
-		if (envelope == null) return null;
-		return envelope.get();
+
+		byte[] image = (byte[])intent.getByteArrayExtra(JPEG_IMAGE);
+		if (image == null) return null;
+
+		return image;
 	}
 	
 	
