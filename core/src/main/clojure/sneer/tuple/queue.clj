07a39cb69f367c2f1135feee23ed8d92724af2f2
Klaus Wuestefeld <klauswuestefeld@gmail.com>
2014-10-31 12:34:05 -0200

    Minor

--- core/src/main/clojure/sneer/tuple/queue.clj
+++ core/src/main/clojure/sneer/tuple/queue.clj
@@ -32,7 +32,10 @@
   (-empty? [store to])
   (-peek [store to])
   (-enqueue [store to tuple])
-  (-pop [store to]))
+  (-pop [store to])
+  (-reset [store to])
+  (-unreset [store to])
+  (-reset? [store to]))
 
 (def IMMEDIATELY (doto (async/chan)
                    async/close!))
@@ -42,43 +45,87 @@
 (defn new-retry-period []
   (async/timeout 3000))
 
+
+
 (defn start-queue-transmitter [from to store tuples-in packets-in packets-out]
   (letfn [(next-packet []
             (when-some [{:keys [sequence tuple]} (-peek store to)]
               {:intent :send :from from :to to :sequence sequence :payload tuple}))]
     (go-trace
       (loop [send-period IMMEDIATELY]
-       
-        (alt! :priority true
+        (when send-period
+          (alt! :priority true
          
-          send-period
-          ([_]
-            (if-some [packet (next-packet)]
-              (do
-                (>! packets-out packet)
-                (recur (new-retry-period)))
-              (recur NEVER)))
+            send-period
+            ([_]
+              (if-some [packet (next-packet)]
+                (do
+                  (>! packets-out packet)
+                  (recur (new-retry-period)))
+                (recur NEVER)))
        
-          tuples-in
-          ([tuple]
-            (when tuple
-              (let [first? (-empty? store to)]
-                (-enqueue store to tuple)
-                (recur (if first? IMMEDIATELY send-period)))))
+            tuples-in
+            ([tuple]
+              (when tuple
+                (let [first? (-empty? store to)]
+                  (-enqueue store to tuple)
+                  (recur (if first? IMMEDIATELY send-period)))))
        
-          packets-in
-          ([packet]
-             (match packet
-                    {:intent :status-of-queues :highest-sequence-to-send hsts}
-                    (if-some [{:keys [sequence]} (-peek store to)]
-                      (if (= hsts sequence)
-                        (do
-                          (-pop store to)
-                          (recur IMMEDIATELY))
-                        (do
-                          (>! packets-out (assoc (next-packet) :reset true))
-                          (recur (new-retry-period))))
-                      (recur NEVER))
-                    :else (recur send-period)))))
+            packets-in
+            ([packet]
+               (match packet
+                      {:intent :status-of-queues :highest-sequence-to-send hsts}
+                      (if-some [{:keys [sequence]} (-peek store to)]
+                        (if (= hsts sequence)
+                          (do
+                            (-pop store to)
+                            (recur IMMEDIATELY))
+                          (do
+                            (>! packets-out (assoc (next-packet) :reset true))
+                            (recur (new-retry-period))))
+                        (recur NEVER))
+                      :else (recur send-period))))))
+   
+     (async/close! packets-out))))
+
+
+(defn start-queue-transmitter2 [from to store tuples-in packets-in packets-out]
+  (letfn [(next-packet []
+            (when-some [{:keys [sequence tuple]} (-peek store to)]
+              {:intent :send :from from :to to :sequence sequence :payload tuple}))]
+    (go-trace
+      (loop [send-period IMMEDIATELY]
+        (when send-period
+          (alt! :priority true
+         
+            send-period
+            ([_]
+              (if-some [packet (next-packet)]
+                (do
+                  (>! packets-out packet)
+                  (recur (new-retry-period)))
+                (recur NEVER)))
+       
+            tuples-in
+            ([tuple]
+              (when tuple
+                (let [first? (-empty? store to)]
+                  (-enqueue store to tuple)
+                  (recur (if first? IMMEDIATELY send-period)))))
+       
+            packets-in
+            ([packet]
+               (match packet
+                      {:intent :status-of-queues :highest-sequence-to-send hsts}
+                      (if-some [{:keys [sequence]} (-peek store to)]
+                        (if (= hsts sequence)
+                          (do
+                            (-pop store to)
+                            (recur IMMEDIATELY))
+                          (do
+                            (>! packets-out (assoc (next-packet) :reset true))
+                            (recur (new-retry-period))))
+                        (recur NEVER))
+                      :else (recur send-period))))))
    
      (async/close! packets-out))))
