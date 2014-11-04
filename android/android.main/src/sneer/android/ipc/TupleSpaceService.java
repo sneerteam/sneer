fcfd4a3a352f96df9dc2f6926af1893d832616a2
Felipe Bueno <bueno.felipe@gmail.com>
2014-11-04 12:13:19 -0200

    minor

--- android/android.main/src/sneer/android/ipc/TupleSpaceService.java
+++ android/android.main/src/sneer/android/ipc/TupleSpaceService.java
@@ -27,25 +27,24 @@ import android.os.Parcelable;
 import android.os.ResultReceiver;
 
 public class TupleSpaceService extends Service {
-	
-	private AtomicInteger nextSubscriptionId = new AtomicInteger();
-	private ConcurrentMap<Integer, Subscription> subscriptions = new ConcurrentHashMap<Integer, Subscription>();
+
+	private final AtomicInteger nextSubscriptionId = new AtomicInteger();
+	private final ConcurrentMap<Integer, Subscription> subscriptions = new ConcurrentHashMap<Integer, Subscription>();
 
 	@SuppressWarnings("unused")
 	private String friendlyErrorMessage; //Return this to the callers. See how it is set, below.
-	
+
 	private final InteractiveSerializer serializer = new InteractiveSerializer();
-	
-	
+
+
 	@SuppressWarnings("unchecked")
 	@Override
 	public int onStartCommand(Intent intent, int flags, int startId) {
 		super.onStartCommand(intent, flags, startId);
-		
-		if (intent == null || !intent.hasExtra("op")) {
+
+		if (intent == null || !intent.hasExtra("op"))
 			return START_STICKY;
-		}
-		
+
 		switch(TupleSpaceFactoryClient.TupleSpaceOp.values()[intent.getIntExtra("op", -1)]) {
 		case PUBLISH:
 			publish((Map<String, Object>)serializer.deserialize(intent.getByteArrayExtra("tuple")));
@@ -62,68 +61,68 @@ public class TupleSpaceService extends Service {
 		default:
 			break;
 		}
-		
+
 		return START_STICKY;
-	}	
-	
-	
+	}
+
+
 	private void unsubscribe(int subscriptionId) {
 		if (subscriptionId == -1)
 			throw new IllegalArgumentException("Unknown subscription: " + subscriptionId);
 
 		Subscription s = subscriptions.remove(subscriptionId);
-		
+
 		if (s != null)
 			s.unsubscribe();
 	}
-	
-	
+
+
 	private void subscribe(Map<String, Object> criteria, ResultReceiver resultReceiver) {
 		subscribe(resultReceiver, sneer().tupleSpace().filter().putFields(criteria).tuples());
 	}
-	
+
 
 	private void subscribeLocal(Map<String, Object> criteria, ResultReceiver resultReceiver) {
 		subscribe(resultReceiver, sneer().tupleSpace().filter().putFields(criteria).localTuples());
 	}
 
-	
+
 	private void subscribe(final ResultReceiver resultReceiver, Observable<Tuple> tuples) {
 		int id = nextSubscriptionId.getAndIncrement();
 		resultReceiver.send(TupleSpaceFactoryClient.SubscriptionOp.SUBSCRIPTION_ID.ordinal(), bundle(id));
-		
+
 		Subscription s = tuples.doOnCompleted(new Action0() { @Override public void call() {
 			resultReceiver.send(ON_COMPLETED.ordinal(), new Bundle());
 		}}).subscribe(new Action1<Tuple>() { @Override public void call(Tuple t1) {
 			resultReceiver.send(ON_NEXT.ordinal(), bundle(serializer.serialize(new HashMap<String, Object>(t1))));
 		}});
-		
+
 		subscriptions.put(id, s);
 	}
 
-	
+
 	private Bundle bundle(Object value) {
 		Bundle ret = new Bundle();
 		if (value instanceof Parcelable)
 			ret.putParcelable("value", (Parcelable) value);
 		else
 			ret.putSerializable("value", (Serializable) value);
-		
+
 		return ret;
 	}
 
-	
+
 	private void publish(Map<String, Object> tuple) {
 		sneer().tupleSpace().publisher().putFields(tuple).pub();
 	}
 
-	
+
 	@Override
 	public IBinder onBind(Intent arg0) {
 		return null;
 	}
 
-	
+
 	public static void startTupleSpaceService(Context context) {
 		Intent startServiceIntent = new Intent(context, TupleSpaceService.class);
 	    context.startService(startServiceIntent);
