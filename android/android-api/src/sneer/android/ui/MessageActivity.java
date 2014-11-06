7554bbb4428ec9febd7105db0e1db8ed849a426f
Felipe Bueno <bueno.felipe@gmail.com>
2014-11-06 14:53:23 -0200

    android-api: inhibit sending images

--- android/android-api/src/sneer/android/ui/MessageActivity.java
+++ android/android-api/src/sneer/android/ui/MessageActivity.java
@@ -1,7 +1,7 @@
 package sneer.android.ui;
 
-import static sneer.SneerAndroidClient.PAYLOAD;
 import static sneer.SneerAndroidClient.JPEG_IMAGE;
+import static sneer.SneerAndroidClient.PAYLOAD;
 import static sneer.SneerAndroidClient.RESULT_RECEIVER;
 import static sneer.SneerAndroidClient.TEXT;
 import sneer.SneerAndroidClient;
@@ -26,21 +26,21 @@ public abstract class MessageActivity extends SneerActivity {
 		Intent intent = getIntent();
 		if (intent == null) return null;
 
-		byte[] image = (byte[])intent.getByteArrayExtra(JPEG_IMAGE);
+		byte[] image = intent.getByteArrayExtra(JPEG_IMAGE);
 		if (image == null) return null;
 
 		return image;
 	}
-	
-	
+
+
 	protected String messageText() {
 		return getIntent() == null ? null : getIntent().getStringExtra(TEXT);
 	}
 
-		
+
 	protected void send(String label, Object payload, byte[] jpegImage) {
 		ResultReceiver resultReceiver = getExtra(RESULT_RECEIVER);
-		SneerAndroidClient.send(resultReceiver, label, payload, jpegImage);
+		SneerAndroidClient.send(resultReceiver, label, payload, null);
 	}
-	
+
 }
