eef957a19dd2ddc3ddf75e9b9b9836eba02bc669
Felipe Bueno <bueno.felipe@gmail.com>
2014-11-10 11:31:02 -0200

    main.tests: run tests with ant

--- run-android-tests.sh
+++ run-android-tests.sh
@@ -1,16 +1,11 @@
 #! /bin/bash
 cd android/android.main.tests
 
-devices=`adb devices | grep 'device$' | cut -f1`
-pids=""
+export ANDROID_SERIAL=`adb devices | grep 'device$' | cut -f1 | head -n 1`
 
-for device in $devices
-do
-    echo "Running tests on device $device"
-    adb -s $device shell am instrument -w sneer.android.test/android.test.InstrumentationTestRunner
-done
+echo "Running tests on device $ANDROID_SERIAL"
+ant clean test
 
 echo "Finished running tests"
 
 cd -
-wait
