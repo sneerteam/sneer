25524eab3bf348951f3d7572a200add548e8c97e
Felipe Bueno <bueno.felipe@gmail.com>
2014-11-10 20:01:17 -0200

    minor

--- android/android.main/src/sneer/android/ui/ConversationAdapter.java
+++ android/android.main/src/sneer/android/ui/ConversationAdapter.java
@@ -32,26 +32,26 @@ import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 class ConversationAdapter extends ArrayAdapter<Message> implements OnClickListener{
-    
-	private int layoutUserResourceId;    
-    private int listContactResourceId;
-	private LayoutInflater inflater;
-	private Party party;
-    
+
+	private final int layoutUserResourceId;
+    private final int listContactResourceId;
+	private final LayoutInflater inflater;
+	private final Party party;
+
     ConversationAdapter(Context context,
     		LayoutInflater inflater,
     		int layoutUserResourceId,
     		int listContactResourceId,
-    		List<Message> data,
+    		List<Message> messages,
     		Party party) {
-        super(context, layoutUserResourceId, data);
+        super(context, layoutUserResourceId, messages);
 		this.inflater = inflater;
         this.layoutUserResourceId = layoutUserResourceId;
         this.listContactResourceId = listContactResourceId;
 		this.party = party;
     }
 
-    
+
 	@SuppressLint("ViewHolder") @Override
     public View getView(int position, View convertView, ViewGroup parent) {
 		final Message message = this.getItem(position);
@@ -59,19 +59,19 @@ class ConversationAdapter extends ArrayAdapter<Message> implements OnClickListen
         	message.isOwn() ? layoutUserResourceId : listContactResourceId,
         	parent,
         	false);
-        
+
         final TextView messageView = findTextView(ret, R.id.messageContent);
         final ImageView imageView = findImageView(ret, R.id.img);
-        
+
         SpannableString messageContent = new SpannableString(message.label());
-        byte[] messageImage = (byte[])message.jpegImage();
-        
+        byte[] messageImage = message.jpegImage();
+
         if (messageImage != null)
         	imageView.setImageBitmap(BitmapFactory.decodeByteArray(messageImage, 0, messageImage.length));
-        
+
         messageView.setAutoLinkMask(Linkify.ALL);
         findTextView(ret, R.id.messageTime).setText(message.timeCreated());
-        
+
         ret.setTag(message);
         if (sneerAndroid().isClickable(message)) {
         	messageView.setAutoLinkMask(0);
@@ -81,22 +81,21 @@ class ConversationAdapter extends ArrayAdapter<Message> implements OnClickListen
         }
 
         messageView.setText(messageContent);
-        
+
         party.name().subscribe(new Action1<String>() { @Override public void call(String sender) {
         	setColors(ret, sender, message.isOwn());
         }});
-        
+
        	return ret;
     }
 
-	
+
 	private void clickableMessageStyle(final TextView messageView, SpannableString messageContent) {
 		messageContent.setSpan(new UnderlineSpan(), 0, messageContent.length(), 0);
 		messageView.setTextColor(Color.rgb(064, 183, 228));
 	}
 
-	
-	@SuppressLint("NewApi")
+
 	private void setColors(View row, String sender, boolean own) {
 		final RelativeLayout speechBubble = (RelativeLayout)row.findViewById(R.id.speechBubble);
 		if (own) {
@@ -105,27 +104,27 @@ class ConversationAdapter extends ArrayAdapter<Message> implements OnClickListen
 		} else {
 			View speechBubbleArrowLeft = row.findViewById(R.id.speechBubbleArrowLeft);
 			speechBubbleArrowLeft.setBackground(new TriangleLeftDrawable(darkColorDeterminedBy(sender)));
-			
+
 			LayerDrawable bubbleLayer = (LayerDrawable) speechBubble.getBackground();
 			GradientDrawable bubbleBackground = (GradientDrawable) bubbleLayer.findDrawableByLayerId(R.id.bubbleBackground);
 			bubbleBackground.setColor(lightColorDeterminedBy(sender));
-			
+
 			GradientDrawable bubbleShadow = (GradientDrawable) bubbleLayer.findDrawableByLayerId(R.id.bubbleShadow);
 			bubbleShadow.setColor(darkColorDeterminedBy(sender));
 		}
 	}
 
-	
+
 	private static int darkColorDeterminedBy(String string) {
 		return colorDeterminedBy(string, 50);
 	}
-	
-	
+
+
 	private static int lightColorDeterminedBy(String string) {
 		return colorDeterminedBy(string, 170);
 	}
-	
-	
+
+
 	private static int colorDeterminedBy(String string, int strength) {
 		Random random = new Random(string.hashCode() * 713);
 		int r = strength + random.nextInt(86);
@@ -134,10 +133,10 @@ class ConversationAdapter extends ArrayAdapter<Message> implements OnClickListen
 		return Color.argb(255, r, g, b);
 	}
 
-	
+
 	@Override
 	public void onClick(View v) {
 		sneerAndroid().doOnClick((Message) v.getTag());
 	}
-	
+
 }
