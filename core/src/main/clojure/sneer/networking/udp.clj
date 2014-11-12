2505b9d07aeef003c5f5d11268a4c4c172046b51
Klaus Wuestefeld <klauswuestefeld@gmail.com>
2014-11-11 23:15:19 -0200

    Core: UDP: MTU of 1400 (anecdotal). Socket is closed when packets-out is closed.

--- core/src/main/clojure/sneer/networking/udp.clj
+++ core/src/main/clojure/sneer/networking/udp.clj
@@ -4,16 +4,17 @@
             [clojure.core.async :as async :refer [>! <! >!! <!!]])
   (:import [java.net DatagramPacket DatagramSocket SocketAddress InetAddress InetSocketAddress SocketException]))
 
+(def MTU 1400)  ; Anecdotal.
 
 (defn new-datagram []
-  (new DatagramPacket (byte-array 1024) 1024))
+  (new DatagramPacket (byte-array MTU) MTU))
 
 (defn data->value
   "Reads the encoded datagram and returns [socket-address value]"
   [^DatagramPacket datagram]
   (let [address (.getSocketAddress datagram)
         value (deserialize (.getData datagram) (.getLength datagram))]
-    (println address value)
+    #_(println address value)
     [address value]))
 
 (defn value->data
@@ -34,30 +35,37 @@
 (defn is-open [socket]
   (not (.isClosed ^DatagramSocket socket)))
 
+(defn- open-socket [port]
+  (if port
+    (DatagramSocket. ^int port)
+    (DatagramSocket.)))
+
 (defn start-udp-server
-  "Opens a UDP socket on port, putting received packets into packets-in, sending packets taken from packets-out.
+  "Opens a UDP socket on port, sending packets taken from packets-out and putting received packets into packets-in.
   Server will stop when packets-out is closed."
-  [packets-in packets-out & [port]]
+  [packets-out packets-in & [port]]
 
   (when port (println "Opening port" port))
 
-  (let [socket (if port (new DatagramSocket ^int port) (new DatagramSocket))
+  (let [socket (open-socket port)
         print-err-if-open #(when (is-open socket) (.printStackTrace ^Exception %))]
 
     (go-trace
       (with-open [^DatagramSocket socket ^DatagramSocket socket]
         (loop []
-          (when-let [packet (<! packets-out)]
-            (try
-              (println "<!" packet)
-              (send-value socket packet)
-              (catch Exception e (print-err-if-open e)))
-            (recur)))))
+          (if-let [packet (<! packets-out)]
+            (do
+              (try
+                #_(println "<!" packet)
+                (send-value socket packet)
+                (catch Exception e (print-err-if-open e)))
+              (recur))
+            (.close socket)))))
 
     (async/thread
       (while (is-open socket)
         (try
           (let [packet (receive-value socket)]
-            (println ">!!" packet)
+            #_(println ">!!" packet)
             (>!! packets-in packet))
           (catch Exception e (print-err-if-open e)))))))
--- core/src/test/clojure/sneer/networking/udp_test.clj
+++ core/src/test/clojure/sneer/networking/udp_test.clj
@@ -2,23 +2,26 @@
   (:require [midje.sweet :refer :all]
             [sneer.networking.udp :refer [start-udp-server]]
             [sneer.test-util :refer :all]
-            [clojure.core.async :as async :refer [chan]])
+            [clojure.core.async :as async :refer [chan >!! close!]])
   (:import [java.net InetSocketAddress]))
 
 ; (do (require 'midje.repl) (midje.repl/autotest))
 
-#_(facts
-  "UDP socket"
+(facts
+"UDP socket"
 
-   (let [echo-port 1024
-         echo (chan)
-         packets-out (chan 3)
-         packets-in (chan)
-         localhost (InetSocketAddress. "localhost" echo-port)]
+ (let [echo-port 1024
+       echo (chan)
+       packets-out (chan 3)
+       packets-in (chan)
+       localhost (InetSocketAddress. "localhost" echo-port)]
     
-     (start-udp-server echo echo echo-port)
-     (start-udp-server packets-out packets-in)
+   (start-udp-server echo echo echo-port)
+   (start-udp-server packets-out packets-in)
     
-     (fact "One value is echoed"
-       (>!! packets-out [localhost (.getBytes "Hello")])
-       (-> (<!!? packets-in) 1 String.) => "Hello")))
+   (fact "One value is echoed"
+     (>!! packets-out [localhost (.getBytes "Hello")])
+     (-> (<!!? packets-in) (get 1) String.) => "Hello")
+   
+   (close! echo)
+   (close! packets-out)))
