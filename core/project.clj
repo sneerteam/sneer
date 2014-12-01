f53f329d7fd339de048c4568120bfbc9be05862a
Felipe Bueno <bueno.felipe@gmail.com>
2014-12-01 10:33:03 -0200

    core: add cider-nrepl plugin

--- core/project.clj
+++ core/project.clj
@@ -11,6 +11,7 @@
                  [com.netflix.rxjava/rxjava-clojure "0.20.3"]
                  [me.sneer/java-api "0.0.1"]
                  [me.sneer/crypto "0.0.1"]]
+  :profiles {:dev {:plugins [[cider/cider-nrepl "0.8.1"]]}}
   :plugins [[lein-midje "3.0.0"]]
   :source-paths ["src/main/clojure"]
   :java-source-paths ["src/main/java"]
