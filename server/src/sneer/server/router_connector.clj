6949b2510052c91cd3e6fcf367206412ca90a9a3
Klaus Wuestefeld <klauswuestefeld@gmail.com>
2014-12-04 17:38:29 -0200

    Server: connector with resend timeouts.

--- server/src/sneer/server/router_connector.clj
+++ server/src/sneer/server/router_connector.clj
@@ -1,22 +1,40 @@
 (ns sneer.server.router-connector
   (:require
-    [clojure.core.async :as async :refer [to-chan chan close! go <!! >!! thread]]
+    [clojure.core.async :as async :refer [to-chan chan close! go <!! >!! thread alts!!]]
+    [clojure.core.async.impl.protocols :as impl]
     [clojure.core.match :refer [match]]
     [sneer.test-util :refer :all]
     [sneer.async :refer :all]
     [sneer.commons :refer [empty-queue loop-state]]
     [sneer.server.router :refer :all]))
 
+
+
+
+(defrecord NamedChannel [name channel]
+  impl/ReadPort
+  (take! [_ fn]
+    (impl/take! channel fn))
+  
+  impl/WritePort
+  (put! [_ value fn]
+    (impl/put! channel value fn))
+    
+  Object
+  (toString [_]
+    (str "Ch" name)))
+
+(defmethod print-method NamedChannel
+  [nc writer]
+  (.write writer (str nc)))
+
+
 (def online-count (constantly 20))
 
-(defn- next-packet-to-send [state]
-  (->>
-    state
-    :online-clients
-    (map (fn [[client {packet :pending-to-send}]]
-           (when packet (assoc packet :to client))))
-    (filter some?)
-    first))
+(defn- next-packet-to-send [online-clients send-round]
+  (let [client (peek send-round)]
+    (when-some [packet (get-in online-clients [client :pending-to-send])]
+      (assoc packet :to client))))
 
 (def tuple-signature (juxt :author :id))
 
@@ -26,27 +44,52 @@
       (let [router (-> state :router (pop-packet-for from))]
         (-> state
           (assoc :router router)
-          (update-in [:online-clients from :pending-to-send] (constantly (peek-packet-for router from)))))
+          (update-in [:online-clients from              :pending-to-send] (constantly (peek-packet-for router from)))
+          (update-in [:online-clients (:author pending) :pending-to-send] (constantly (peek-packet-for router (:author pending))))
+          ))
       state)))
 
+; [{:keys [packets-in packets-out router online-clients send-round resend-timer] :as state}]
 
-(defn- -iterate [{:keys [packets-in packets-out router online-clients send-round] :as state}]
-  (when-some [packet (next-packet-to-send state)]
-    (>!! packets-out packet))
+(defn- send-op [{:keys [packets-out online-clients send-round resend-timeout]}]
+  (if-some [packet (next-packet-to-send online-clients send-round)]
+    [packets-out packet]
+    resend-timeout))
 
-  (if-some [packet (<!! packets-in)]
+(defn- channel-ops [{:keys [packets-in] :as state}]
+  #_(println "Send-op: " (send-op state))
+  [(send-op state)
+   packets-in
+   #_offline-timer])
+
+(defn- ->named [resend-timeout-fn]
+  (NamedChannel. :resend-timeout (resend-timeout-fn)))
+
+(defn- send-round [online-clients]
+  (->>
+    online-clients
+    keys
+    (filter #(get-in online-clients [% :pending-to-send]))
+    (into empty-queue)))
+
+(defmulti handle (fn [_ _ channel] (:name channel)))
+
+(defmethod handle :packets-in [state packet _]
+  (if packet
     (when-some [from (:from packet)]
-      (let [state (update-in state [:online-clients from :online-countdown] online-count)
+      (let [router (:router state)
+            state (update-in state [:online-clients from :online-countdown] online-count)
             state (update-in state [:online-clients from :pending-to-send] #(if % % (peek-packet-for router from)))]
         (match packet
           {:send tuple :to to}
-          (if (queue-full? router from to)
-            (do
-              (>!! packets-out {:nak (:id tuple) :for to :to from})
-              state)
-            (do
-              (>!! packets-out {:ack (:id tuple) :for to :to from})
-              (update-in state [:router] enqueue! from to tuple)))
+          (let [packets-out (:packets-out state)]
+            (if (queue-full? router from to)
+              (do
+                (>!! packets-out {:nak (:id tuple) :for to :to from})
+                state)
+              (do
+                (>!! packets-out {:ack (:id tuple) :for to :to from})
+                (update-in state [:router] enqueue! from to tuple))))
 
           {:ack author :id id}
           (ack state from [author id])
@@ -55,12 +98,27 @@
           state)))
     :break))
 
-(defn start-connector [queue-size packets-in packets-out]
+(defmethod handle :packets-out [state packet _]
+  (update-in state [:send-round] pop))
+
+(defmethod handle :resend-timeout [{:keys [online-clients resend-timeout-fn]} _ _]
+  {:send-round (send-round online-clients)
+   :resend-timeout (->named resend-timeout-fn)})
+
+(defn- -iterate [state]
+  (let [chosen (alts!! (channel-ops state) :priority :true)]
+    #_(println "Chosen:" chosen)
+    (apply handle state
+      chosen)))
+
+(defn start-connector [queue-size packets-in packets-out resend-timeout-fn]
   (thread
     (loop-state -iterate
-                {:packets-in packets-in
-                 :packets-out packets-out
+                {:packets-in (NamedChannel. :packets-in packets-in)
+                 :packets-out (NamedChannel. :packets-out packets-out)
                  :router (create-router queue-size)
                  :online-clients {}
-                 :send-round empty-queue})
+                 :send-round empty-queue
+                 :resend-timeout-fn resend-timeout-fn
+                 :resend-timeout (->named resend-timeout-fn)} )
     (close! packets-out)))
--- server/test/sneer/server/router_connector_test.clj
+++ server/test/sneer/server/router_connector_test.clj
@@ -1,13 +1,13 @@
 (ns sneer.server.router-connector-test
   (:require
     [midje.sweet :refer :all]
-    [clojure.core.async :as async :refer [to-chan chan close! go <! >!]]
+    [clojure.core.async :as async :refer [thread to-chan chan close! >!!]]
     [clojure.core.match :refer [match]]
     [sneer.test-util :refer :all]
     [sneer.async :refer :all]
     [sneer.server.router-connector :refer :all]))
 
-; (do (require 'midje.repl) (midje.repl/autotest))
+;  (do (require 'midje.repl) (midje.repl/autotest))
 
 (def t1 {:id 1 :author :A :payload "1"})
 (def t2 {:id 2 :author :A :payload "2"})
@@ -16,9 +16,17 @@
 (tabular "Router Connector"
   (fact ?fact
      (let [max-q-size 2
-           packets-in (to-chan ?packets-in)
+           packets-in (chan)
            packets-out (chan)
-           subject (start-connector max-q-size packets-in packets-out)]
+           resend-timeout (chan)
+           resend-timeout-fn (constantly resend-timeout)
+           subject (start-connector max-q-size packets-in packets-out resend-timeout-fn)]
+       (thread
+         (doseq [event ?packets-in]
+           (if (= event :resend)
+             (>!! resend-timeout :stimulus)
+             (>!! packets-in event)))
+         (close! packets-in))
        (->> packets-out (async/into []) <!!?) => ?packets-out))
   
   ?fact ?packets-in ?packets-out
@@ -32,16 +40,17 @@
   [{:ack   1 :to   :A :for :B}]
 
   "A tuple is sent when client comes online (sends a ping)"
-  [{:send t1 :from :A :to  :B} {:from :B}]
+  [{:send t1 :from :A :to  :B} {:from :B} :resend]
   [{:ack   1 :to   :A :for :B} {:send t1 :to :B}]
+  
 
   "Full queue emits NAK"
   [{:send t1 :from :A :to  :B} {:send t2 :from :A :to  :B} {:send t3 :from :A :to  :B}]
   [{:ack   1 :to   :A :for :B} {:ack   2 :to   :A :for :B} {:nak   3 :to   :A :for :B}]
 
   "B receives a tuple with another enqueued"
-  [{:send t1 :from :A :to  :B} {:send t2 :from :A :to  :B} {:from :B}        {:ack :A :id 1 :from :B}]
-  [{:ack   1 :to   :A :for :B} {:ack   2 :to   :A :for :B} {:send t1 :to :B} {:send t2      :to   :B}]
+  [{:send t1 :from :A :to  :B} {:send t2 :from :A :to  :B} {:from :B} :resend {:ack :A :id 1 :from :B} :resend]
+  [{:ack   1 :to   :A :for :B} {:ack   2 :to   :A :for :B} {:send t1 :to :B}  {:send t2      :to   :B}]
 
   "A is notified when its send queue for B is empty."
   [
@@ -49,9 +58,6 @@
    {:send t1 :from :A :to :B} {:send t2 :from :A :to :B} {:send t3 :from :A :to :B}
    ; acks from :B to :A
    {:ack :A :id 1 :from :B} {:ack :A :id 2 :from :B}
-   ; ping from A to give server a chance to send :cts
-   {:from :A}
+   :resend
   ]
-  (fn [packets] (= (last packets) {:cts :B :to :A}))
-
-)
+  (fn [packets] (= (last packets) {:cts :B :to :A})))
