afc1719038d2a8c14f3a873aecf544b155f19afd
Felipe Bueno <bueno.felipe@gmail.com>
2014-12-10 15:56:18 -0200

    core: move docstring to proper position

--- core/src/main/clojure/sneer/networking/client_new.clj
+++ core/src/main/clojure/sneer/networking/client_new.clj
@@ -1,10 +1,10 @@
 (ns sneer.networking.client-new
   (:import [sneer.commons SystemReport]))
 
-(defn connect-to-follower [client follower-puk tuples-out]
+(defn connect-to-follower 
   "client is what is returned by start-client, below.
-   Throws IllegalState if there already is a connection to that follower."
-  )
+  Throws IllegalState if there already is a connection to that follower."
+  [client follower-puk tuples-out]
 
 (defn start-client [own-puk packets-in packets-out tuples-received]
   )
