8f4775f66b21abd292192d327d18cd113df4e050
Felipe Bueno <bueno.felipe@gmail.com>
2014-12-11 12:30:29 -0200

    core: WIP resend tuples

--- core/src/main/clojure/sneer/networking/client_new.clj
+++ core/src/main/clojure/sneer/networking/client_new.clj
@@ -1,18 +1,58 @@
 (ns sneer.networking.client-new
-  (:require [clojure.core.async :refer [pipe map< map> mult tap filter<]])
+  (:require [clojure.core.async :refer [>! <! map< map> mult tap filter< go-loop timeout alt!]]
+            [sneer.async :refer [go-while-let dropping-chan]]
+            [clojure.core.match :refer [match]])
   (:import [sneer.commons SystemReport]))
 
 (defn- ->ack [tuple]
   {:ack (:author tuple) :id (:id tuple)})
 
-(defn connect-to-follower [client follower-puk tuples-out]
-  (pipe
-    tuples-out
-    (map> #(do {:send % :to follower-puk}) (:packets-out client))))
+(defn- connect! [client follower-puk]
+  (let [connection (dropping-chan)]
+    (swap! (:follower-channels client) assoc follower-puk connection)
+    connection))
+
+(defn connect-to-follower [client follower-puk tuples-out & [resend-timeout-fn]]
+  (let [follower-packets (connect! client follower-puk)
+        packets-out (:packets-out client)
+        resend-timeout-fn (or resend-timeout-fn #(timeout 5000))]
+    (go-while-let
+     [tuple (<! tuples-out)]
+     (loop []
+       (>! packets-out {:send tuple :to follower-puk})
+       (alt!
+         follower-packets
+         ([packet]
+          (match packet
+            {:ack id}
+            (when-not (= id (:id tuple))
+              (recur))
+
+            {:nak id}
+            ;;TODO: don't retry until :cts
+            (recur)))
+
+         (resend-timeout-fn)
+         ([_]
+          (recur)))))))
 
 (defn start-client [own-puk packets-in packets-out tuples-received]
-  (let [packets-out (map> #(assoc % :from own-puk) packets-out)
-        tuples-in (->> packets-in (map< :send) (filter< some?) mult)]
-    (tap tuples-in tuples-received)
-    (tap tuples-in (map> ->ack packets-out))
-    {:packets-out packets-out}))
+  (let [follower-channels (atom {})
+        packets-out (map> #(assoc % :from own-puk) packets-out)]
+    (go-while-let
+     [packet (<! packets-in)]
+     (match packet
+       {:send tuple}
+       (do (>! tuples-received tuple)
+           (>! packets-out (->ack tuple)))
+
+       {:for follower}
+       (if-some [follower-channel (get @follower-channels follower)]
+         (>! follower-channel packet)
+         (println "Dropping packet for disconnected follower"))
+
+       {:cts follower}
+       ;;TODO: redirect to follower channel as above
+       nil))
+    {:packets-out packets-out
+     :follower-channels follower-channels}))
--- core/src/test/clojure/sneer/networking/client_test.clj
+++ core/src/test/clojure/sneer/networking/client_test.clj
@@ -1,10 +1,10 @@
 (ns sneer.networking.client-test
   (:require
-    [midje.sweet :refer :all]
-    [clojure.core.async :as async :refer [thread to-chan chan close! >!!]]
-    [sneer.test-util :refer :all]
-    [sneer.async :refer :all]
-    [sneer.networking.client-new :refer :all]))
+   [midje.sweet :refer :all]
+   [clojure.core.async :as async :refer [thread to-chan chan close! >!!]]
+   [sneer.test-util :refer :all]
+   [sneer.async :refer :all]
+   [sneer.networking.client-new :refer :all]))
 
 ;  (do (require 'midje.repl) (midje.repl/autotest))
 
@@ -18,12 +18,14 @@
       packets-out (chan)
       tuples-received (chan)
       subject (start-client :A packets-in packets-out tuples-received)
-      toB (chan)
-      toC (chan)
-      toD (chan)]
-  (connect-to-follower subject :B toB)
-  (connect-to-follower subject :C toC)
-  (connect-to-follower subject :D toD)
+      resend-timeout (chan)
+      resend-timeout-fn (constantly resend-timeout)
+      toB (chan 2)
+      toC (chan 2)
+      toD (chan 2)]
+  (connect-to-follower subject :B toB resend-timeout-fn)
+  (connect-to-follower subject :C toC resend-timeout-fn)
+  (connect-to-follower subject :D toD resend-timeout-fn)
   
   (fact "A tuple is sent"
     (>!!? toB t1) ; Non-blocking
@@ -32,4 +34,18 @@
   (fact "A tuple is received"
     (>!!? packets-in {:send tC})
     (<!!? tuples-received) => tC
-    (<!!? packets-out) => {:from :A :ack (:author tC) :id (:id tC)}))
+    (<!!? packets-out) => {:from :A :ack (:author tC) :id (:id tC)})
+
+  (fact "Pending tuple is sent when :cts"
+    (>!!? packets-in {:nak (:id t1) :for :B})
+    (>!!? packets-in {:cts :B})
+    (<!!? packets-out) => {:from :A :send t1 :to :B})
+
+  (fact "Pending tuple is resent"
+    (>!!? resend-timeout :stimulus)
+    (<!!? packets-out) => {:from :A :send t1 :to :B})
+  
+  (fact "Next tuple is sent on :ack"
+    (>!!? toB t2)
+    (>!!? packets-in {:ack (:id t1) :for :B})
+    (<!!? packets-out) => {:from :A :send t2 :to :B}))
