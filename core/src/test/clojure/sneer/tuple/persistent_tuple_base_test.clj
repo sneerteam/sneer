ff794ff273c49f0f4d5d9ad61be5a424e3244b8d
Felipe Bueno <bueno.felipe@gmail.com>
2014-12-17 16:03:57 -0200

    core: extract jdbc-tuple-base so it can be reused by tuple-transmitter test

--- core/src/test/clojure/sneer/tuple/persistent_tuple_base_test.clj
+++ core/src/test/clojure/sneer/tuple/persistent_tuple_base_test.clj
@@ -1,46 +1,21 @@
 (ns sneer.tuple.persistent-tuple-base-test
   (:require [clojure.java.jdbc :as sql]
             [clojure.string :as string]
-            [sneer.tuple.persistent-tuple-base :as tuple-base :refer [query-tuples store-tuple]]
+            [sneer.tuple.persistent-tuple-base :refer [query-tuples store-tuple]]
             [sneer.core :as core]            
             [sneer.test-util :refer [<!!?]]
             [midje.sweet :refer :all]
-            [clojure.core.async :as async])
+            [clojure.core.async :as async]
+            [sneer.tuple.jdbc-tuple-base :as jdbc-tuple-base])
   (:import [java.sql DriverManager]
            [sneer.crypto.impl KeysImpl]))
 
 ;  (do (require 'midje.repl) (midje.repl/autotest))
 
-(defn create-sqlite-db [databaseFile]
-  (let [connection (DriverManager/getConnection (if databaseFile (str "jdbc:sqlite:" (.getAbsolutePath databaseFile)) "jdbc:sqlite::memory:"))
-        db {:connection connection}]
-    (reify
-      tuple-base/Database
-      (db-create-table [this table columns]
-        (let [tuple-ddl (apply sql/create-table-ddl table columns)]
-          (sql/execute! db [tuple-ddl])))
-
-      (db-create-index [this table index-name columns-names]
-        (sql/execute! db [(str "CREATE INDEX " index-name " ON " (name table) "(" (string/join "," (map name columns-names)) ")" )]))
-
-      (db-insert [this table row]
-        (sql/insert! db table row))
-
-      (db-query [this sql-and-params]
-        (sql/query db sql-and-params :result-set-fn doall :as-arrays? true))
-
-      core/Disposable
-      (dispose [this]
-        (.close connection)))))
-
-(defn create [& [databaseFile]]
-  (let [db (create-sqlite-db databaseFile)]
-    (tuple-base/create db)))
-
 (defn- create-puk [^bytes rep] (.createPublicKey (KeysImpl.) rep))
 
 (facts "About query-tuples"
-  (let [subject (create)
+  (let [subject (jdbc-tuple-base/create)
         result (async/chan)
         lease (async/chan)
         t1 {"type" "sub" "payload" "42" "author" (create-puk (.getBytes "neide"))}
