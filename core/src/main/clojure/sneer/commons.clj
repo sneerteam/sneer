50cb320205fca1519add92661b37caa4520696ee
Felipe Bueno <bueno.felipe@gmail.com>
2014-12-18 11:20:53 -0200

    :lipstick:

--- core/src/main/clojure/sneer/commons.clj
+++ core/src/main/clojure/sneer/commons.clj
@@ -6,13 +6,12 @@
   (java.util.Arrays/equals a1 a2))
 
 (defn produce! [fn-if-absent map-atom key]
-  (let [existing (get @map-atom key)]
-    (if (some? existing)
-      existing
-      (get
-        (swap! map-atom update-in [key]
-          (fn [existing2] (if (some? existing2) existing2 (fn-if-absent key))))
-        key))))
+  (if-some [existing (get @map-atom key)]
+    existing
+    (get
+     (swap! map-atom update-in [key]
+            (fn [existing2] (if (some? existing2) existing2 (fn-if-absent key))))
+     key)))
 
 (defn now [] (sneer.commons.Clock/now))
 
