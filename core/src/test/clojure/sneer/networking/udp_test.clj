417492d3c57824e8856d061979cf4fe3961444cd
Klaus Wuestefeld <klauswuestefeld@gmail.com>
2014-12-30 23:17:24 -0200

    Minor

--- core/src/test/clojure/sneer/networking/udp_test.clj
+++ core/src/test/clojure/sneer/networking/udp_test.clj
@@ -8,23 +8,24 @@
 ; (do (require 'midje.repl) (midje.repl/autotest))
 
 (let [echo-port 1024
-       loopback (chan)
-       packets-out (chan 3)
-       packets-in (chan)
-       localhost (InetSocketAddress. "localhost" echo-port)
-       echo (fn [string]
-              (>!! packets-out [localhost (.getBytes string)])
-              (let [p (<!!? packets-in)]
-                (if (= p :timeout) :timeout (-> p second String.))))]
+      loopback (chan)
+      packets-out (chan)
+      packets-in (chan)
+      localhost (InetSocketAddress. "localhost" echo-port)
+      echo (fn [string]
+             (assert
+               (>!!? packets-out [localhost (.getBytes string)]))
+             (let [p (<!!? packets-in)]
+               (if (= p :timeout) :timeout (-> p second String.))))]
     
    (start-udp-server loopback loopback echo-port)
    (start-udp-server packets-in packets-out)
     
    (fact "Packets are sent and received"
-    (echo "Chance for loopback server to start.")
-    (echo "Hello") => "Hello"
-    (echo "42") => "42"
-    (echo "Goodbye") => "Goodbye")
+     (echo "Chance for loopback server to start.")
+     (echo "Hello") => "Hello"
+     (echo "42") => "42"
+     (echo "Goodbye") => "Goodbye")
    
    (close! loopback)
    (close! packets-out))
