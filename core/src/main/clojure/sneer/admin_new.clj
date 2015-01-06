fd7070556d2f68cbf0c3146717a6d5c9a4ef1a3e
Felipe Bueno <bueno.felipe@gmail.com>
2015-01-06 16:25:58 -0200

    core.tests: migrate SneerAdminTest to sneer.admin-new

--- core/src/main/clojure/sneer/admin_new.clj
+++ core/src/main/clojure/sneer/admin_new.clj
@@ -3,7 +3,6 @@
    [rx.lang.clojure.core :as rx]
    [sneer.tuple.space :as space]
    [sneer.impl-new :as impl]
-   [sneer.networking.client-new :as client]
    [sneer.tuple.persistent-tuple-base :as persistence])
   (:import
    [sneer PrivateKey]
@@ -14,11 +13,8 @@
 (defprotocol Restartable
   (restart [this]))
 
-(defn connect [client puk]
-  (assert false))
-
 (defn new-sneer-admin
-  [^PrivateKey own-prik client tuple-base]
+  [^PrivateKey own-prik tuple-base]
   (let [puk (.publicKey own-prik)
         tuple-space (space/reify-tuple-space puk tuple-base)
         sneer (impl/new-sneer tuple-space own-prik)]
@@ -35,7 +31,7 @@
       (persistence/db-insert db :keys {"prik" (.toBytes new-key)})
       new-key)))
 
-(defn new-sneer-admin-over-db [db client]
+(defn new-sneer-admin-over-db [db]
   (let [tuple-base (persistence/create db)
         own-prik (produce-private-key db)]
-    (new-sneer-admin own-prik client tuple-base)))
+    (new-sneer-admin own-prik tuple-base)))
