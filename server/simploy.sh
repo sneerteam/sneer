a73eb30d44d12b8fdeff0f5c08239cb060fd8fbd
Klaus Wuestefeld <klauswuestefeld@gmail.com>
2015-01-19 17:51:23 -0200

    Server: Redirecting sterr to log too.

--- server/simploy.sh
+++ server/simploy.sh
@@ -18,5 +18,5 @@ mv logs/log "logs/log-`date +%Y-%m-%d_%H-%M`"
 
 echo START NEW SERVER PROCESS
 SERVER_PORT=5555
-java -jar ./sneer.server-0.1.0-SNAPSHOT-standalone.jar $SERVER_PORT  > logs/log&
+java -jar ./sneer.server-0.1.0-SNAPSHOT-standalone.jar $SERVER_PORT  &> logs/log&
 echo DONE
