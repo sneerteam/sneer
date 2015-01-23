d9d21b424217b05c3e46c737c4e0955caedd8ec0
Rodrigo B. de Oliveira <rbo@acm.org>
2015-01-23 18:17:01 -0200

    elm-spike: center clock

--- spikes/elm/Main.elm
+++ spikes/elm/Main.elm
@@ -1,17 +1,14 @@
 module Main where
-
 import Color (..)
 import Graphics.Collage (..)
 import Graphics.Element (..)
-import Signal
+import Signal (..)
 import Time (..)
+import Window
 
+main = clock <~ every second ~ Window.dimensions
 
-main =
-  Signal.map clock (every second)
-
-
-clock t =
+clock t (sw, sh) =
   collage 400 400
     [ filled    lightGrey   (ngon 12 110)
     , outlined (solid grey) (ngon 12 110)
@@ -19,7 +16,7 @@ clock t =
     , hand charcoal 100 (t/60)
     , hand charcoal 60  (t/720)
     ]
-
+  |> container sw sh middle
 
 hand clr len time =
   let angle = degrees (90 - 6 * inSeconds time)
