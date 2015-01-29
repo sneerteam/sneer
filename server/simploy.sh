f07897b5de394532c67a7773dbbd51c786903c9c
Klaus Wuestefeld <klauswuestefeld@gmail.com>
2015-01-29 19:03:44 -0200

    Server: running inside /root/sneer-live folder

--- server/simploy.sh
+++ server/simploy.sh
@@ -4,19 +4,21 @@ export LEIN_ROOT=yes
 # Build new server version
 lein do clean, midje, uberjar || exit -1
 
-echo REPLACE UBERJAR
-mv target/uberjar/*standalone.jar . || exit -1
+mkdir -p /root/sneer-live      || exit -1
+mkdir -p /root/sneer-live/logs || exit -1
 
-echo MKDIR LOGS
-mkdir -p /root/logs || exit -1
+echo REPLACE UBERJAR
+mv target/uberjar/*standalone.jar /root/sneer-live/. || exit -1
 
 echo PKILL OLD SERVER PROCESS
 pkill -f sneer.server-0.1.0-SNAPSHOT-standalone.jar
 
+cd /root/sneer-live
+
 # Append date to previous log filename
-mv /root/logs/log "/root/logs/log-`date +%Y-%m-%d_%H-%M`"
+mv logs/log "logs/log-`date +%Y-%m-%d_%H-%M`"
 
 echo START NEW SERVER PROCESS
 SERVER_PORT=5555
-java -jar ./sneer.server-0.1.0-SNAPSHOT-standalone.jar $SERVER_PORT  &> /root/logs/log&
+java -jar ./sneer.server-0.1.0-SNAPSHOT-standalone.jar $SERVER_PORT  &> logs/log&
 echo DONE
