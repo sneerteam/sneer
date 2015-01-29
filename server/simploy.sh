957b4c7b0a62885653c30a8d14fd46457240759b
Klaus Wuestefeld <klauswuestefeld@gmail.com>
2015-01-29 18:51:00 -0200

    Server: log folder moved out of the repo to /root/logs

--- server/simploy.sh
+++ server/simploy.sh
@@ -8,15 +8,15 @@ echo REPLACE UBERJAR
 mv target/uberjar/*standalone.jar . || exit -1
 
 echo MKDIR LOGS
-mkdir -p logs || exit -1
+mkdir -p /root/logs || exit -1
 
 echo PKILL OLD SERVER PROCESS
 pkill -f sneer.server-0.1.0-SNAPSHOT-standalone.jar
 
 # Append date to previous log filename
-mv logs/log "logs/log-`date +%Y-%m-%d_%H-%M`"
+mv /root/logs/log "/root/logs/log-`date +%Y-%m-%d_%H-%M`"
 
 echo START NEW SERVER PROCESS
 SERVER_PORT=5555
-java -jar ./sneer.server-0.1.0-SNAPSHOT-standalone.jar $SERVER_PORT  &> logs/log&
+java -jar ./sneer.server-0.1.0-SNAPSHOT-standalone.jar $SERVER_PORT  &> /root/logs/log&
 echo DONE
