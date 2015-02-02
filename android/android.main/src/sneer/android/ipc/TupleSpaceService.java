d87495192e6ac6f6ef6b6181b69ada21ca48ee5e
Rodrigo B. de Oliveira <rbo@acm.org>
2015-02-02 18:33:30 -0200

    android.main: start a single ALIVE logger

--- android/android.main/src/sneer/android/ipc/TupleSpaceService.java
+++ android/android.main/src/sneer/android/ipc/TupleSpaceService.java
@@ -9,115 +9,43 @@ import android.util.Log;
 import java.util.concurrent.TimeUnit;
 
 import rx.Observable;
+import rx.Subscription;
 import rx.functions.Action1;
 import rx.functions.Func1;
+import rx.subscriptions.SerialSubscription;
 
 public class TupleSpaceService extends Service {
 
-//	private final AtomicInteger nextSubscriptionId = new AtomicInteger();
-//	private final ConcurrentMap<Integer, Subscription> subscriptions = new ConcurrentHashMap<Integer, Subscription>();
+    private Subscription subscription;
 
-	@SuppressWarnings("unused")
-	private String friendlyErrorMessage; //Return this to the callers. See how it is set, below.
-
-//	private final InteractiveSerializer serializer = new InteractiveSerializer();
-
-
-	@Override
+    @Override
 	public int onStartCommand(Intent intent, int flags, int startId) {
 		super.onStartCommand(intent, flags, startId);
-
-		if (intent == null || !intent.hasExtra("op")) {
-            logInterval();
-            return START_STICKY;
-        }
-
-//		switch(TupleSpaceFactoryClient.TupleSpaceOp.values()[intent.getIntExtra("op", -1)]) {
-//		case PUBLISH:
-//			publish((Map<String, Object>)serializer.deserialize(intent.getByteArrayExtra("tuple")));
-//			break;
-//		case SUBSCRIBE:
-//			subscribe((Map<String, Object>)serializer.deserialize(intent.getByteArrayExtra("criteria")), (ResultReceiver)intent.getParcelableExtra("resultReceiver"));
-//			break;
-//		case SUBSCRIBE_LOCAL:
-//			subscribeLocal((Map<String, Object>)serializer.deserialize(intent.getByteArrayExtra("criteria")), (ResultReceiver)intent.getParcelableExtra("resultReceiver"));
-//			break;
-//		case UNSUBSCRIBE:
-//			unsubscribe(intent.getIntExtra("subscription", -1));
-//			break;
-//		default:
-//			break;
-//		}
-
+		logInterval();
 		return START_STICKY;
 	}
 
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if (subscription != null)
+            subscription.unsubscribe();
+    }
 
-//	private void unsubscribe(int subscriptionId) {
-//		if (subscriptionId == -1)
-//			throw new IllegalArgumentException("Unknown subscription: " + subscriptionId);
-//
-//		Subscription s = subscriptions.remove(subscriptionId);
-//
-//		if (s != null)
-//			s.unsubscribe();
-//	}
-//
-//
-//	private void subscribe(Map<String, Object> criteria, ResultReceiver resultReceiver) {
-//		subscribe(resultReceiver, sneer().tupleSpace().filter().putFields(criteria).tuples());
-//	}
-//
-//
-//	private void subscribeLocal(Map<String, Object> criteria, ResultReceiver resultReceiver) {
-//		subscribe(resultReceiver, sneer().tupleSpace().filter().putFields(criteria).localTuples());
-//	}
-
-
-//	private void subscribe(final ResultReceiver resultReceiver, Observable<Tuple> tuples) {
-//		int id = nextSubscriptionId.getAndIncrement();
-//		resultReceiver.send(TupleSpaceFactoryClient.SubscriptionOp.SUBSCRIPTION_ID.ordinal(), bundle(id));
-//
-//		Subscription s = tuples.doOnCompleted(new Action0() { @Override public void call() {
-//			resultReceiver.send(ON_COMPLETED.ordinal(), new Bundle());
-//		}}).subscribe(new Action1<Tuple>() { @Override public void call(Tuple tuple) {
-//			resultReceiver.send(ON_NEXT.ordinal(), bundle(serializer.serialize(new HashMap<String, Object>(tuple))));
-//		}});
-//
-//		subscriptions.put(id, s);
-//	}
-
-
-//	private Bundle bundle(Object value) {
-//		Bundle ret = new Bundle();
-//		if (value instanceof Parcelable)
-//			ret.putParcelable("value", (Parcelable) value);
-//		else
-//			ret.putSerializable("value", (Serializable) value);
-//
-//		return ret;
-//	}
-
-
-//	private void publish(Map<String, Object> tuple) {
-//		sneer().tupleSpace().publisher().putFields(tuple).pub();
-//	}
-
-
-	@Override
+    @Override
 	public IBinder onBind(Intent arg0) {
 		return null;
 	}
-
 
 	public static void startTupleSpaceService(Context context) {
 		Intent startServiceIntent = new Intent(context, TupleSpaceService.class);
 	    context.startService(startServiceIntent);
 	}
 
-
     private void logInterval() {
-        Observable.interval(1, TimeUnit.SECONDS).map(new Func1<Long, String>() { @Override public String call(Long interval) {
+        if (subscription != null)
+            return;
+        subscription = Observable.interval(5, TimeUnit.SECONDS).map(new Func1<Long, String>() { @Override public String call(Long interval) {
             return "IT'S ALIVE: " + System.currentTimeMillis() / 1000;
         }}).subscribe(new Action1<Object>() { @Override public void call(Object obj) {
             Log.d("", obj.toString());
