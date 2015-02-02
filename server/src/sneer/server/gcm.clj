6cffca7655b0c5e6a10178ff50516fadd6a7953b
Rodrigo B. de Oliveira <rbo@acm.org>
2015-02-02 18:32:15 -0200

    server: send gcm notifications

--- server/src/sneer/server/gcm.clj
+++ server/src/sneer/server/gcm.clj
@@ -3,23 +3,11 @@
 
 (def api-key "AIzaSyDkBjnfvd2bzp-qNDIWRLXSvRVy12KwdsU")
 
-#_("
-
-Content-Type:application/json
-Authorization:key=AIzaSyB-1uEai2WiUapxCs2Q0GZYzPu7Udno5aA
-{
-  "registration_ids" : ["APA91bHun4MxP5egoKMwt2KZFBaFUH-1RYqx..."],
-  "data" : {
-          ...
-          },
-}
-")
-
-(defn send-msg []
+(defn send-to [id callback]
   (http/post "https://android.googleapis.com/gcm/send"
-    {:headers {"Content-Type" "application/json"
-               "Authorization" "key=AIzaSyAqqgrLlio9WyDH2_Ztetxn82u4x_cvLfY"}
-     :body "{
-               \"registration_ids\": [ \"APA91bGdLsycUvLKLaWXw807-USTEBUcYE6p-D_758maR_iNQJpu_cb9-4NPDPe0HXimtGlZYLYACNsd_Imb5hQsFspdpJLOIhWknbxh1T7dqVimNr2pLoF_7_iya5RuYO5ksddp7YUjB7C_0n4bXzgDSJzf7tVtog\" ]
-            }"
- }))
+             {:headers {"Content-Type" "application/json"
+                        "Authorization" (str "key=" api-key)}
+              :timeout 3000
+              :body (str "{\"registration_ids\": [ \"" id "\" ]
+                          ,\"collapse_key\"    : \"wakeup\"}")}
+             callback))
--- server/src/sneer/server/http_server.clj
+++ server/src/sneer/server/http_server.clj
@@ -3,78 +3,107 @@
             [compojure.core :refer :all]
             [compojure.route :as route]
             [ring.middleware.params :as params]
-            [clojure.core.async :as async :refer [<!]]
-            [sneer.async :refer [IMMEDIATELY]]
-            [sneer.keys :as keys]))
+            [clojure.core.async :as async :refer [<! >! >!! go-loop alt!]]
+            [sneer.async :refer [go-while-let sliding-chan]]
+            [sneer.keys :as keys]
+            [sneer.server.gcm :as gcm]))
 
 (defn async-gcm-notify [gcm-id]
   (let [result (async/chan)]
-    (async/go
-      (async/>! result {:headers {"Retry-After" "120"}}))
+    (gcm/send-to gcm-id
+                 (fn [response] (>!! result response)))
     result))
+
+(defn- start-gcm-notification-rounds [gcm-ids gcm-ids-notified]
+  (go-while-let [round (<! gcm-ids)]
+    (loop [round round]
+      (when-not (empty? round)
+        (let [gcm-id (first round)
+              response (<! (async-gcm-notify gcm-id))
+              status (:status response)]
+          (println "GCM: " response)
+          (if (= 200 status)
+            (>! gcm-ids-notified gcm-id)
+            (when-some [retry-after-secs (-> response :headers :retry-after)]
+              (println "GCM: WAITING" retry-after-secs "SECONDS")
+              (<! (async/timeout (* 1000 (Integer/valueOf retry-after-secs))))))
+          (recur (rest round)))))))
+
+(defn- start-gcm-notifier [puk->gcm-id-in puks-in]
+
+  (let [gcm-ids-out (sliding-chan)
+        gcm-ids-notified (async/chan)]
+
+    (start-gcm-notification-rounds gcm-ids-out gcm-ids-notified)
+
+    (go-loop [puk->gcm-id {}
+              gcm-id->puk {}
+              puks-to-notify #{}
+              ids-to-notify #{}
+              previous-ids-to-notify ids-to-notify]
+
+      (when-not (identical? previous-ids-to-notify ids-to-notify)
+        (>! gcm-ids-out ids-to-notify))
+
+      (alt!
+        puks-in
+        ([puk]
+         (when (some? puk)
+           (recur puk->gcm-id
+                  gcm-id->puk
+                  (conj puks-to-notify puk)
+                  (if-some [gcm-id (get puk->gcm-id puk)]
+                    (conj ids-to-notify gcm-id)
+                    ids-to-notify)
+                  ids-to-notify)))
+
+        puk->gcm-id-in
+        ([[puk gcm-id]]
+         (recur (assoc puk->gcm-id-in puk gcm-id)
+                (assoc gcm-id->puk gcm-id puk)
+                puks-to-notify
+                (if (contains? puks-to-notify puk)
+                  (conj ids-to-notify gcm-id)
+                  ids-to-notify)
+                ids-to-notify))
+
+        gcm-ids-notified
+        ([gcm-id]
+         (recur puk->gcm-id
+                gcm-id->puk
+                (disj puks-to-notify (get gcm-id->puk gcm-id))
+                (disj ids-to-notify gcm-id)
+                ids-to-notify))))))
 
 (defn- gcm-register [puk->gcm-id req]
   (let [params (:query-params req)
         puk (get params "puk")
         id (get params "id")]
     (assert (not (or (empty? puk) (empty? id))))
-    (swap! puk->gcm-id assoc (keys/->puk puk) id)
+    (async/go
+      (>! puk->gcm-id [(keys/->puk puk) id]))
     (str "id for " puk " set to " id)))
-
-(defn rand-set-element [set]
-  (let [n (count set)]
-    (when-not (zero? n)
-      (nth (seq set) (rand-int n)))))
-
-(def NEVER (async/chan))
-
-(defn- start-gcm-notifier [puk->gcm-id puks-in]
-  (async/go-loop [puks #{}
-                  send-timeout NEVER]
-    (async/alt!
-      puks-in
-      ([puk]
-        (when (some? puk)
-          (if (contains? @puk->gcm-id puk)
-            (let [puks' (conj puks puk)
-                  send-timeout' (if (identical? send-timeout NEVER)
-                                  IMMEDIATELY
-                                  send-timeout)]
-              (recur puks' send-timeout'))
-            (recur puks send-timeout))))
-
-      send-timeout
-      ([_]
-        (let [puk (rand-set-element puks)
-              gcm-id (get @puk->gcm-id puk)]
-          (println "GCM: sending gcm notification to" gcm-id)
-          (let [response (<! (async-gcm-notify gcm-id))]
-            (println "GCM:" response)
-            ;; TODO: treat error codes here
-            (if-some [retry-after-secs (-> response :headers (get "Retry-After"))]
-              (recur puks (async/timeout (* 1000 (Integer/valueOf retry-after-secs))))
-              (recur (disj puks puk) IMMEDIATELY))))))))
 
 (defn start [port puks-in]
 
-  (let [puk->gcm-id (atom {})]
-
-    (start-gcm-notifier puk->gcm-id puks-in)
+  (let [puk->gcm-id-out (async/chan)]
 
     (defroutes app
-      (GET "/gcm/register" req (gcm-register puk->gcm-id req))
-      (GET "/gcm/query" [] (str @puk->gcm-id))
+      (GET "/gcm/register" req (gcm-register puk->gcm-id-out req))
       (route/not-found "<h1>page not found</h1>"))
 
-    (http/run-server (-> app params/wrap-params) {:port port})))
+    (let [server (http/run-server (-> app params/wrap-params) {:port port})]
+      (async/go
+        (<! (start-gcm-notifier puk->gcm-id-out puks-in))
+        (async/close! puk->gcm-id-out)
+        (server)))))
 
 ;; for REPL usage
 ;(def puks-in (async/chan))
 ;(def server (start 8080 puks-in))
 ;
 ;(defn stop []
-;  (async/close! puks-in)
-;  (server))
+;  (async/close! puks-in))
 ;
 ;(defn notify [puk]
 ;  (async/go
