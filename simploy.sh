42dd03d57f49c6ec012a95f595f3c3a98484cb49
Klaus Wuestefeld <klauswuestefeld@gmail.com>
2015-02-03 18:54:03 -0200

    Server: java-api -> sneer-java-api

--- simploy.sh
+++ simploy.sh
@@ -3,6 +3,7 @@ git log -1
 
 source /root/.android-sdk-installer/env
 # Install necessary project dependencies to local maven cache
-./gradlew --configure-on-demand :core:install :java-api:install :crypto:install || exit -1
+./gradlew --configure-on-demand :core:install :sneer-java-api:install :crypto:install || exit -1
 
 cd server && ./simploy.sh && cd - || exit -1
+
