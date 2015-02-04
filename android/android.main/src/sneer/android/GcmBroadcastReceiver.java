5039c49d1685ffee2da5fde101b32f3ef34b1f56
Klaus Wuestefeld <klauswuestefeld@gmail.com>
2015-02-04 01:01:16 -0200

    Main: Spike: Notification on GCM (must be moved to new message)

--- android/android.main/src/sneer/android/GcmBroadcastReceiver.java
+++ android/android.main/src/sneer/android/GcmBroadcastReceiver.java
@@ -1,23 +1,46 @@
 package sneer.android;
 
 import android.app.Activity;
+import android.app.NotificationManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
 import com.google.android.gms.gcm.GoogleCloudMessaging;
 
+import sneer.commons.Clock;
+
 public class GcmBroadcastReceiver extends BroadcastReceiver {
+
+    private static int next_id = (int)Clock.now();
+
     @Override
     public void onReceive(Context context, Intent intent) {
         // Bundle extras = intent.getExtras();
         GoogleCloudMessaging gcm = GoogleCloudMessaging.getInstance(context);
-
         String messageType = gcm.getMessageType(intent);
-
         Log.d(getClass().getName(), messageType);
+
+        createNotificationIfNecessary(context);
 
         setResultCode(Activity.RESULT_OK);
     }
+
+    private void createNotificationIfNecessary(Context context) {
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context);
+        builder.setSmallIcon(R.drawable.ic_launcher)
+                .setContentTitle("This is the Notification Title")
+                .setContentText("This is the notification text. Bla bla bla")
+                .setWhen(Clock.now())
+                .setAutoCancel(true)
+//              .setContentIntent(pendIntent)
+                .setOngoing(false);
+
+		NotificationManager man = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+		man.notify("sneer notification", next_id++, builder.build());
+    }
+
+
 }
