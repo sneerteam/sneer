16580979c949ad9493a6d8a412372b490b8e5477
Klaus Wuestefeld <klauswuestefeld@gmail.com>
2015-02-09 16:37:45 -0200

    reset-env script vastly simplified.

--- reset-environment.sh
+++ reset-environment.sh
@@ -1,22 +1,9 @@
-#!/bin/bash
-echo "-------------------> Resetting workspace and pulling changes"
-
-hash_before_pull=$(git hash-object "$0")
+#!/bin/bash -v
 
 git pull --rebase || exit -1
 
-hash_after_pull=$(git hash-object "$0")
-
 #Delete all untracked files and directories (-d), even ignored ones (-x).
-git clean -x -d --force --quiet
-
-if [ "$hash_before_pull" != "$hash_after_pull" ]; then
-	echo "-------------------> This script changed, rerunning it"
-	trap '$0' 0
-	exit 0
-fi
-
-echo "-------------------> Preparing the workspace"
+git clean -d -x --force --quiet
 
 rm ~/.m2/repository/me/sneer/ -rf
 
