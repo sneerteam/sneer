65d76b52c6cec1e8fb3efb8c444601cb48f6a2de
Klaus Wuestefeld <klauswuestefeld@gmail.com>
2015-02-12 01:43:02 -0200

    Server: test gcm server starts only once and avoids "port already in use".

--- server/test/sneer/server/http_server_test.clj
+++ server/test/sneer/server/http_server_test.clj
@@ -6,29 +6,31 @@
             [org.httpkit.client :as http]
             [midje.sweet :refer :all]))
 
-(tabular "Pending notification is sent after registration"
-  (let [puks-to-notify (async/chan)]
-    (try
-      (let [to-google (async/chan)
-            from-google (async/chan)
-            gcm-notify (fn [id] (do (>!!? to-google id)
-                                    from-google))
-            puk (keys/->puk name?)]
+(let [to-google (async/chan)
+      from-google (async/chan)
+      gcm-notify (fn [id] (do (>!!? to-google id)
+                              from-google))
+      puks-to-notify (async/chan)]
 
-        (subject/start 4242 puks-to-notify gcm-notify)
+  (subject/start 4242 puks-to-notify gcm-notify)
+
+  (try
+    (tabular "Pending notification is sent after registration"
+
+      (let [puk (keys/->puk name?)]
 
         (>!!? puks-to-notify puk)
 
-        (let [uri (str "http://localhost:4242/gcm/register?id=" gcm-id? "&puk=" (.toHex puk))
-              response @(http/get uri)]
-          (:status response) => 200)
+          (let [uri (str "http://localhost:4242/gcm/register?id=" gcm-id? "&puk=" (.toHex puk))
+            response @(http/get uri)]
+              (:status response) => 200)
 
-        (<!!? to-google) => gcm-id?
-        (>!!? from-google {:status 200}))
+                (<!!? to-google) => gcm-id?
+                (>!!? from-google {:status 200}))
 
-      (finally
-        (async/close! puks-to-notify))))
+      name?     gcm-id?
+      "neide"   "101010"
+      "maicon"  "202020")
 
-  name?     gcm-id?
-  "neide"   "101010"
-  "maicon"  "202020")
+    (finally
+      (async/close! puks-to-notify))))
