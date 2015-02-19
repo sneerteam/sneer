38180b1d87ce7c44a402a188c7d8450f30101d6a
Rodrigo B. de Oliveira <rbo@acm.org>
2015-02-19 00:21:37 -0200

    @ignore PubSubOverServerNetwork

--- core.tests/src/sneer/core/tests/PubSubOverServerNetwork.java
+++ core.tests/src/sneer/core/tests/PubSubOverServerNetwork.java
@@ -1,7 +1,10 @@
 package sneer.core.tests;
 
+import org.junit.Ignore;
+
 import static sneer.ClojureUtils.var;
 
+@Ignore("avoid polluting the server")
 public class PubSubOverServerNetwork extends PubSubTest {
 
 	@Override
