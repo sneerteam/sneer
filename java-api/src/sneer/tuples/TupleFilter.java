accfc983c8a390cc7f98950e940bc6ec986bafcb
Klaus Wuestefeld <klauswuestefeld@gmail.com>
2015-02-23 19:09:12 -0300

    Revert "TupleFilter.last"
    This reverts commit 2b5b07a383e3c787311c85a2b78dbe5ce6a98139.

--- java-api/src/sneer/tuples/TupleFilter.java
+++ java-api/src/sneer/tuples/TupleFilter.java
@@ -15,9 +15,6 @@ public interface TupleFilter {
 	TupleFilter field(String field, Object value);
 	TupleFilter putFields(Map<String, Object> fields);
 
-	/** Causes the filter to emit tuples starting from the last known one to satisfy this filter. */
-	TupleFilter last();
-
 	/** @return An observable that emits tuples satisfying this filter. The returned Observable does not complete. */
 	Observable<Tuple> tuples();
 	
