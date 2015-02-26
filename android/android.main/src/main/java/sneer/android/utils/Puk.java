3f2692138424e2bf4202c5c84e157df1b815e9b0
Rodrigo B. de Oliveira <rbo@acm.org>
2015-02-26 17:15:15 -0300

    improve public key sharing message

--- android/android.main/src/main/java/sneer/android/utils/Puk.java
+++ android/android.main/src/main/java/sneer/android/utils/Puk.java
@@ -14,14 +14,12 @@ public class Puk {
 	public static void sendYourPublicKey(Activity activity, Party party, boolean self, String nickname) {
 		Intent sharingIntent = new Intent(Intent.ACTION_SEND);
 		sharingIntent.setType("text/plain");
-		sharingIntent.putExtra(Intent.EXTRA_SUBJECT, "My Sneer public key");
+		sharingIntent.putExtra(Intent.EXTRA_SUBJECT, (self ? "My" : nickname + "'s") + " Sneer public key");
 		sharingIntent.putExtra(Intent.EXTRA_TEXT, buildSneerUri(party.publicKey().current().toHex()));
 
-		String title;
-		if (self)
-			title = "Share Your Public Key";
-		else
-			title = "Share " + nickname  + "'s Public Key";
+		String title = self
+				? "Share Your Public Key"
+				: "Share " + nickname  + "'s Public Key";
 		Intent chooser = Intent.createChooser(sharingIntent, title);
 
 		if (sharingIntent.resolveActivity(activity.getPackageManager()) != null) {
