185f64afdc1255d647820fbab5b88c9b1627b237
Klaus Wuestefeld <klauswuestefeld@gmail.com>
2015-03-06 00:09:49 -0300

    Server: building disabled to start server

--- server/simploy.sh
+++ server/simploy.sh
@@ -2,13 +2,13 @@
 export LEIN_ROOT=yes
 
 # Build new server version
-lein do clean, midje, uberjar || exit -1
+#lein do clean, midje, uberjar || exit -1
 
 mkdir -p /root/sneer-live      || exit -1
 mkdir -p /root/sneer-live/logs || exit -1
 
 echo REPLACE UBERJAR
-mv target/uberjar/*standalone.jar /root/sneer-live/. || exit -1
+#mv target/uberjar/*standalone.jar /root/sneer-live/. || exit -1
 
 echo PKILL OLD SERVER PROCESS
 pkill -f sneer.server-0.1.0-SNAPSHOT-standalone.jar
