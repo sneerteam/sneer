09966fa604d1c12e74942d6b828273c9fa9f7213
Felipe Bueno <bueno.felipe@gmail.com>
2015-03-10 14:02:59 -0300

    minor

--- android/android.main/src/main/java/sneer/android/SneerApp.java
+++ android/android.main/src/main/java/sneer/android/SneerApp.java
@@ -3,29 +3,14 @@ package sneer.android;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Application;
-import android.app.NotificationManager;
-import android.app.PendingIntent;
 import android.content.Context;
-import android.content.Intent;
-import android.os.Handler;
-import android.os.Looper;
-import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GooglePlayServicesUtil;
 
-import java.util.Collection;
-import java.util.Date;
-
-import rx.functions.Action1;
-import sneer.Conversation;
 import sneer.android.gcm.GcmRegistrationAlarmReceiver;
 import sneer.android.impl.SneerAndroidImpl;
-import sneer.android.ui.MainActivity;
-import sneer.commons.Clock;
-
-import static sneer.android.SneerAndroidSingleton.sneer;
 
 public class SneerApp extends Application {
 
@@ -33,7 +18,7 @@ public class SneerApp extends Application {
 	public void onCreate() {
 		super.onCreate();
 
-	    //Do not delete this. We must revive the simulator for rapid UI development.
+		//Do not delete this. We must revive the simulator for rapid UI development.
 //		SneerAndroidSingleton.setInstance(isCoreAvailable()
 //			? new SneerAndroidImpl(getApplicationContext())
 //			: new SneerAndroidSimulator(getApplicationContext()));
@@ -47,19 +32,19 @@ public class SneerApp extends Application {
 
 
 	public void checkPlayServices(Activity activity) {
-        int resultCode = GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
-        if (resultCode != ConnectionResult.SUCCESS) {
-            if (GooglePlayServicesUtil.isUserRecoverableError(resultCode)) {
-                new AlertDialog.Builder(activity)
-                    .setTitle("Missing Google Play services")
-                    .setMessage("Some features such as using maps and receiving notifications when idle will not work without Google Play Services, which are missing from this phone.")
-                    .setPositiveButton("OK", null)
-                    .show();
-            } else {
-                Log.i(getClass().getName(), "This device is not supported.");
-                activity.finish();
-            }
-        }
-    }
+		int resultCode = GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
+		if (resultCode != ConnectionResult.SUCCESS) {
+			if (GooglePlayServicesUtil.isUserRecoverableError(resultCode)) {
+				new AlertDialog.Builder(activity)
+						.setTitle("Missing Google Play services")
+						.setMessage("Some features such as using maps and receiving notifications when idle will not work without Google Play Services, which are missing from this phone.")
+						.setPositiveButton("OK", null)
+						.show();
+			} else {
+				Log.i(getClass().getName(), "This device is not supported.");
+				activity.finish();
+			}
+		}
+	}
 
 }
