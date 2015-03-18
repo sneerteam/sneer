5cd11e170fbc1ba227e4752f4610b97271c29f8a
Rodrigo B. de Oliveira <rbo@acm.org>
2015-03-18 18:42:00 -0300

    split android projects so core can be built via lein skummet

--- core/project.clj
+++ core/project.clj
@@ -1,43 +1,60 @@
-(defproject core "0.1.5"
+(defproject me.sneer/core "0.1.5"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.skummet/clojure "1.7.0-alpha5-r1"]
+  :dependencies [
+                 [org.skummet/clojure "1.7.0-alpha5-r1"]
                  [me.sneer/sneer-java-api "0.1.5" :exclusions [org.clojure/clojure]]
                  [me.sneer/crypto "0.1.5" :exclusions [org.clojure/clojure]]
                  [org.clojure/core.match "0.2.2" :exclusions [org.clojure/clojure]]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha" :exclusions [org.clojure/clojure]]
-                 [com.cognitect/transit-clj "0.8.259" :exclusions [org.clojure/clojure]]
+                 [com.cognitect/transit-clj "0.8.259" :exclusions [org.clojure/clojure ]]
 ;                 [midje "1.6.3" :exclusions [org.clojure/clojure]]
                  [com.netflix.rxjava/rxjava-clojure "0.20.7" :exclusions [org.clojure/clojure]]
                  [org.clojure/java.jdbc "0.3.6" :exclusions [org.clojure/clojure]]
-                 [org.xerial/sqlite-jdbc "3.8.6" :exclusions [org.clojure/clojure]]]
+                 ;[org.xerial/sqlite-jdbc "3.8.6" :exclusions [org.clojure/clojure]]
+                 ]
 
   :skummet-skip-vars [
                       ;"#'sneer.async/go-while-let"
                       ]
                                         ;  :aot :all
-  :aot [#"clojure.+"
-        sneer.commons
-        sneer.rx
-        sneer.rx-macros
-        sneer.admin
-        sneer.contact
-        sneer.impl
-        sneer.conversation
-        sneer.impl
-        sneer.keys
-        sneer.party
-        sneer.profile
-        sneer.serialization
-        sneer.tuple.persistent-tuple-base
-        sneer.tuple.space
-        sneer.tuple.queue
-        sneer.tuple.tuple-transmitter
-        sneer.networking.client
-        sneer.networking.udp
-        sneer.networking.client]
+  :aot [
+    sneer.main
+;    sneer.tuple.protocols
+    sneer.tuple.persistent-tuple-base
+    sneer.tuple.space
+    sneer.tuple.queue
+    sneer.tuple.tuple-transmitter
+    sneer.commons
+    sneer.rx
+    sneer.rx-macros
+    sneer.admin
+    sneer.contact
+    sneer.impl
+    sneer.conversation
+    sneer.impl
+    sneer.keys
+    sneer.party
+    sneer.profile
+    sneer.serialization
+    sneer.networking.client
+    sneer.networking.udp
+    sneer.networking.client
+    #"clojure.+"
+  ]
+
+  :uberjar-exclusions [
+    #"META-INF/DUMMY.SF"
+    #"^org/(apache|bouncycastle|json)"
+    #"^rx/(exceptions|functions|internal|observables|observers|plugins|schedulers|subjects|subscriptions)"
+    #"^rx/Notification"
+    #"^rx/Observ"
+    #"^rx/Produc"
+    #"^rx/Sched"
+    #"^rx/Subs"
+  ]
   :profiles {:default []}
 ;  :profiles  {:dev {:plugins [[cider/cider-nrepl "0.8.2"][refactor-nrepl "0.2.2"]]}}
   :plugins [;[lein-midje "3.0.0"]
