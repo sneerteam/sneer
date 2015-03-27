cdfc8e3ebbb0357521a779c53e7c810de9844d29
Rodrigo B. de Oliveira <rbo@acm.org>
2015-03-27 18:59:48 -0300

    publish skummet artifacts to ./skummet-artifacts and conditionally enable dependency on android.main

--- core/project.clj
+++ core/project.clj
@@ -57,10 +57,8 @@
               :plugins [[cider/cider-nrepl "0.8.2"]
                         [refactor-nrepl "0.2.2"]
                         [lein-midje "3.0.0"]]
-
               }
-
-             }
+            }
 
 
   :source-paths ["src/main/clojure"]
