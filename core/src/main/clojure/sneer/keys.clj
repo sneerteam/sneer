c1f78522e75bb284fed83a1789f7a939b3257d2f
Rodrigo B. de Oliveira <rbo@acm.org>
2015-03-30 16:21:07 -0300

    [skummet] replace `let over defn` idiom by more orthodox code (fixes underflow dex error)

--- core/src/main/clojure/sneer/keys.clj
+++ core/src/main/clojure/sneer/keys.clj
@@ -1,16 +1,16 @@
 (ns sneer.keys
   (:import [sneer.crypto.impl KeysImpl]))
 
-(let [keys-impl (KeysImpl.)]
+(def ^:private keys-impl (KeysImpl.))
 
-  (defn create-prik []
-    (.createPrivateKey keys-impl))
+(defn create-prik []
+  (.createPrivateKey keys-impl))
 
-  (defn create-puk [^bytes rep]
-    (.createPublicKey keys-impl rep))
+(defn create-puk [^bytes rep]
+  (.createPublicKey keys-impl rep))
 
-  (defn from-hex [^String hex]
-    (.createPublicKey keys-impl hex))
+(defn from-hex [^String hex]
+  (.createPublicKey keys-impl hex))
 
-  (defn ->puk [^String rep]
-    (create-puk (.getBytes rep))))
+(defn ->puk [^String rep]
+  (create-puk (.getBytes rep)))
