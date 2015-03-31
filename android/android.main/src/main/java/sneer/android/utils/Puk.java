49c56cde2101f97448200ca5444c63f63fed8633
Felipe Bueno <bueno.felipe@gmail.com>
2015-03-31 20:53:08 -0300

    main: Add contact UI adjustments

--- android/android.main/src/main/java/sneer/android/utils/Puk.java
+++ android/android.main/src/main/java/sneer/android/utils/Puk.java
@@ -12,7 +12,7 @@ public class Puk {
 	public static void shareOwnPublicKey(Activity activity, Party self, long inviteCode, String receiver) {
 		Intent sharingIntent = new Intent(Intent.ACTION_SEND);
 		sharingIntent.setType("text/plain");
-		sharingIntent.putExtra(Intent.EXTRA_SUBJECT, "My Sneer Public Key");
+		sharingIntent.putExtra(Intent.EXTRA_SUBJECT, "Sneer Invite");
 		sharingIntent.putExtra(Intent.EXTRA_TEXT,
 				"\n\nIf you don't have the Sneer app, install it using the Play Store: https://play.google.com/store/apps/details?id=me.sneer\n\n" +
 				"Then, tap to add me as a Sneer contact: " +
