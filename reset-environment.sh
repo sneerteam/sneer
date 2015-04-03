b20a2ed8f9856a5554e09d831704d1e69ef79396
oakes <zsoakes@gmail.com>
2015-04-02 20:52:14 -0400

    Fix rm command

--- reset-environment.sh
+++ reset-environment.sh
@@ -5,7 +5,7 @@ git pull --rebase || exit -1
 #Delete all untracked files and directories (-d), even ignored ones (-x).
 git clean -d -x --force --quiet
 
-rm ~/.m2/repository/me/sneer/ -rf
+rm -rf ~/.m2/repository/me/sneer/
 
 ./gradlew clean check install || exit -1
 
