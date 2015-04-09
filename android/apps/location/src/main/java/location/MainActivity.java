1cc446ff90f478705d5f049c8c6db20f49b2570d
Klaus Wuestefeld <klauswuestefeld@gmail.com>
2015-04-09 10:31:43 -0300

    Location: app created

---
+++ android/apps/location/src/main/java/location/MainActivity.java
@@ -0,0 +1,17 @@
+package location;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.ppeccin.sneer.location.R;
+
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+}
