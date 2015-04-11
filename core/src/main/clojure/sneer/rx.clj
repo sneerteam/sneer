c77ecaa0c9ee2ff0ffbde7129120aefa806fa671
Klaus Wuestefeld <klauswuestefeld@gmail.com>
2015-04-11 03:41:44 -0300

    Minor

--- core/src/main/clojure/sneer/rx.clj
+++ core/src/main/clojure/sneer/rx.clj
@@ -2,21 +2,22 @@
   (:require
     [rx.lang.clojure.core :as rx]
     [rx.lang.clojure.interop :as interop])
-  (:import [rx.subjects Subject BehaviorSubject]
-           [rx.schedulers Schedulers]))
+  (:import [rx.subjects BehaviorSubject]
+           [rx.schedulers Schedulers]
+           [rx Observable]))
 
 (defn on-subscribe [f]
   "Reifies a rx.Observable.OnSubscribe instance from a regular clojure function `f'."
   (reify rx.Observable$OnSubscribe
     (call [this subscriber] (f subscriber))))
 
-(defn on-subscribe-for [^rx.Observable observable]
+(defn on-subscribe-for [^Observable observable]
   "Returns a rx.Observable.OnSubscribe instance that subscribes subscribers to `observable'."
   (on-subscribe
     (fn [^rx.Subscriber subscriber]
       (.add subscriber (.subscribe observable subscriber)))))
 
-(defn subject* [^rx.Observable observable ^rx.Observer observer]
+(defn subject* [^Observable observable ^rx.Observer observer]
   "Creates a rx.Subject from an `observable' part and an `observer' part."
   (let [subscriber (on-subscribe-for observable)]
     (sneer.rx.CompositeSubject. subscriber observer)))
@@ -30,7 +31,7 @@
       observable)))
 
 (defn seq->observable [^java.lang.Iterable iterable]
-  (rx.Observable/from iterable))
+  (Observable/from iterable))
 
 (defn atom->observable [atom]
   (let [subject (BehaviorSubject/create @atom)]
@@ -38,29 +39,29 @@
                                 (rx/on-next subject new-value)))
     (.asObservable subject)))
 
-(defn flatmapseq [^rx.Observable o]
+(defn flatmapseq [^Observable o]
   (.flatMapIterable o (interop/fn [seq] seq)))
 
-(defn observe-for-computation [^rx.Observable o]
+(defn observe-for-computation [^Observable o]
   (.observeOn o (Schedulers/computation)))
 
-(defn observe-for-io [^rx.Observable o]
+(defn observe-for-io [^Observable o]
   (.observeOn o (Schedulers/io)))
 
 (defn subscribe-on-io
-  ([^rx.Observable o on-next-action]
+  ([^Observable o on-next-action]
    (rx/subscribe
     (subscribe-on-io o)
     on-next-action))
-  ([^rx.Observable o]
+  ([^Observable o]
    (rx/subscribe-on (Schedulers/io) o)))
 
-(defn shared-latest [^rx.Observable o]
+(defn shared-latest [^Observable o]
   "Returns a `rx.Observable' that publishes the latest value of the source sequence
    while sharing a single subscription as long as there are subscribers."
   (.. o (replay 1) refCount))
 
-(defn latest [^rx.Observable o]
+(defn latest [^Observable o]
   (doto (. o (replay 1))
     .connect))
 
@@ -70,7 +71,7 @@
 
 (defn combine-latest [f ^java.util.List os]
   (let [^rx.functions.FuncN fn (func-n f)]
-    (rx.Observable/combineLatest os fn)))
+    (Observable/combineLatest os fn)))
 
 (defn switch-map [f ^rx.Observable o]
   (.switchMap o (interop/fn [x] (f x))))
