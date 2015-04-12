37551b7d72ad9c778ed318e2fe1d987df2e109ac
Klaus Wuestefeld <klauswuestefeld@gmail.com>
2015-04-12 03:18:30 -0300

    Minor

--- java-api/src/sneer/Conversation.java
+++ java-api/src/sneer/Conversation.java
@@ -13,18 +13,17 @@ public interface Conversation {
 // INVITES-TODO: Add this: For now, the UI should disable message sending based on this. We cannot reify two different instances of Conversation. It has to be the same instance, reacting according to it's contact having a party or not (open invite).
 //  /** @return Emits false when messages cannot be sent (contact() doesn't have a party yet because it is an open invite). */
 	Observable<Boolean> canSendMessages();
-
 	void sendMessage(String label);
 
 	Observable<List<Message>> messages();
-	Observable<List<Message>> unreadMessages();
-	Observable<Long> mostRecentMessageTimestamp();
 	Observable<String> mostRecentMessageContent();
-	
+	Observable<Long> mostRecentMessageTimestamp();
 
-	Observable<List<ConversationMenuItem>> menu();
-	
+	Observable<List<Message>> unreadMessages();
 	Observable<Long> unreadMessageCount();
 
 	void setRead(Message last);
+
+	Observable<List<ConversationMenuItem>> menu();
+
 }
