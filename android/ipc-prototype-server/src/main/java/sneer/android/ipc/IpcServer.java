8cc469bd8add1026470194f4b59cf9b20e92d70c
Paulo Peccin <peccin@objective.com.br>
2015-04-14 19:26:19 -0300

    main: ipc: Plugin launch dialog with icons

--- android/ipc-prototype-server/src/main/java/sneer/android/ipc/IpcServer.java
+++ android/ipc-prototype-server/src/main/java/sneer/android/ipc/IpcServer.java
@@ -1,20 +1,24 @@
 package sneer.android.ipc;
 
+import android.app.AlertDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
-import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
-import android.widget.PopupMenu;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.List;
 
 
 public class IpcServer extends ActionBarActivity {
-
-    private PopupMenu launchMenu;
 
     @Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -29,44 +33,66 @@ public class IpcServer extends ActionBarActivity {
 		return true;
 	}
 
-    @Override
-    protected void onPostCreate(Bundle savedInstanceState) {
-        super.onPostCreate(savedInstanceState);
-        Button button = (Button) findViewById(R.id.bManda);
-        launchMenu = new PopupMenu(this, button);
-    }
 
     @Override
 	public boolean onOptionsItemSelected(MenuItem item) {
 		int id = item.getItemId();
-
 		if (id == R.id.action_settings) return true;
-
 		return super.onOptionsItemSelected(item);
 	}
 
 
     public void launchInstalledPlugins(View view) {
-        Menu menu = launchMenu.getMenu();
-
-        menu.close();
-        menu.clear();
-
-        for (final Plugin p : InstalledPlugins.all(this)) {
-            menu.add(p.caption).setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
-                Plugin plugin = p;
-                @Override
-                public boolean onMenuItemClick(MenuItem item) {
-                    launch(IpcServer.this, p);
-                    return true;
-                }
-            });
-        }
-
-        launchMenu.show();
+        AlertDialog dialog = createPluginsDialog();
+        dialog.show();
     }
 
-	private void launch(Context context, Plugin p) {
+    private AlertDialog createPluginsDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+        builder.setTitle("Launch SneerApp");
+
+        final List<Plugin> plugins = InstalledPlugins.all(this);
+
+        final LayoutInflater inflater = getLayoutInflater();
+        ListView listView = (ListView) inflater.inflate(R.layout.plugins_list, null);
+
+        ArrayAdapter<Plugin> adapter = new ArrayAdapter<Plugin>(this, R.layout.plugins_list_item, plugins) {
+            @Override
+            public View getView(int position, View convertView, ViewGroup parent) {
+                TextView textView;
+                if (convertView != null)
+                    textView = (TextView) convertView;
+                else
+                    textView = (TextView) inflater.inflate(R.layout.plugins_list_item, null);
+
+                Plugin plugin = plugins.get(position);
+                textView.setText(plugin.caption);
+
+                plugin.icon.setBounds(0, 0, 84, 84);    // TODO Not a good solution
+                textView.setCompoundDrawables(plugin.icon, null, null, null);
+
+                return textView;
+            }
+        };
+        listView.setAdapter(adapter);
+
+        AdapterView.OnItemClickListener clickListener = new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                launch(IpcServer.this, plugins.get(position));
+            }
+        };
+        listView.setOnItemClickListener(clickListener);
+
+        AlertDialog dialog = builder.create();
+        dialog.setView(listView, 0, 0, 0, 0);
+
+        return dialog;
+    }
+
+
+    private void launch(Context context, Plugin p) {
 		Intent intent = new Intent();
         intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         intent.setClassName(p.packageName, p.activityName);
