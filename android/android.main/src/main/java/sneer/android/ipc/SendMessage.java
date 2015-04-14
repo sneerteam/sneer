308ddbca43edac921cb4454120667d5653a33ac5
Klaus Wuestefeld <klauswuestefeld@gmail.com>
2015-04-14 19:09:06 -0300

    Main: SendMessage service ready to be integrated.

--- android/android.main/src/main/java/sneer/android/ipc/SendMessage.java
+++ android/android.main/src/main/java/sneer/android/ipc/SendMessage.java
@@ -2,29 +2,52 @@ package sneer.android.ipc;
 
 import android.app.IntentService;
 import android.content.Intent;
-import android.os.Handler;
-import android.widget.Toast;
+import android.util.Log;
+
+import sneer.Contact;
+import sneer.Conversation;
+import sneer.Party;
+import sneer.PublicKey;
+
+import static sneer.android.SneerAndroidSingleton.sneer;
+import static sneer.android.SneerAndroidSingleton.sneerAndroid;
 
 public class SendMessage extends IntentService {
 
-	public static final String SEND_MESSAGE       = "SEND_MESSAGE";
-	public static final String CONVERSATION_TOKEN = "CONVERSATION_TOKEN";
+	public static final String TOKEN = "TOKEN";
 
 	public SendMessage() {
         super("SendMessage");
     }
 
 
+	static Intent intentFor(Conversation convo) {
+		return new Intent()
+			.setClassName("me.sneer", SendMessage.class.getName())
+			.putExtra(TOKEN, convo.contact().party().current().publicKey().current().toHex());
+	}
+
+
     @Override
     protected void onHandleIntent(Intent intent) {
-        if (intent == null) return;
-
-        final String message = intent.getStringExtra(CONVERSATION_TOKEN) + " " + intent.getAction();
-
-        new Handler(getMainLooper()).post(new Runnable() { @Override public void run() {
-            Toast.makeText(getApplicationContext(), "" + message, Toast.LENGTH_SHORT).show();
-        }});
-
+		if (intent == null) return;
+		try {
+			tryToHandle(intent);
+		} catch (RuntimeException x) {
+			Log.d(getClass().getName(), "Unable to send message", x);
+        }
     }
+
+
+	private void tryToHandle(Intent intent) {
+		String pukHex  = intent.getStringExtra(TOKEN);
+		String message = intent.getAction();
+
+		PublicKey puk = sneerAndroid().admin().keys().createPublicKey(pukHex);
+		Party party = sneer().produceParty(puk);
+		Contact contact = sneer().findContact(party);
+
+		sneer().conversations().withContact(contact).sendMessage(message);
+	}
 
 }
