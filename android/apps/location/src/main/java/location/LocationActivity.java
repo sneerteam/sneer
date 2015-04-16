8cb9b9c2ce768f45fb09546d8e446cd55316bb5b
Paulo Peccin <peccin@objective.com.br>
2015-04-16 18:44:36 -0300

    Location: Done

--- android/apps/location/src/main/java/location/LocationActivity.java
+++ android/apps/location/src/main/java/location/LocationActivity.java
@@ -1,23 +1,104 @@
 package location;
 
 import android.app.Activity;
-import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
-import android.os.Parcelable;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.ppeccin.sneer.location.R;
 
+import static android.location.LocationManager.GPS_PROVIDER;
 
-public class LocationActivity extends Activity {
 
-	@Override
+public class LocationActivity extends Activity implements LocationListener {
+
+    private LocationManager locationManager;
+    private Location latestLocation;
+    private TextView textAccuracy;
+    private Button sendButton;
+
+
+    @Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-
-        String msg = "LOCATION : " + System.currentTimeMillis();
-        startService(getIntent().<Intent>getParcelableExtra("SEND_MESSAGE").setAction(msg));
+        textAccuracy = (TextView) findViewById(R.id.textAccuracy);
+        sendButton = (Button) findViewById(R.id.buttonSend);
+        sendButton.setEnabled(false);
     }
 
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        if (!locationManager.isProviderEnabled(GPS_PROVIDER))
+            textAccuracy.setText("GPS is off.");
+        else
+            locationManager.requestLocationUpdates(GPS_PROVIDER, 1000, 0, this);
+    }
+
+
+    @Override
+    protected void onPause() {
+        locationManager.removeUpdates(this);
+        finish();
+        super.onPause();
+    }
+
+
+    public void onSendClicked(View view) {
+        Intent sendMessageIntent = getIntent().<Intent>getParcelableExtra("SEND_MESSAGE");
+        if (sendMessageIntent != null) {
+            String url = "Location:\nhttps://google.com/maps/place/" + latestLocation.getLatitude() + "," + latestLocation.getLongitude();
+            startService(sendMessageIntent.setAction(url));
+        }
+        finish();
+    }
+
+
+    public void onCancelClicked(View view) {
+        finish();
+    }
+
+
+    @Override
+    public void onLocationChanged(Location location) {
+        latestLocation = location;
+        updateTextAccuracy();
+    }
+
+    private void updateTextAccuracy() {
+        Log.d(">>>> LocationChanged: ", latestLocation.toString());
+
+        textAccuracy.post(new Runnable() {
+            @Override
+            public void run() {
+                sendButton.setEnabled(true);
+                textAccuracy.setText("Accuracy " + (int) latestLocation.getAccuracy() + " meters");
+            }
+        });
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+    @Override
+    public void onProviderDisabled(String provider) {
+    }
 }
+
+
+
