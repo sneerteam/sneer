ed2181bf0ae11514efcf90ab6c2e38be1c143433
Klaus Wuestefeld <klauswuestefeld@gmail.com>
2015-04-24 19:24:14 -0300

    Minor

--- java-api/src/sneer/ConversationItem.java
+++ java-api/src/sneer/ConversationItem.java
@@ -3,19 +3,19 @@ package sneer;
 import sneer.tuples.Tuple;
 
 public interface ConversationItem {
-	boolean isOwn();
-	long id();
 	PublicKey author();
+	boolean isOwn();
+
 	String type();
+
 	String label();
 	byte[] jpegImage();
 
-	/** When this message was created. */
 	long timestampCreated();
-	/** When this message was received. */
 	long timestampReceived();
 	/** Pretty representation of the time created. */
 	String timeCreated();
 
+	long id();
 	Tuple tuple();
 }
