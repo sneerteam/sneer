3415dad22e173d96f7098ff8731ee7712ab7f1c6
Klaus Wuestefeld <klauswuestefeld@gmail.com>
2015-05-03 02:49:55 -0300

    Java-api: Container: Allows components to load others using: Container.of(this).produce(OtherComponent.class)

--- java-api/src/sneer/commons/Container.java
+++ java-api/src/sneer/commons/Container.java
@@ -1,42 +1,64 @@
 package sneer.commons;
 
-import java.lang.Class;import java.lang.ClassNotFoundException;import java.lang.Exception;import java.lang.Object;import java.lang.RuntimeException;import java.lang.String;import java.util.HashMap;
+import java.lang.Class;
+import java.lang.Object;
+import java.util.HashMap;
 import java.util.Map;
+import java.util.WeakHashMap;
+
+import static sneer.commons.exceptions.Exceptions.check;
 
 
 public class Container {
 
+	static private final WeakHashMap<Object, Container> containersByComponent = new WeakHashMap<Object, Container>();
+
+	/** Useful for components to load others using: Container.of(this).produce(OtherComponent.class) */
+	synchronized
+	static public Container of(Object component) {
+		Container ret = containersByComponent.get(component);
+		if (ret == null) throw new IllegalArgumentException("Unable to find single container of " + component + " (it is contained by none or more than one container)." );
+
+		return ret;
+	}
+
+
 	private final ComponentLoader loader;
-	private final Map<Class<?>, Object> instancesByInterface = new HashMap<Class<?>, Object>();
+	private final Map<Class<?>, Object> componentsByInterface = new HashMap<Class<?>, Object>();
 
 	public Container(ComponentLoader loader) {
 		this.loader = loader;
 	}
 
-	synchronized
 	public <T> T produce(Class<T> componentInterface) {
-		T cached = (T)instancesByInterface.get(componentInterface);
-		if (cached != null) return cached;
+		synchronized (Container.class) {
+			T cached = (T) componentsByInterface.get(componentInterface);
+			if (cached != null) return cached;
 
-		T loaded = loader.load(componentInterface);
-		instancesByInterface.put(componentInterface, loaded);
-		return loaded;
+			T loaded = loader.load(componentInterface);
+			keep(componentInterface, loaded);
+			return loaded;
+		}
 	}
 
 
-	interface ComponentLoader {
+	public void inject(Class<?> componentInterface, Object component) {
+		synchronized (Container.class) {
+			check(componentInterface.isAssignableFrom(component.getClass()));
+			check(!componentsByInterface.containsKey(componentInterface));
+
+			keep(componentInterface, component);
+		}
+	}
+
+	private void keep(Class<?> componentInterface, Object component) {
+		componentsByInterface.put(componentInterface, component);
+		containersByComponent.put(component, this);
+	}
+
+
+	public interface ComponentLoader {
 		<T> T load(Class<T> componentInterface);
-	}
-
-
-	public static ComponentLoader withPadding(final String prefix, final String suffix) {
-		return new ComponentLoader() { @Override public <T> T load(Class<T> intrface) {
-			try {
-				return (T)Class.forName(prefix + intrface.getName() + suffix).newInstance();
-			} catch (Exception e) {
-				throw new RuntimeException(e);
-			}
-		}};
 	}
 
 }
