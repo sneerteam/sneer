33c90cca42d4e50981d509f86d6b505aaac791c0
Felipe Bueno <bueno.felipe@gmail.com>
2015-05-06 17:20:11 -0300

    android-api: new IPCProtocol

--- android/android-api/src/main/java/sneer/android/impl/IPCProtocol.java
+++ android/android-api/src/main/java/sneer/android/impl/IPCProtocol.java
@@ -2,17 +2,7 @@ package sneer.android.impl;
 
 public class IPCProtocol {
 
-	//Message
-	public static final String LABEL = "label";
-	public static final String PAYLOAD = "payload";
-    public static final String JPEG_IMAGE = "jpeg-image";
-	public static final String RESULT_RECEIVER = "result";
-
-	//Session
-	public static final String PARTNER_NAME = "partnerName";
-	public static final String OWN = "own";
-	public static final String REPLAY_FINISHED = "replayFinished";
-	public static final String ERROR = "error";
-	public static final String UNSUBSCRIBE = "unsubscribe";
+	public static final String JOIN_SESSION = "JOIN_SESSION";
+	public static final String ENVELOPE = "ENVELOPE";
 
 }
