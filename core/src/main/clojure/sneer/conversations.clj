3837ed78c5b2db24f6297cacc2715327186020db
Klaus Wuestefeld <klauswuestefeld@gmail.com>
2015-05-19 00:38:10 -0300

    Core: Conversations summarization processes all tuples.

--- core/src/main/clojure/sneer/conversations.clj
+++ core/src/main/clojure/sneer/conversations.clj
@@ -3,12 +3,13 @@
     [clojure.core.async :refer [go chan close! <! >! sliding-buffer]]
     [rx.lang.clojure.core :as rx]
     [sneer.async :refer [go-trace link-chan-to-subscriber thread-chan-to-subscriber]]
-    [sneer.commons :refer [now produce!]]
+    [sneer.commons :refer [now produce! flip]]
+    [sneer.clojure.core :refer [nvl]]
     [sneer.contact :refer [get-contacts puk->contact]]
     [sneer.conversation :refer :all]
     [sneer.rx :refer [atom->observable subscribe-on-io latest shared-latest combine-latest switch-map behavior-subject]]
     [sneer.party :refer [party->puk]]
-    [sneer.tuple.persistent-tuple-base :as ptb]
+    [sneer.tuple.persistent-tuple-base :as tb]
     [sneer.tuple.protocols :refer :all]
     [sneer.tuple.space :refer [payload]]
     [sneer.tuple-base-provider :refer :all])
@@ -20,101 +21,63 @@
     [sneer.conversations ConversationList ConversationList$Summary ConversationList]
     [sneer.rx ObservedSubject]))
 
-(defn create-summarization-state []
-  {})
-
 (defn- contact-puk [tuple]
   (tuple "party"))
 
-(defn update-contact [contact state]
+(defn- update-contact [contact state]
   (let [contact-id (contact-puk contact)
         contact-name (contact "payload")
         timestamp (contact "timestamp")]
-;    (println "Update contact:" contact)
-    (assoc state contact-id {:name contact-name :preview "" :timestamp timestamp :unread ""})))
+    (if contact-id
+      (-> state
+          (assoc-in  [contact-id :name] contact-name)
+          (update-in [contact-id :timestamp] #(Math/max (or % 0) timestamp))
+          (update-in [contact-id :preview] nvl "")
+          (update-in [contact-id :unread ] nvl ""))
+      state)))
 
-(defn unread-status [label]
-  (if (.contains label "?") "?" "*"))
+(defn- unread-status [label old-status]
+  (cond
+    (= old-status "?")    "?"
+    (.contains label "?") "?"
+    :else                 "*"))
 
-(defn update-message [own-puk message state]
+(defn- update-message [own-puk message state]
   (let [author (message "author")
         label (message "label")
         contact-puk (if (= author own-puk) (message "audience") author)]
-;    (println "Update message:" message)
     (-> state (update contact-puk assoc
                 :preview   (or label "")
                 :timestamp (message "timestamp"))
-              (assoc-in [contact-puk :unread] (unread-status label)))))
+              (update-in [contact-puk :unread] #(unread-status label %)))))
 
-(defn flip [f]
-  (fn [x y] (f y x)))
-
-(defn summarize [state]
+(defn- summarize [state]
   (->> state
        vals
+       (filter :name)
        (sort-by :timestamp (flip compare))
        vec))
 
-(defn summarization-state-from-contacts [contacts]
-  (reduce
-   (flip update-contact)
-   (create-summarization-state)
-   contacts))
-
 (defn start-summarization-machine [own-puk tuple-base summaries-out lease]
-  (let [tuples (chan)]
+  (let [tuples (chan)
+        query-tuples (fn [criteria]
+                       (query-tuples tuple-base criteria tuples lease))]
+    (query-tuples {"type" "contact" "author" own-puk "audience" own-puk})
+    (query-tuples {"type" "message"})
 
-    ; link machine lifetime to lease
-    (go (<! lease)
-        (close! tuples))
+    (go
+      ; link machine lifetime to lease
+      (<! lease)
+      (close! tuples))
 
     (go-trace
-
-     ; create initial summarization state based on all contacts
-     (let [contact-query {"type" "contact" "author" own-puk "audience" own-puk}
-           existing-contacts (<! (ptb/query-all tuple-base contact-query))
-           state (summarization-state-from-contacts existing-contacts)
-
-           last-contact-id (-> existing-contacts last (get "id"))
-           existing-contacts nil
-
-           ; convenience functions
-           query-tuples (fn [criteria]
-                          (query-tuples tuple-base criteria tuples lease))
-
-           query-messages (fn [contact-puk]
-                            (let [message-query {"type" "message" ptb/last-by-id true}]
-                              (query-tuples (assoc message-query "audience" own-puk "author" contact-puk))
-                              ; there's a subtle issue here, since we are emitting two separate queries onto the same
-                              ; channel there's no guarantee the tuples will be ordered by id
-                              ; a good solution would be to extend query-tuples criteria to support :or clauses
-                              (query-tuples (assoc message-query "author" own-puk "audience" contact-puk))))]
-
-       ; keep an eye on messages from existing contacts
-       (doseq [contact-puk (keys state)]
-         (query-messages contact-puk))
-
-       ; query new contact tuples
-       (query-tuples (assoc contact-query ptb/after-id last-contact-id))
-
-       ; update conversation summaries
-       (loop [state state]
-         (>! summaries-out (summarize state))
-         (when-some [tuple (<! tuples)]
-           (case (tuple "type")
-             "contact"
-             (if-some [puk (contact-puk tuple)]
-               (do
-                 ; keep an eye on messages from new contact
-                 (query-messages puk)
-                 ; update
-                 (recur (update-contact tuple state)))
-               ; ignore contacts without a party for now
-               (recur state))
-
-             "message"
-             (recur
-              (update-message own-puk tuple state)))))))))
+      (loop [state {}]
+        (>! summaries-out (summarize state))
+        (when-some [tuple (<! tuples)]
+          (recur
+            (case (tuple "type")
+              "contact" (update-contact tuple state)
+              "message" (update-message own-puk tuple state))))))))
 
 
 ; Java interface
