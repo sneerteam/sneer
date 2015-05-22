0d9c438c8bf015cd25a60a1128f4ad9e2a0343ab
Klaus Wuestefeld <klauswuestefeld@gmail.com>
2015-05-22 18:55:33 -0300

    Minor

--- core/src/test/clojure/mvstore/mvstore_test.clj
+++ core/src/test/clojure/mvstore/mvstore_test.clj
@@ -2,22 +2,32 @@
   (:require [midje.sweet :refer :all])
   (:import
     [org.h2.mvstore MVStore]
-    (java.util.concurrent Semaphore)))
+    (java.net DatagramPacket)
+    (java.io File)))
 
 (facts "MVStore Works"
-  (let [in-memory nil
-        store (MVStore/open in-memory)
-        map1 (.openMap store "map1")]
+  (let [filename (str "//sneer/lixo" (System/currentTimeMillis))]
+    (with-open [store (MVStore/open filename)]
+      (let [map1 (.openMap store "map-name")]
 
-    (fact "Map put and get work."
-      (.get map1 "key") => nil
-      (.put map1 "key" "value")
-      (.get map1 "key") => "value")
+        (fact "Map put and get work."
+          (.get map1 "key") => nil
+          (.put map1 "key" "value")
+          (.get map1 "key") => "value")
 
-    (fact "Arrays work as keys"
-      (.put map1 (.getBytes "key") "value")
-      (.get map1 (.getBytes "key")) => "value")
+        (fact "Arrays work as keys"
+          (.put map1 (.getBytes "key2") "value2")
+          (.get map1 (.getBytes "key2")) => "value2")
 
-    (fact "Serializable objects can be stored"
-      (.put map1 "key" (Semaphore. 42))
-      (-> map1 (.get "key") .availablePermits) => 42)))
+        (fact "Serializable objects can be stored"
+          (.put map1 "key3" {:a :b})
+          (.get map1 "key3") => {:a :b})))
+
+    (with-open [store (MVStore/open filename)]
+      (let [map1 (.openMap store "map-name")]
+        (fact "Map is persistent"
+          (.get map1 "key") => "value"
+          (.get map1 (.getBytes "key2")) => "value2"
+          (.get map1 "key3") => {:a :b})))
+
+    (-> filename File. .delete) => true))
