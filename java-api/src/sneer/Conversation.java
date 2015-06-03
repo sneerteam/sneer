751c295ebd2f28df0a9370a97b550fa38bd6d649
Klaus Wuestefeld <klauswuestefeld@gmail.com>
2015-06-03 01:56:01 -0300

    Minor

--- java-api/src/sneer/Conversation.java
+++ java-api/src/sneer/Conversation.java
@@ -7,11 +7,10 @@ import sneer.rx.Observed;
 
 public interface Conversation {
 
-//	/** @return The Contact having this conversation with us. */
+	/** @return The Contact having this conversation with us. */
 	Contact contact();
 
-// INVITES-TODO: Add this: For now, the UI should disable message sending based on this. We cannot reify two different instances of Conversation. It has to be the same instance, reacting according to it's contact having a party or not (open invite).
-//  /** @return Emits false when messages cannot be sent (contact() doesn't have a party yet because it is an open invite). */
+	/** @return Emits false when messages cannot be sent (contact() doesn't have a party yet because it is an open invite). */
 	Observable<Boolean> canSendMessages();
 	void sendMessage(String label);
 
