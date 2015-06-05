ab26ca753f2c9050d799c30c941ca10d7978313c
Klaus Wuestefeld <klauswuestefeld@gmail.com>
2015-06-05 02:59:07 -0300

    Core: integration-test: fails intermittently: ConversationList.summaries fails to emit a value sometimes.

--- core/src/test/clojure/sneer/integration_test.clj
+++ core/src/test/clojure/sneer/integration_test.clj
@@ -1,32 +1,44 @@
 (ns sneer.integration-test
   (:require [midje.sweet :refer :all]
-            [sneer.test-util :refer [->chan emits tmp-folder]])
+            [sneer.admin :refer [new-sneer-admin]]
+            [sneer.test-util :refer [->chan emits tmp-folder]]
+            [sneer.tuple.jdbc-database :refer [create-sqlite-db]]
+            [sneer.tuple.persistent-tuple-base :as tb])
   (:import [sneer.conversations ConversationList]
            [clojure.lang IFn]
            [sneer.commons Container PersistenceFolder]
-           [sneer.impl CoreLoader]))
+           [sneer.impl CoreLoader]
+           (sneer.crypto.impl KeysImpl)
+           (sneer.admin SneerAdmin)
+           (sneer Sneer)))
 
 ; (do (require 'midje.repl) (midje.repl/autotest))
 
-(defn container! []
+(defn- create-persistence-folder! []
+  (let [folder (tmp-folder)]
+    (reify PersistenceFolder
+      (get [_] folder))))
+
+(defn create-admin! []
+  (let [db (create-sqlite-db)
+        tb (tb/create db)]
+    (new-sneer-admin (.createPrivateKey (KeysImpl.)) tb)))
+
+(defn- inject-sneer! [container]
+  (let [admin (create-admin!)]
+    (.inject container PersistenceFolder (create-persistence-folder!))
+    (.inject container SneerAdmin admin)
+    (.inject container Sneer (.sneer admin))))
+
+(defn- sneer-container! []
   (let [delegate (Container. (CoreLoader.))]
-    (.inject delegate PersistenceFolder
-             (let [folder (tmp-folder)] (reify PersistenceFolder
-                                          (get [_] folder))))
+    (inject-sneer! delegate)
+    (reify IFn (invoke [_ component]
+                 (.produce delegate component)))))
 
-;    neide-db (create-sqlite-db)
-;    neide-tb (tb/create neide-db)
-;    neide-admin (new-sneer-admin (.createPrivateKey (KeysImpl.)) neide-tb)
-;    neide-puk (.. neide-admin privateKey publicKey)
-;    neide-sneer (.sneer neide-admin)
+(facts "Sneer as a whole"
+  (let [neide (sneer-container!)]
+    (. (neide ConversationList) summaries) => (emits #(.isEmpty %)))
+  ;(. (neide Contacts) problemWithNewNickname) => nil
+  )
 
-
-    (reify
-      IFn
-      (invoke [_ component]
-        (.produce delegate component)))))
-
-(def neide (container!))
-
-#_(facts "Sneer as a whole"
-  (. (neide ConversationList) summaries) => (emits #(.isEmpty %)) )
