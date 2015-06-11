a6418eada0ebf4417be2cd6c50a69a4b4f7d4a6d
Klaus Wuestefeld <klauswuestefeld@gmail.com>
2015-06-11 02:12:49 -0300

    Core: Convo started

--- core/src/main/clojure/sneer/convo.clj
+++ core/src/main/clojure/sneer/convo.clj
@@ -1,20 +1,29 @@
 (ns sneer.convo
-  #_(:require
-    [clojure.core.async :refer [go chan]])
+  (:require [rx.lang.clojure.core :as rx]
+            [sneer.async :refer [pipe-to-subscriber!]]
+            [sneer.chat :refer [reify-Chat]]
+            [sneer.rx :refer [shared-latest]])
   (:import
-    [sneer.convos Convo]))
+    [sneer.convos Convo]
+    [rx Subscriber]))
 
-(defn reify-Convo [id]
-  (reify Convo
-    ;Observable<String> nick();
-    (nick [_])
+(defn nick-obs* [nick-ch]
+  (shared-latest
+    (rx/observable*
+      (fn [^Subscriber subscriber]
+        (pipe-to-subscriber! nick-ch subscriber "convo nick")))))
 
-    ;/** @return null if invite already accepted
-    ;*/Observable<String> inviteCodePending ()
-    (inviteCodePending [_])
+(defn reify-Convo [contact-puk nick-ch]
+  (let [nick-obs (nick-obs* nick-ch)]
+    (reify Convo
+      (nick [_] nick-obs)
 
-    ;Chat chat ()
-    (chat [_])
+      ;/** @return null if invite already accepted
+      ;*/Observable<String> inviteCodePending ()
+      (inviteCodePending [_])
 
-    ;Observable<List<SessionSummary>>
-    (sessionSummaries [_])))
+      (chat [_]
+        (reify-Chat contact-puk))
+
+      ;Observable<List<SessionSummary>>
+      (sessionSummaries [_]))))
