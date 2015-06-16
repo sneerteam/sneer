5fd615649f030f2db70d993e29a88db9fccbd32d
Felipe Bueno <bueno.felipe@gmail.com>
2015-06-16 17:26:08 -0300

    minor

--- android/apps/location/src/main/java/location/MainActivity.java
+++ android/apps/location/src/main/java/location/MainActivity.java
@@ -2,23 +2,12 @@ package location;
 
 import android.app.Activity;
 import android.app.AlertDialog;
-import android.app.ProgressDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.location.Location;
-import android.location.LocationManager;
 import android.os.Bundle;
-import android.widget.Button;
-import android.widget.TextView;
 
 
 public class MainActivity extends Activity {
-
-    private LocationManager locationManager;
-    private Location latestLocation;
-    private TextView textAccuracy;
-    private Button sendButton;
-
 
     @Override
 	protected void onCreate(Bundle savedInstanceState) {
