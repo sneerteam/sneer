dbfa499404fa6f6f752a0f13429995ba6b15ab73
Rodrigo B. de Oliveira <rbo@acm.org>
2015-06-19 18:11:41 -0300

    [convo] handle contact tuples

--- core/src/main/clojure/sneer/convo.clj
+++ core/src/main/clojure/sneer/convo.clj
@@ -11,28 +11,52 @@
     [sneer.convos Convo]
     [rx Subscriber]))
 
+(defn- tuple-base [container]
+  (tuple-base-of (.produce container sneer.admin.SneerAdmin)))
+
+(defn- query-convo-tuples [container starting-id tuples]
+  ; contact, push (invite accept), message
+  (query-tuples (tuple-base container) {after-id starting-id} tuples))
+
 ; public Convo(String nick, String inviteCodePending, Chat chat, List<SessionSummary> sessionSummaries)
 ; public SessionSummary(long id, String type, String title, String date, String unread)
 ; interface Chat { List<Message> messages(); }
 ; public Message(long id, String text, boolean isOwn, String date)
 (defn- to-foreign [state]
-  (Convo. "Maico" "" nil nil))
+  (Convo. (state :nick) "" nil nil))
+
+(defn- update-nick [state tuple]
+  (some->> (tuple "payload")
+           (assoc state :nick)))
+
+(defn- update-contact-with-puk [state tuple]
+  (if (= (state :puk) (tuple "party"))
+    (update-nick state tuple)
+    state))
+
+(defn- update-contact-without-puk [state tuple]
+  (if (= (state :id) (tuple "id"))
+    (update-nick state tuple)
+    (if (= (state :nick) (tuple "payload"))
+      (assoc state :puk (tuple "party"))
+      state)))
+
+(defn- handle-contact [state tuple]
+  (if (contains? state :puk)
+    (update-contact-with-puk state tuple)
+    (update-contact-without-puk state tuple)))
 
 (defn- handle-tuple [state tuple]
-  (assoc state :id (tuple "id")))
-
-(defn- tuple-base [container]
-  (tuple-base-of (.produce container sneer.admin.SneerAdmin)))
+  (->
+   (case (tuple "type")
+     "contact" (handle-contact state tuple)
+     state)
+   (assoc :last-id (tuple "id"))))
 
 (defn- catch-up [container id]
   (let [tuples (chan)]
-    (query-tuples (tuple-base container) {after-id (dec id)} tuples)
-    (go-loop-trace [state {}
-                    tuple (<! tuples)]
-      (if (some? tuple)
-        (recur (handle-tuple state tuple)
-               (<! tuples))
-        state))))
+    (query-convo-tuples container (dec id) tuples)
+    (async/reduce handle-tuple {:id id} tuples)))
 
 (defn- query-remaining-tuples [container last-id lease]
   (let [tuples (chan)]
@@ -43,9 +67,11 @@
   "`id' is the id of the first contact tuple for this party"
   [container id convo-ch lease]
   (go-trace
+    ; don't emit historical snapshots upon startup
     (let [state (<! (catch-up container id))]
       (>! convo-ch state)
-      (let [taps (state-machine state handle-tuple (query-remaining-tuples container (:id state) lease))]
+      (let [events (query-remaining-tuples container (:last-id state) lease)
+            taps (state-machine state handle-tuple events)]
         (>! taps convo-ch)
         (<! lease)))))
 
