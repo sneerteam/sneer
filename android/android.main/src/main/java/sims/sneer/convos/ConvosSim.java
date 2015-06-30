2870ee066d2e5a7232aef53057754ceb55acb31a
Klaus Wuestefeld <klauswuestefeld@gmail.com>
2015-06-30 17:56:10 -0300

    Recovered ConvosSim

--- android/android.main/src/main/java/sims/sneer/convos/ConvosSim.java
+++ android/android.main/src/main/java/sims/sneer/convos/ConvosSim.java
@@ -1,21 +1,19 @@
 package sims.sneer.convos;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
-import java.util.concurrent.TimeUnit;
 
 import rx.Observable;
-import rx.android.schedulers.AndroidSchedulers;
-import rx.functions.Func1;
 import rx.subjects.BehaviorSubject;
 import sneer.commons.exceptions.FriendlyException;
+import sneer.convos.ChatMessage;
 import sneer.convos.Convo;
 import sneer.convos.Convos;
 import sneer.convos.SessionSummary;
+import sneer.convos.Summary;
 
-import static java.util.concurrent.TimeUnit.SECONDS;
 
-COnvosAc
 @SuppressWarnings("unused")
 public class ConvosSim implements Convos {
 
@@ -30,10 +28,11 @@ public class ConvosSim implements Convos {
 	}
 
 	@Override
-	public String problemWithNewNickname(String newNick) {
-		if (newNick.isEmpty()) return "cannot be empty";
-		if (newNick.equals("Neide")) return "is already a contact";
-		return null;
+	public Observable<String> problemWithNewNickname(String newNick) {
+		String ret = null;
+		if (newNick.isEmpty()) ret = "cannot be empty";
+		if (newNick.equals("Neide")) ret = "is already a contact";
+		return Observable.just(ret);
 	}
 
 	@Override
@@ -44,22 +43,25 @@ public class ConvosSim implements Convos {
 		return Observable.just(4242L);
 	}
 
+	@Override
+	public Observable<Long> acceptInvite(String newContactNick, String contactPuk, String inviteCodeReceived) {
+		System.out.println("Convos.acceptInvite: " + newContactNick + " puk: " + contactPuk + " inviteCode: " + inviteCodeReceived);
+		return startConvo(newContactNick);
+	}
+
     @Override
     public Observable<Convo> getById(long id) {
-        String inviteCodePending = id % 2 == 1 ? null : "InviteCode" + id;
-        return inviteCodePending == null
-            ? Observable.just(new Convo("Nicholas", null, new ChatSim(), new ArrayList<SessionSummary>()))
-//            : Observable.just(new Convo("Nicholas", inviteCodePending, new ChatSim(), new ArrayList<SessionSummary>()));
-            : Observable.just(new Convo("Nicholas", inviteCodePending, new ChatSim(), new ArrayList<SessionSummary>())).concatWith(
-                Observable.timer(3, SECONDS, AndroidSchedulers.mainThread()).map(new Func1<Long, Convo>() {
-                    @Override
-                    public Convo call(Long aLong) {
-                        return new Convo("Nicholas", null, new ChatSim(), new ArrayList<SessionSummary>());
-                    }
-                }));
+        return Observable.just(new Convo(id, "Wesley " + id, null, messages(), new ArrayList<SessionSummary>()));
     }
 
-    private static final String[] UNREAD_OPTIONS = {"?", "*", ""};
+	private List<ChatMessage> messages() {
+		return Arrays.asList(
+			new ChatMessage(1, "Yo bro, sup?", false, "Mar 23"),
+			new ChatMessage(2, "My bad, just saw your message", true, "30 mins ago"),
+			new ChatMessage(3, "Hi. Sorry, too late...", false, "15 mins ago"));
+	}
+
+	private static final String[] UNREAD_OPTIONS = {"?", "*", ""};
 	private static String unread(int i) {
 		return UNREAD_OPTIONS[i % 3];
 	}
