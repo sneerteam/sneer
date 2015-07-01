254c3b511dfd9696e40fb59b7f074473acef4971
Klaus Wuestefeld <klauswuestefeld@gmail.com>
2015-07-01 04:32:30 -0300

    java-api: Making contact-id explicit.

--- java-api/src/sneer/convos/Convo.java
+++ java-api/src/sneer/convos/Convo.java
@@ -15,11 +15,11 @@ public class Convo {
 	public final List<ChatMessage> messages;
     public final List<SessionSummary> sessionSummaries;
 
-    private final long id;
+    private final long contactId;
 
-    public Convo(long id, String nickname, String inviteCodePending, List<ChatMessage> messages, List<SessionSummary> sessionSummaries) { this.id = id; this.nickname = nickname; this.inviteCodePending = inviteCodePending; this.messages = messages; this.sessionSummaries = sessionSummaries; }
+    public Convo(long contactId, String nickname, String inviteCodePending, List<ChatMessage> messages, List<SessionSummary> sessionSummaries) { this.contactId = contactId; this.nickname = nickname; this.inviteCodePending = inviteCodePending; this.messages = messages; this.sessionSummaries = sessionSummaries; }
 
-	public Action setNickname(String newNick) { return action("set-nickname", "convo-id", id, "new-nick", newNick); }
-	public Action sendMessage(String text) { return action("send-message", "convo-id", id, "text", text); }
+	public Action setNickname(String newNick) { return action("set-nickname", "contact-id", contactId, "new-nick", newNick); }
+	public Action sendMessage(String text) { return action("send-message", "contact-id", contactId, "text", text); }
 
 }
--- java-api/src/sneer/convos/Convos.java
+++ java-api/src/sneer/convos/Convos.java
@@ -11,13 +11,13 @@ public interface Convos {
 	/** @return null if the new nickname is ok or a reason why the new nickname is not ok (empty or already used by another Contact). */
 	Observable<String> problemWithNewNickname(String newContactNick);
 
-	/** @return The new convo id.
+	/** @return The new contact id.
 	 * @throws sneer.commons.exceptions.FriendlyException (via Observable) (see problemWithNewNickname(newContactNick)). */
 	Observable<Long> startConvo(String newContactNick);
-	/** @return The new convo id.
+	/** @return The new contact id.
 	 * @throws sneer.commons.exceptions.FriendlyException (via Observable) (see problemWithNewNickname(newContactNick)). */
 	Observable<Long> acceptInvite(String newContactNick, String contactPuk, String inviteCodeReceived);
 
-	Observable<Convo> getById(long id);
+	Observable<Convo> getById(long contactId);
 
 }
