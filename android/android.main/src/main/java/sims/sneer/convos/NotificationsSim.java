50f7d56285c6398f16341cbe3385cb1af7aa36f0
Felipe Bueno <bueno.felipe@gmail.com>
2015-07-06 16:06:20 -0300

    java-api: WIP new notification system

---
+++ android/android.main/src/main/java/sims/sneer/convos/NotificationsSim.java
@@ -0,0 +1,7 @@
+package sims.sneer.convos;
+
+/**
+ * Created by felipe on 7/6/15.
+ */
+public class NotificationsSim {
+}
---
+++ java-api/src/sneer/convos/Notifications.java
@@ -0,0 +1,7 @@
+package sneer.convos;
+
+/**
+ * Created by felipe on 7/6/15.
+ */
+public interface Notifications {
+}
