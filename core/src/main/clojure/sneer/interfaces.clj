82abae93521784298fefd4ae39c29a90cb583073
unknown <klauswusetefeld@gmail.com>
2015-07-08 02:42:46 -0300

    Core: interfaces: TODO: standardize Contacts and ConvoSummarization

--- core/src/main/clojure/sneer/interfaces.clj
+++ core/src/main/clojure/sneer/interfaces.clj
@@ -1,5 +1,5 @@
-(ns sneer.interfaces)
+(ns sneer.interfaces) ; TODO: Organize namespaces by intent, rather than programming language stereotype.
 
-(definterface ConvoSummarization
+(definterface ConvoSummarization  ; TODO: Make Contacts and this use the same strategy (interface ou protocol) and make both use interface, protocol or keyword as handle for container.
   (slidingSummaries [])
   (processUpToId [id]))
