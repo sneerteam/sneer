9f5d9cecf003bba69b71405fbeec88eb4578b802
Felipe Bueno <bueno.felipe@gmail.com>
2015-08-04 17:18:09 -0300

    main: fix findConvo observable so we can add contacts again :smile:

--- android/android.main/src/main/java/sneer/android/ui/AcceptInviteActivity.java
+++ android/android.main/src/main/java/sneer/android/ui/AcceptInviteActivity.java
@@ -5,7 +5,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -14,11 +13,12 @@ import android.widget.Toast;
 import rx.Subscriber;
 import rx.functions.Action1;
 import sneer.android.SneerAndroidContainer;
-import sneer.android.utils.AndroidUtils;
 import sneer.convos.Convos;
 import sneer.main.R;
 
 import static sneer.android.ui.SneerActivity.ui;
+import static sneer.android.utils.AndroidUtils.finishWith;
+import static sneer.android.utils.AndroidUtils.toastOnMainThread;
 
 public class AcceptInviteActivity extends Activity {
 
@@ -30,58 +30,57 @@ public class AcceptInviteActivity extends Activity {
     private String contactPuk;
     private String inviteCode;
 
-    @Override
+	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 
-        parseQuery(getIntent());
+		parseQuery(getIntent());
 
-        if (contactPuk == null || inviteCode == null) {
-            AndroidUtils.finishWith("Invalid Invite", this);
+		if (contactPuk == null || inviteCode == null) {
+			finishWith("Invalid Invite", this);
 			return;
 		}
 
 		convos = SneerAndroidContainer.component(Convos.class);
 
-		convos.findConvo(contactPuk).subscribe(new Action1<Long>() {
-			@Override
-			public void call(Long convoId) {
-                if (convoId != null) {
-                    ConvoActivityWithTabs.open(AcceptInviteActivity.this, convoId);
-                    finish();
-                }
+		ui(convos.findConvo(contactPuk)).subscribe(new Action1<Long>() { @Override public void call(Long convoId) {
+			if (convoId != null) {
+				navigateToConvo(convoId);
+			} else {
+				showNicknameDialog();
 			}
-		});
+		}});
+	}
 
+	private void showNicknameDialog() {
 		setContentView(R.layout.activity_add_contact);
-
 
 		nicknameEdit = (EditText) findViewById(R.id.nickname);
 		btnDone = (Button) findViewById(R.id.btn_done);
 
-        btnDone.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                convos.acceptInvite(getNickname(), contactPuk, inviteCode).subscribe(new Subscriber<Long>() {
-                    @Override
-                    public final void onCompleted() {
-                    }
+		btnDone.setOnClickListener(new View.OnClickListener() { @Override public void onClick(View v) {
+			convos.acceptInvite(getNickname(), contactPuk, inviteCode).subscribe(new Subscriber<Long>() {
+				@Override
+				public final void onCompleted() {}
 
-                    @Override
-                    public final void onError(Throwable e) {
-                        AndroidUtils.toastOnMainThread(AcceptInviteActivity.this, e.getMessage(), Toast.LENGTH_LONG);
-                    }
+				@Override
+				public final void onError(Throwable e) {
+					toastOnMainThread(AcceptInviteActivity.this, e.getMessage(), Toast.LENGTH_LONG);
+				}
 
-                    @Override
-                    public final void onNext(Long convoId) {
-                        ConvoActivityWithTabs.open(AcceptInviteActivity.this, convoId);
-                        finish();
-                    }
-                });
-            }
-        });
+				@Override
+				public final void onNext(Long convoId) {
+					navigateToConvo(convoId);
+				}
+			});
+		}});
 
 		setNicknameValidationOnTextChanged(nicknameEdit);
+	}
+
+	private void navigateToConvo(Long convoId) {
+		ConvoActivityWithTabs.open(AcceptInviteActivity.this, convoId);
+		finish();
 	}
 
 	private void setNicknameValidationOnTextChanged(final EditText editText) {
@@ -89,13 +88,9 @@ public class AcceptInviteActivity extends Activity {
 			@Override
 			public void afterTextChanged(Editable s) {
 				nickname = nicknameEdit.getText().toString();
-				Log.d("FELIPETESTE", "nickname->" + nickname);
-
 				ui(convos.problemWithNewNickname(nickname)).subscribe(new Action1<String>() {
 					@Override
 					public void call(String error) {
-						Log.d("FELIPETESTE", "error->" + error);
-
 						refreshNicknameProblem(error);
 					}
 				});
@@ -108,7 +103,6 @@ public class AcceptInviteActivity extends Activity {
     private void refreshNicknameProblem(String error) {
         if (!nickname.isEmpty() && error != null) nicknameEdit.setError(error);
         btnDone.setEnabled(!nickname.isEmpty() && error == null);
-		Log.d("FELIPETESTE", "btnDone.isEnabled?->" + btnDone.isEnabled());
 	}
 
     private void parseQuery(Intent intent) {
