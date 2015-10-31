240f20658897819d334089faa5394d268665204e
Klaus Wuestefeld <klauswuestefeld@gmail.com>
2015-10-31 01:26:38 -0200

    Minor

--- reset-environment.sh
+++ reset-environment.sh
@@ -4,6 +4,5 @@
 
 cd core       && ../gradlew clean check install && cd .. || exit -1
 cd server     && ../gradlew clean check install && cd .. || exit -1
-cd core.tests && ../gradlew clean check install && cd .. || exit -1
 
 #Reset environment successful
