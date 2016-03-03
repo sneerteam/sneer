9e03852d54952f1fa687ecb0b0a0b4d3ac150b34
Klaus Wuestefeld <klauswuestefeld@gmail.com>
2016-03-03 19:55:21 -0300

    core2: Correctly named me.sneer/core2

--- core2/project.clj
+++ core2/project.clj
@@ -1,4 +1,4 @@
-(defproject sneer.core2 "0.0.1-SNAPSHOT"
+(defproject me.sneer/core2 "0.0.1-SNAPSHOT"
   :description "Sneer core features with no IO"
   :dependencies [[org.clojure/clojure "1.8.0"]]
   :profiles {:dev {:dependencies [[midje "1.8.3"]]}})
