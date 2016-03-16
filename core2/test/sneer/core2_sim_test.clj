fb291346b851393d091a565cb47b68ac7cd043bb
Klaus Wuestefeld <klauswuestefeld@gmail.com>
2016-03-16 18:35:57 -0300

    core2: sim tests renamed

--- core2/test/sneer/core2_sim_test.clj
+++ core2/test/sneer/core2_sim_test.clj
@@ -1,20 +1,20 @@
-(ns sneer.core2-test
+(ns sneer.core2-sim-test
   (:use midje.sweet)
   (:use [sneer.core2-sim]))
 
 (facts "UI Sims"
-  (let [ui (atom nil)
-        dispatcher (dispatcher #(reset! ui %))]
+  (let [fake-ui (atom nil)
+        dispatcher (dispatcher #(reset! fake-ui %))]
 
     (fact "Convo sims are generated"
       (+ 1 1) => 2
 
       (dispatch! dispatcher {"event" "sim-next"})
-      @ui => {"view" "convos"
+      @fake-ui => {"view" "convos"
               "convo-list" []}
 
       (dispatch! dispatcher {"event" "sim-next"})
-      @ui => {"view" "convos"
+      @fake-ui => {"view" "convos"
               "convo-list" [{"id"       1000
                              "nickname" "Neide 0"
                              "preview"  "Hi There! 0"
@@ -22,18 +22,18 @@
                              "unread"   ""}]}
 
       (dispatch! dispatcher {"event" "sim-next"})
-      (count (@ui "convo-list")) => 100)
+      (count (@fake-ui "convo-list")) => 100)
 
 
     (fact "Message sims are generated"
       (dispatch! dispatcher {"event" "sim-next"})
-      @ui => {"view" "convo"
+      @fake-ui => {"view" "convo"
               "id" 1042
               "tab" "chat"
               "message-list" []}
 
       (dispatch! dispatcher {"event" "sim-next"})
-      @ui => {"view" "convo"
+      @fake-ui => {"view" "convo"
               "id" 1042
               "tab" "chat"
               "message-list" [{"id"     10000
@@ -42,9 +42,9 @@
                                "date"   "Today 0"}]}
 
       (dispatch! dispatcher {"event" "sim-next"})
-      (count (@ui "message-list")) => 10000)
+      (count (@fake-ui "message-list")) => 10000)
 
     (fact "Events are simulated as toasts"
       (dispatch! dispatcher {"event" "select-convo"
                              "id" 1042})
-      (@ui "toast") => "{\"event\" \"select-convo\", \"id\" 1042}")))
+      (@fake-ui "toast") => "{\"event\" \"select-convo\", \"id\" 1042}")))
