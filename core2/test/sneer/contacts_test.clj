e431a92c1f0def196d217d2b19b3561fe1c4135c
Klaus Wuestefeld <klauswuestefeld@gmail.com>
2016-03-28 17:13:44 -0300

    Minor

--- core2/test/sneer/contacts_test.clj
+++ core2/test/sneer/contacts_test.clj
@@ -15,30 +15,22 @@
     (fact "New contact appears on top of convo list"
       (handle! subject {:type :contact-new
                         :nick "Carla"})
-
       (get-in @ui [:convo-list 0 :nick]) => "Carla"
 
       (handle! subject {:type :contact-new
                         :nick "Maico"})
-
       (get-in @ui [:convo-list 0 :nick]) => "Maico"
       (get-in @ui [:convo-list 1 :nick]) => "Carla")
 
     (fact "Deleted contact is removed from convo list"
       (let [maico-id (get-in @ui [:convo-list 0 :contact-id])]
-        maico-id => some?                                   ;delete
-
         (handle! subject {:type :contact-delete
                           :contact-id maico-id})
-
         (get-in @ui [:convo-list 0 :nick]) => "Carla"))
 
     (fact "Contacts can be renamed"
       (let [carla-id (get-in @ui [:convo-list 0 :contact-id])]
-        carla-id => some?                                   ;delete
-
         (handle! subject {:type :contact-rename
                           :contact-id carla-id
                           :new-nick "Kharla"})
-
         (get-in @ui [:convo-list 0 :nick]) => "Kharla"))))
