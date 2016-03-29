0919e0fa4d281bc45de1cce96fd2bb0cf710a9db
Klaus Wuestefeld <klauswuestefeld@gmail.com>
2016-03-29 12:15:27 -0300

    Minor

--- core2/test/sneer/midje_util.clj
+++ core2/test/sneer/midje_util.clj
@@ -1,5 +1,5 @@
 (ns sneer.midje-util)
 
-(def => "Anything, just to prevent warnings 'symbol => undefined' from the midje fact macro.")
+(def => "Anything, just to prevent warnings 'symbol => undefined' from the midje fact macro. :(")
 
 ; (do (require 'midje.repl) (midje.repl/autotest))
