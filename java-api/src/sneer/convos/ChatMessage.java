7bdec9bbf8cdaeb65c01a4be29833c67e03c4188
Klaus Wuestefeld <klauswuestefeld@gmail.com>
2017-12-06 21:18:17 -0200

    ChatMessage has delivery status (WIP)

--- java-api/src/sneer/convos/ChatMessage.java
+++ java-api/src/sneer/convos/ChatMessage.java
@@ -2,11 +2,14 @@ package sneer.convos;
 
 public class ChatMessage {
 
+	public enum DeliveryStatus { WAITING, SENT, RECEIVED, READ };
+
    	public final long id;
    	public final String text;
    	public final boolean isOwn;
    	public final String date;
+   	public final DeliveryStatus delivery;
 
-   	public ChatMessage(long id, String text, boolean isOwn, String date) { this.id = id; this.text = text; this.isOwn = isOwn; this.date = date; }
+   	public ChatMessage(long id, String text, boolean isOwn, String date, DeliveryStatus delivery) { this.id = id; this.text = text; this.isOwn = isOwn; this.date = date; this.delivery = delivery}
 
 }
